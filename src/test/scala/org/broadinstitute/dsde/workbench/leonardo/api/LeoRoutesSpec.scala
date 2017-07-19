440d58da80f7299f4cd7a0f624c8a416f0d7167b
Hussein Elgridly <hussein@broadinstitute.org>
2017-07-19 15:26:46 -0400

    swagger and logback

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/api/LeoRoutesSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/api/LeoRoutesSpec.scala
@@ -2,12 +2,13 @@ package org.broadinstitute.dsde.workbench.leonardo.api
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.broadinstitute.dsde.workbench.leonardo.config.SwaggerConfig
 import org.scalatest.{FlatSpec, Matchers}
 
 class LeoRoutesSpec extends FlatSpec with Matchers with ScalatestRouteTest {
 
-  class TestLeoRoutes()
-    extends LeoRoutes
+  class TestLeoRoutes
+    extends LeoRoutes(SwaggerConfig())
 
   "LeoRoutes" should "200 on ping" in {
     val leoRoutes = new TestLeoRoutes()
