0276e849524272a01f8a66799de75438da70ddfb
Hussein Elgridly <hussein@broadinstitute.org>
2017-07-19 16:19:55 -0400

    wtf heinous indentation

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/api/SwaggerRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/api/SwaggerRoutes.scala
@@ -8,59 +8,60 @@ import akka.util.ByteString
 import org.broadinstitute.dsde.workbench.leonardo.config.SwaggerConfig
 
 trait SwaggerRoutes {
-private val swaggerUiPath = "META-INF/resources/webjars/swagger-ui/2.2.5"
+  private val swaggerUiPath = "META-INF/resources/webjars/swagger-ui/2.2.5"
 
-        val swaggerConfig: SwaggerConfig
+  val swaggerConfig: SwaggerConfig
 
-        val swaggerRoutes: server.Route = {
-        path("") {
-        get {
-        parameter("url") {urlparam =>
-        extractUri {uri =>
-        redirect(uri.withRawQueryString(""), StatusCodes.MovedPermanently)
-        }
+  val swaggerRoutes: server.Route = {
+    path("") {
+      get {
+        parameter("url") { urlparam =>
+          extractUri { uri =>
+            redirect(uri.withRawQueryString(""), StatusCodes.MovedPermanently)
+          }
         } ~
-        serveIndex()
-        }
-        } ~
-        path("api-docs.yaml") {
-        get {
+          serveIndex()
+      }
+    } ~
+    path("api-docs.yaml") {
+      get {
         getFromResource("swagger/api-docs.yaml")
-        }
-        } ~
-        // We have to be explicit about the paths here since we're matching at the root URL and we don't
-        // want to catch all paths lest we circumvent Spray's not-found and method-not-allowed error
-        // messages.
-        (pathSuffixTest("o2c.html") | pathSuffixTest("swagger-ui.js")
-        | pathPrefixTest("css" /) | pathPrefixTest("fonts" /) | pathPrefixTest("images" /)
-        | pathPrefixTest("lang" /) | pathPrefixTest("lib" /)) {
-        get {
+      }
+    } ~
+    // We have to be explicit about the paths here since we're matching at the root URL and we don't
+    // want to catch all paths lest we circumvent Spray's not-found and method-not-allowed error
+    // messages.
+    (pathSuffixTest("o2c.html") | pathSuffixTest("swagger-ui.js")
+      | pathPrefixTest("css" /) | pathPrefixTest("fonts" /) | pathPrefixTest("images" /)
+      | pathPrefixTest("lang" /) | pathPrefixTest("lib" /)) {
+      get {
         getFromResourceDirectory(swaggerUiPath)
-        }
-        }
-        }
+      }
+    }
+  }
 
-private def serveIndex(): server.Route = {
-        val swaggerOptions =
-        """
+  private def serveIndex(): server.Route = {
+    val swaggerOptions =
+      """
         |        validatorUrl: null,
         |        apisSorter: "alpha",
         |        operationsSorter: "alpha",
-        """.stripMargin
+      """.stripMargin
 
-        mapResponseEntity { entityFromJar =>
-        entityFromJar.transformDataBytes(Flow.fromFunction[ByteString, ByteString] { original: ByteString =>
+    mapResponseEntity { entityFromJar =>
+      entityFromJar.transformDataBytes(Flow.fromFunction[ByteString, ByteString] { original: ByteString =>
         ByteString(original.utf8String
-//        .replace("your-client-id", swaggerConfig.googleClientId) //awaiting integration with dev
-//        .replace("your-realms", swaggerConfig.realm)
-//        .replace("your-app-name", swaggerConfig.realm)
-        .replace("scopeSeparator: \",\"", "scopeSeparator: \" \"")
-        .replace("jsonEditor: false,", "jsonEditor: false," + swaggerOptions)
-        .replace("url = \"http://petstore.swagger.io/v2/swagger.json\";", "url = '/api-docs.yaml';")
-        )})
-        } {
-        getFromResource(swaggerUiPath + "/index.html")
-        }
-        }
+          //        .replace("your-client-id", swaggerConfig.googleClientId) //awaiting integration with dev
+          //        .replace("your-realms", swaggerConfig.realm)
+          //        .replace("your-app-name", swaggerConfig.realm)
+          .replace("scopeSeparator: \",\"", "scopeSeparator: \" \"")
+          .replace("jsonEditor: false,", "jsonEditor: false," + swaggerOptions)
+          .replace("url = \"http://petstore.swagger.io/v2/swagger.json\";", "url = '/api-docs.yaml';")
+        )
+      })
+    } {
+      getFromResource(swaggerUiPath + "/index.html")
+    }
+  }
 
-        }
+}
