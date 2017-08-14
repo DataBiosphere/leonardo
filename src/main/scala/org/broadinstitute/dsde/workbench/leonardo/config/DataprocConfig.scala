c5f08b00b62cc4a476b80f5687602163306c78ec
ansingh7115 <singh.an@husky.neu.edu>
2017-08-14 16:32:49 -0400

    GAWB-2268: serving up cluster url

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/DataprocConfig.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/DataprocConfig.scala
@@ -1,3 +1,8 @@
 package org.broadinstitute.dsde.workbench.leonardo.config
 
-case class DataprocConfig(serviceAccount: String, dataprocInitScriptURI: String, dataprocDefaultZone: String, dataprocDockerImage: String, serviceAccountPemPath: String)
+case class DataprocConfig(serviceAccount: String,
+                          dataprocInitScriptURI: String,
+                          dataprocDefaultZone: String,
+                          dataprocDockerImage: String,
+                          serviceAccountPemPath: String,
+                          clusterUrlBase: String)
