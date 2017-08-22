acc40165c3a2e21840138a4f722722dfba0bcfd3
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-08-22 13:59:29 -0400

    add mysql host and port

--- docker/install.sh
+++ docker/install.sh
@@ -6,7 +6,7 @@ LEONARDO_DIR=$1
 cd $LEONARDO_DIR
 
 # Test
-sbt -J-Xms4g -J-Xmx4g test
+sbt -J-Xms4g -J-Xmx4g test -Dmysql.host=mysql -Dmysql.port=3306
 sbt -J-Xms4g -J-Xmx4g assembly
 LEONARDO_JAR=$(find target | grep 'leonardo.*\.jar')
 mv $LEONARDO_JAR .
