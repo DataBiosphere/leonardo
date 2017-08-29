be82a4e775c7044e51427612ac4b6e97b310ee09
Joel Thibault <thibault@broadinstitute.org>
2017-08-29 11:45:37 -0400

    Build script updates - DB name changed to leotestdb - Generate error when no arguments are supplied - Remove old jars

--- docker/sql_validate.sh
+++ docker/sql_validate.sh
@@ -7,4 +7,4 @@ echo "sleeping for 60 seconds during mysql boot..."
 sleep 60
 mysql -uroot -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
 mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
-mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()" testdb
+mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()" leotestdb
