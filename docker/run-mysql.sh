30495e2233a3fb83f55b8c88d5e1654105a63d20
Jackie Roberti <jacqueline.roberti93@gmail.com>
2017-08-29 16:00:28 -0400

    fix

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -1,28 +1,19 @@
 #!/usr/bin/env bash
 
+# The CloudSQL console simply states "MySQL 5.6" so we may not match the minor version number
+MYSQL_VERSION=5.6
 start() {
-    # check if mysql is running
-    RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER || echo "false")
+    echo "starting up mysql container..."
+    docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=rawls-test -e MYSQL_USER=rawls-test -e MYSQL_PASSWORD=rawls-test -e MYSQL_DATABASE=testdb -d -p 3310:3306 mysql/mysql-server:$MYSQL_VERSION
 
-    # mysql set-up
-    if ! $RUNNING; then
-        # force remove mysql in case it is stopped
-        echo "attempting to remove old $CONTAINER container..."
-        docker rm -f $CONTAINER || echo "docker rm failed. nothing to rm."
-
-        # start up mysql
-        echo "starting up mysql container..."
-        docker run --name $CONTAINER -e MYSQL_ROOT_PASSWORD=${SERVICE}-test -e MYSQL_USER=${SERVICE}-test -e MYSQL_PASSWORD=${SERVICE}-test -e MYSQL_DATABASE=leotestdb -d -p 3310:3306 mysql/mysql-server:5.7.15
-
-        # validate mysql
-        echo "running mysql validation..."
-        docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh ${SERVICE}
-        if [ 0 -eq $? ]; then
-            echo "mysql validation succeeded."
-        else
-            echo "mysql validation failed."
-            exit 1
-        fi
+    # validate mysql
+    echo "running mysql validation..."
+    docker run --rm --link mysql:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh
+    if [ 0 -eq $? ]; then
+        echo "mysql validation succeeded."
+    else
+        echo "mysql validation failed."
+        exit 1
     fi
 }
 
@@ -34,7 +25,12 @@ stop() {
 
 CONTAINER=mysql-$(date +%s)
 COMMAND=$1
-SERVICE=$2
+
+if [ ${#@} == 0 ]; then
+    echo "Usage: $0 stop|start"
+    exit 1
+fi
+
 if [ $COMMAND = "start" ]; then
     start
 elif [ $COMMAND = "stop" ]; then
