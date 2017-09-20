7ae732e3557233ff7c3044ca2b593689699cf76e
Robert Title <rtitle@broadinstitute.org>
2017-09-20 15:22:45 -0400

    [GAWB-2002] Implement the GET /clusters endpoint to list notebook server clusters (#37)
    * Working, just need additional tests
    
    * Add tests
    
    * Swagger
    
    * Allow passing labels in a special way for swagger
    
    * Swagger wording
    
    * Comment
    
    * More testing
    
    * more tweaks
    
    * Moved label parsing to LeonardoService, updated tests
    
    * Handle _labels='' case
    
    * Swagger indentation fix
    
    * PR feedback, so far
    
    * More swagger verbiage

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/api/LeoRoutesSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/api/LeoRoutesSpec.scala
@@ -1,10 +1,10 @@
 package org.broadinstitute.dsde.workbench.leonardo.api
+
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import org.broadinstitute.dsde.workbench.leonardo.db.TestComponent
 import org.broadinstitute.dsde.workbench.leonardo.model.{Cluster, ClusterRequest}
 import org.broadinstitute.dsde.workbench.leonardo.model.LeonardoJsonSupport._
-import org.broadinstitute.dsde.workbench.leonardo.service.ClusterNotFoundException
 import org.broadinstitute.dsde.workbench.model.ErrorReport
 import org.scalatest.{FlatSpec, Matchers}
 import spray.json._
@@ -57,6 +57,75 @@ class LeoRoutesSpec extends FlatSpec with Matchers with ScalatestRouteTest with 
   it should "404 when deleting a cluster that does not exist" in {
     Delete(s"/api/cluster/nonexistent/bestclustername") ~> leoRoutes.route ~> check {
       status shouldEqual StatusCodes.NotFound
+    }
+  }
+
+  it should "200 when listing no clusters" in isolatedDbTest {
+    Get("/api/clusters") ~> leoRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      responseAs[List[Cluster]] shouldBe 'empty
+    }
+  }
+
+  it should "list clusters" in isolatedDbTest {
+    val googleProject = "test-project"
+    val clusterName = "test-cluster"
+    val newCluster = ClusterRequest("test-bucket-path", "test-service-account", Map.empty, None)
+    for (i <- 1 to 10) {
+      Put(s"/api/cluster/$googleProject/$clusterName-$i", newCluster.toJson) ~> leoRoutes.route ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+
+    Get("/api/clusters") ~> leoRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      val responseClusters = responseAs[List[Cluster]]
+      responseClusters should have size 10
+      responseClusters foreach { cluster =>
+        cluster.googleProject shouldEqual "test-project"
+        cluster.googleBucket shouldEqual "test-bucket-path"
+        cluster.googleServiceAccount shouldEqual "test-service-account"
+        cluster.labels shouldEqual Map.empty
+      }
+    }
+  }
+
+  it should "list clusters with labels" in isolatedDbTest {
+    val googleProject = "test-project"
+    val clusterName = "test-cluster"
+    val newCluster = ClusterRequest("test-bucket-path", "test-service-account", Map.empty, None)
+    for (i <- 1 to 10) {
+      Put(s"/api/cluster/$googleProject/$clusterName-$i", newCluster.copy(labels = Map(s"label$i" -> s"value$i")).toJson) ~> leoRoutes.route ~> check {
+        status shouldEqual StatusCodes.OK
+      }
+    }
+
+    Get("/api/clusters?label6=value6") ~> leoRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      val responseClusters = responseAs[List[Cluster]]
+      responseClusters should have size 1
+      val cluster = responseClusters.head
+      cluster.googleProject shouldEqual "test-project"
+      cluster.clusterName shouldEqual "test-cluster-6"
+      cluster.googleBucket shouldEqual "test-bucket-path"
+      cluster.googleServiceAccount shouldEqual "test-service-account"
+      cluster.labels shouldEqual Map("label6" -> "value6")
+    }
+
+    Get("/api/clusters?_labels=label4%3Dvalue4") ~> leoRoutes.route ~> check {
+      status shouldEqual StatusCodes.OK
+      val responseClusters = responseAs[List[Cluster]]
+      responseClusters should have size 1
+      val cluster = responseClusters.head
+      cluster.googleProject shouldEqual "test-project"
+      cluster.clusterName shouldEqual "test-cluster-4"
+      cluster.googleBucket shouldEqual "test-bucket-path"
+      cluster.googleServiceAccount shouldEqual "test-service-account"
+      cluster.labels shouldEqual Map("label4" -> "value4")
+    }
+
+    Get("/api/clusters?_labels=bad") ~> leoRoutes.route ~> check {
+      status shouldEqual StatusCodes.BadRequest
     }
   }
 
