2a27de95bb7422835eaef1071ea14d2bff15a1e3
Joel Thibault <thibault@broadinstitute.org>
2017-10-06 14:32:22 -0400

    Jupyter notebook tests

---
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/util/LocalFileUtil.scala
@@ -0,0 +1,17 @@
+package org.broadinstitute.dsde.workbench.util
+
+import java.io.File
+import java.nio.file.Files
+
+import org.openqa.selenium.WebDriver
+
+trait LocalFileUtil extends WebBrowserUtil {
+  def moveFile(source: File, dest: File)(implicit webDriver: WebDriver): Unit = {
+    await condition source.exists()
+
+    if (!dest.getParentFile.exists()) {
+      dest.getParentFile.mkdirs()
+    }
+    Files.move(source.toPath, dest.toPath)
+  }
+}
