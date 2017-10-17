50b31c4672e34f23c3e7ded60d4395049fd7dca7
ansingh7115 <singh.an@husky.neu.edu>
2017-10-17 19:58:19 -0400

    adding a mock userinfo

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/api/MockUserInfoDirectives.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/api/MockUserInfoDirectives.scala
@@ -1,14 +1,22 @@
 package org.broadinstitute.dsde.workbench.leonardo.api
 
+import java.time.Duration
+
+import akka.http.scaladsl.model.headers.OAuth2BearerToken
 import akka.http.scaladsl.server.Directive1
 import akka.http.scaladsl.server.Directives.provide
 import org.broadinstitute.dsde.workbench.leonardo.model.UserInfo
+import org.broadinstitute.dsde.workbench.model.{WorkbenchUserEmail, WorkbenchUserId}
 
 /**
   * Created by rtitle on 10/16/17.
   */
 trait MockUserInfoDirectives extends UserInfoDirectives {
-  val userInfo: UserInfo
+  val userInfo: UserInfo =
+    UserInfo(OAuth2BearerToken(""),
+      WorkbenchUserId(""),
+      WorkbenchUserEmail("test@test.com"),
+      10)
 
   override def requireUserInfo: Directive1[UserInfo] = provide(userInfo)
 }
