8aa71917a006d0393ad2891a4b7954754f495c3a
jmthibault79 <thibault@broadinstitute.org>
2017-11-28 12:51:30 -0500

    Attempt to de-flake GAWB-2617 tests (#78)
    * Increase timeouts
    * download fix

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/ResourceFile.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/ResourceFile.scala
@@ -4,7 +4,3 @@ import java.io.File
 
 // a standardized way to handle file upload/download involving the resources folder
 case class ResourceFile(path: String) extends File("src/test/resources/" + path)
-
-object ResourceFile {
-  def downloadsFile(path: String) = ResourceFile(s"downloads/$path")
-}
