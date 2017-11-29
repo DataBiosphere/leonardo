ebb618d969984583078bd9635130873b2e8df4f3
Joel Thibault <thibault@broadinstitute.org>
2017-11-29 14:37:15 -0500

    GoogleServiceAccount -> WorkbenchEmail

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/package.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/package.scala
@@ -5,7 +5,7 @@ import java.io.File
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ValueReader
 import org.broadinstitute.dsde.workbench.leonardo.model._
-import org.broadinstitute.dsde.workbench.model.google.{GoogleProject, ServiceAccountName}
+import org.broadinstitute.dsde.workbench.model.google.{GoogleProject, WorkbenchEmail}
 import org.broadinstitute.dsde.workbench.util.toScalaDuration
 
 package object config {
@@ -19,7 +19,7 @@ package object config {
   implicit val dataprocConfigReader: ValueReader[DataprocConfig] = ValueReader.relative { config =>
     DataprocConfig(
       config.getString("applicationName"),
-      GoogleServiceAccount(config.getString("serviceAccount")),
+      WorkbenchEmail(config.getString("serviceAccount")),
       config.getString("dataprocDefaultRegion"),
       GoogleProject(config.getString("leoGoogleProject")),
       config.getString("dataprocDockerImage"),
