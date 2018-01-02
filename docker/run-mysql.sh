522bc2790f150a79a889d32049b66a73e81e6a38
Hussein Elgridly <helgridly@users.noreply.github.com>
2018-01-02 15:41:22 -0500

    remove dependency on dsde-toolbox (#106)

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -13,7 +13,7 @@ start() {
 
     # validate mysql
     echo "running mysql validation..."
-    docker run --rm --link $CONTAINER:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh broadinstitute/dsde-toolbox /working/sql_validate.sh leonardo
+    docker run --rm --link $CONTAINER:mysql -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh mysql:$MYSQL_VERSION /working/sql_validate.sh leonardo
     if [ 0 -eq $? ]; then
         echo "mysql validation succeeded."
     else
