1e2010fd0b9fe3cde7041f1ada9ad023d76e658a
ansingh7115 <singh.an@husky.neu.edu>
2018-01-09 21:52:50 -0500

    oops

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/auth/SamAuthProviderSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/auth/SamAuthProviderSpec.scala
@@ -8,7 +8,7 @@ class SamAuthProviderSpec extends FlatSpec with CommonTestData with ScalaFutures
   private val samAuth = new SamAuthProvider(config.getConfig("auth.samAuthProviderConfig"), serviceAccountProvider)
 
   "SamAuthProvider" should "construct a sane resourcesApi" in {
-    val resourcesAPI = samAuth.resourcesApiAsPet(defaultUserInfo.userEmail)
+    val resourcesAPI = samAuth.resourcesApiAsPet(defaultUserInfo.userEmail, project.value)
     resourcesAPI.resourceAction("billing-project", project.value, "launch_notebook_cluster")
   }
 }
