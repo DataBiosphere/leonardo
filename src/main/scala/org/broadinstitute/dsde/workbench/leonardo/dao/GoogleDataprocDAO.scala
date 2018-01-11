6d404ab86fe9b56f514faab69c61d05da1e8ffc7
jmthibault79 <thibault@broadinstitute.org>
2018-01-11 15:47:32 -0500

    GAWB-2838: BigQuery inside notebooks (#113)
    * Add Bigquery scopes to Dataproc cluster SA
    * BigQuery Swat test
    * Update formatting for new version of executeCell()
    * Copy Google Role endpoints from UI-Repo Swat

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/GoogleDataprocDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/GoogleDataprocDAO.scala
@@ -18,6 +18,7 @@ import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest
 import com.google.api.client.http.{AbstractInputStreamContent, FileContent, InputStreamContent}
 import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.bigquery.BigqueryScopes
 import com.google.api.services.cloudresourcemanager.{CloudResourceManager, CloudResourceManagerScopes}
 import com.google.api.services.compute.model.Firewall.Allowed
 import com.google.api.services.compute.model.{Firewall, Instance}
@@ -75,6 +76,7 @@ class GoogleDataprocDAO(protected val leoServiceAccountEmail: WorkbenchEmail,
   private lazy val vmScopes = List(ComputeScopes.COMPUTE, ComputeScopes.CLOUD_PLATFORM)
   private lazy val oauth2Scopes = List(Oauth2Scopes.USERINFO_EMAIL, Oauth2Scopes.USERINFO_PROFILE)
   private lazy val cloudResourceManagerScopes = List(CloudResourceManagerScopes.CLOUD_PLATFORM)
+  private lazy val bigqueryScopes = List(BigqueryScopes.BIGQUERY)
 
   private lazy val oauth2 =
     new Builder(httpTransport, jsonFactory, null)
@@ -165,7 +167,7 @@ class GoogleDataprocDAO(protected val leoServiceAccountEmail: WorkbenchEmail,
     // Set the cluster service account, if present.
     // This is the service account passed to the create cluster API call.
     serviceAccountInfo.clusterServiceAccount.foreach { serviceAccountEmail =>
-      gceClusterConfig.setServiceAccount(serviceAccountEmail.value).setServiceAccountScopes(oauth2Scopes.asJava)
+      gceClusterConfig.setServiceAccount(serviceAccountEmail.value).setServiceAccountScopes((oauth2Scopes ++ bigqueryScopes).asJava)
     }
 
     // Create a NodeInitializationAction, which specifies the executable to run on a node.
