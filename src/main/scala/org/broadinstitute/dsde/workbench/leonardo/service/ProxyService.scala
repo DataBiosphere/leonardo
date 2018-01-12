771d05a81c1b0b8ee3dd4648f3a0f2e056c5c96e
ansingh7115 <singh.an@husky.neu.edu>
2018-01-12 10:50:01 -0500

    more logging

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/ProxyService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/ProxyService.scala
@@ -74,6 +74,7 @@ class ProxyService(proxyConfig: ProxyConfig,
       if (!hasViewPermission) {
         throw ClusterNotFoundException(googleProject, clusterName)
       } else if (!hasRequiredPermission) {
+        logger.info("auth check failed: hasRequiredPermission:" + hasRequiredPermission.toString)
         throw AuthorizationError(Option(userInfo.userEmail))
       } else {
         ()
