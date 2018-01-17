3adb6137d0f27c1e746545a27c8f67b14d98b217
ansingh7115 <singh.an@husky.neu.edu>
2018-01-16 22:26:57 -0500

    go back to whitelist

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/service/LeonardoServiceSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/service/LeonardoServiceSpec.scala
@@ -56,9 +56,7 @@ class LeonardoServiceSpec extends TestKit(ActorSystem("leonardotest")) with Flat
     // TODO look into parameterized tests so both provider impls can both be tested
     //serviceAccountProvider = new MockPetServiceAccountProvider(configFactory.getConfig("serviceAccounts.config"))
     serviceAccountProvider = new MockPetsPerProjectServiceAccountProvider(configFactory.getConfig("serviceAccounts.config"))
-
-    //authProvider = new SamAuthProvider(configFactory.getConfig("auth.samAuthProviderConfig"), serviceAccountProvider)
-    authProvider = new MockLeoAuthProvider(configFactory.getConfig("auth.samAuthProviderConfig"), serviceAccountProvider)
+    authProvider = new WhitelistAuthProvider(configFactory.getConfig("auth.providerConfig"),serviceAccountProvider)
 
     leo = new LeonardoService(dataprocConfig, clusterFilesConfig, clusterResourcesConfig, proxyConfig, swaggerConfig, gdDAO, iamDAO, DbSingleton.ref, system.actorOf(NoopActor.props), authProvider, serviceAccountProvider, whitelist)
   }
