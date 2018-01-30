cadf98b4c1aa5756231d41f5d7577b1acd1c6c23
jmthibault79 <thibault@broadinstitute.org>
2018-01-30 11:14:17 -0500

    GAWB-3108 Defer Deletion of Sam Cluster Resources (#139)
    * Delete cluster resources in Sam only after deletion is complete in Google and the Leo DB
    * Integration Tests: Downgrade selenium/node-chrome to 3.8.1-erbium

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/monitor/TestClusterSupervisorActor.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/monitor/TestClusterSupervisorActor.scala
@@ -6,19 +6,19 @@ import org.broadinstitute.dsde.workbench.google.GoogleIamDAO
 import org.broadinstitute.dsde.workbench.leonardo.config.{DataprocConfig, MonitorConfig}
 import org.broadinstitute.dsde.workbench.leonardo.dao.DataprocDAO
 import org.broadinstitute.dsde.workbench.leonardo.db.DbReference
-import org.broadinstitute.dsde.workbench.leonardo.model.Cluster
+import org.broadinstitute.dsde.workbench.leonardo.model.{Cluster, LeoAuthProvider}
 
 import scala.concurrent.duration._
 
 object TestClusterSupervisorActor {
-  def props(dataprocConfig: DataprocConfig, gdDAO: DataprocDAO, googleIamDAO: GoogleIamDAO, dbRef: DbReference, clusterDnsCache: ActorRef, testKit: TestKit): Props =
-    Props(new TestClusterSupervisorActor(dataprocConfig, gdDAO, googleIamDAO, dbRef, clusterDnsCache, testKit))
+  def props(dataprocConfig: DataprocConfig, gdDAO: DataprocDAO, googleIamDAO: GoogleIamDAO, dbRef: DbReference, clusterDnsCache: ActorRef, testKit: TestKit, authProvider: LeoAuthProvider): Props =
+    Props(new TestClusterSupervisorActor(dataprocConfig, gdDAO, googleIamDAO, dbRef, clusterDnsCache, testKit, authProvider))
 }
 
 /**
   * Extends ClusterMonitorSupervisor so the akka TestKit can watch the child ClusterMonitorActors.
   */
-class TestClusterSupervisorActor(dataprocConfig: DataprocConfig, gdDAO: DataprocDAO, googleIamDAO: GoogleIamDAO, dbRef: DbReference, clusterDnsCache: ActorRef, testKit: TestKit) extends ClusterMonitorSupervisor(MonitorConfig(100 millis), dataprocConfig, gdDAO, googleIamDAO, dbRef, clusterDnsCache) {
+class TestClusterSupervisorActor(dataprocConfig: DataprocConfig, gdDAO: DataprocDAO, googleIamDAO: GoogleIamDAO, dbRef: DbReference, clusterDnsCache: ActorRef, testKit: TestKit, authProvider: LeoAuthProvider) extends ClusterMonitorSupervisor(MonitorConfig(100 millis), dataprocConfig, gdDAO, googleIamDAO, dbRef, clusterDnsCache, authProvider) {
   override def createChildActor(cluster: Cluster): ActorRef = {
     val child = super.createChildActor(cluster)
     testKit watch child
