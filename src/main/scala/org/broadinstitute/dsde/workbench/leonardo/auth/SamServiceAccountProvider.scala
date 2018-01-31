67db854eba3ddbd602179a99beed8ee7daf2b5b0
vkumra-broad <vkumra@broadinstitute.org>
2018-01-31 16:00:23 -0500

    these configs should work(fingers crossed)

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/auth/SamServiceAccountProvider.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/auth/SamServiceAccountProvider.scala
@@ -20,11 +20,11 @@ abstract class SamServiceAccountProvider(config: Config) extends ServiceAccountP
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
-  protected lazy val samConfig = config.as[SamConfig]("samServer")
+  protected lazy val samServer = config.as[String]("samServer")
   protected lazy val cacheExpiryTime = toScalaDuration(Option(config.getDuration("samCacheExpiryTime")).getOrElse(Duration.ofMinutes(60)))
   protected lazy val cacheMaxSize = Option(config.as[Int]("samCacheMaxSize")).getOrElse(1000)
   protected lazy val (leoEmail, leoPemFile) = getLeoServiceAccountAndKey
-  protected lazy val samDAO = new HttpSamDAO(samConfig.server)
-  protected lazy val samClient = new SwaggerSamClient(samConfig.server, cacheExpiryTime, cacheMaxSize, leoEmail, leoPemFile)
+  protected lazy val samDAO = new HttpSamDAO(samServer)
+  protected lazy val samClient = new SwaggerSamClient(samServer, cacheExpiryTime, cacheMaxSize, leoEmail, leoPemFile)
 
 }
