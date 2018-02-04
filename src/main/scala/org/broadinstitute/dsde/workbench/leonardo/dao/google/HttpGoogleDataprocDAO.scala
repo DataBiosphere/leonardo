c39c397555910829f1e10b6550f50e2f5ad94a77
Rob Title <rtitle@broadinstitute.org>
2018-02-04 00:14:29 -0500

    Minor typo

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/google/HttpGoogleDataprocDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/google/HttpGoogleDataprocDAO.scala
@@ -91,7 +91,7 @@ class HttpGoogleDataprocDAO(appName: String,
       ()
     } {
       case e: HttpResponseException if e.getStatusCode == StatusCodes.NotFound.intValue => ()
-      case e: GoogleJsonResponseException if e.getStatusCode == StatusCodes.BadRequest &&
+      case e: GoogleJsonResponseException if e.getStatusCode == StatusCodes.BadRequest.intValue &&
         e.getDetails.getMessage.contains("it has other pending delete operations against it") => ()
     }
   }
