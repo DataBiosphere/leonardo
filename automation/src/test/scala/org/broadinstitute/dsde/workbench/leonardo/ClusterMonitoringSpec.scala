d06a0ab7dd32f925dc10c7d3f1f68417d8b59f46
Joel Thibault <thibault@broadinstitute.org>
2018-02-09 12:12:51 -0500

    Preemptible test for #127

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/ClusterMonitoringSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/ClusterMonitoringSpec.scala
@@ -1,7 +1,10 @@
 package org.broadinstitute.dsde.workbench.leonardo
 
-import org.broadinstitute.dsde.workbench.service.Orchestration
-import org.broadinstitute.dsde.workbench.dao.Google.googleIamDAO
+import org.broadinstitute.dsde.workbench.service.{Orchestration, Sam}
+import org.broadinstitute.dsde.workbench.dao.Google.{googleIamDAO, googleStorageDAO}
+import org.broadinstitute.dsde.workbench.model.google.GcsEntityTypes.Group
+import org.broadinstitute.dsde.workbench.model.google.GcsRoles.Reader
+import org.broadinstitute.dsde.workbench.model.google.{GcsEntity, GcsObjectName, GcsPath, parseGcsPath}
 import org.scalatest.{FreeSpec, ParallelTestExecution}
 
 class ClusterMonitoringSpec extends FreeSpec with LeonardoTestUtils with ParallelTestExecution {
@@ -59,6 +62,36 @@ class ClusterMonitoringSpec extends FreeSpec with LeonardoTestUtils with Paralle
         googlePetEmail2 shouldBe Some(petEmail)
       }
     }
+
+    "should execute Hail with correct permissions on a cluster with preemptible workers" in withWebDriver { implicit driver =>
+      withNewBillingProject { project =>
+        Orchestration.billing.addUserToBillingProject(project.value, ronEmail, Orchestration.billing.BillingProjectRole.User)(hermioneAuthToken)
+
+        withNewGoogleBucket(project) { bucket =>
+          val srcPath = parseGcsPath("gs://genomics-public-data/1000-genomes/vcf/ALL.chr20.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.vcf").right.get
+          val destPath = GcsPath(bucket, GcsObjectName("chr20.vcf"))
+          googleStorageDAO.copyObject(srcPath.bucketName, srcPath.objectName, destPath.bucketName, destPath.objectName)
+
+          implicit val token = ronAuthToken
+          val ronProxyGroup = Sam.user.proxyGroup(ronEmail)
+          val ronPetEntity = GcsEntity(ronProxyGroup, Group)
+          googleStorageDAO.setObjectAccessControl(destPath.bucketName, destPath.objectName, ronPetEntity, Reader)
+
+          val request = ClusterRequest(machineConfig = Option(MachineConfig(
+            // need at least 2 regular workers to enable preemptibles
+            numberOfWorkers = Option(2),
+            numberOfPreemptibleWorkers = Option(10)
+          )))
+
+          withNewCluster(project, request = request) { cluster =>
+            withNewNotebook(cluster) { notebookPage =>
+              verifyHailImport(notebookPage, destPath, cluster.clusterName)
+            }
+          }
+        }
+      }
+    }
+
 
   }
 
