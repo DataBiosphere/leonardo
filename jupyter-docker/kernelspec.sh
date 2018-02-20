e34ad12c1e1e0d39ed7366f03ad5bfb14a5cdb08
ansingh7115 <singh.an@husky.neu.edu>
2018-02-20 18:52:05 -0500

    this works, its different this time I swear

---
+++ jupyter-docker/kernelspec.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+TMP_KERNELSPEC_DIR=$1
+KERNELSPEC_HOME=$2
+
+# replace the contents of the kernel scripts
+sed 's/${PY_VERSION}/2/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec_template.json > ${KERNELSPEC_HOME}/python2/kernel.json
+sed 's/${PY_VERSION}/3/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec_template.json > ${KERNELSPEC_HOME}/python3/kernel.json
+
+
--- src/main/resources/jupyter/init-actions.sh
+++ src/main/resources/jupyter/init-actions.sh
@@ -24,6 +24,7 @@ if [[ "${ROLE}" == 'Master' ]]; then
     JUPYTER_HOME=/etc/jupyter
     JUPYTER_USER_HOME=/home/jupyter-user
     PYSPARK=/usr/bin/pyspark
+    KERNELSPEC_HOME=/usr/local/share/jupyter/kernels
 
     # The following values are populated by Leo when a cluster is created.
     export CLUSTER_NAME=$(clusterName)
@@ -95,7 +96,7 @@ if [[ "${ROLE}" == 'Master' ]]; then
     docker-compose -f /etc/cluster-docker-compose.yaml up -d
 
     # Change python 2 and 3 kernel specs to allow each to have its own spark
-    docker exec -u root -d ${JUPYTER_SERVER_NAME} /etc/jupyter/python_kernelspec.sh
+    docker exec -u root -d ${JUPYTER_SERVER_NAME} ${JUPYTER_HOME}/kernelspec.sh ${JUPYTER_HOME} ${KERNELSPEC_HOME}
 
     # Install the Hail additions to Spark conf.
     # OK to do this after pyspark runs; it needs to happen before the Jupyter kernel starts.
