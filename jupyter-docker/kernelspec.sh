215bca1299ebce664233c059019cf86ed2d464fd
ansingh7115 <singh.an@husky.neu.edu>
2018-02-20 21:07:29 -0500

    rebasing for debian gave me two different python3s

--- jupyter-docker/kernelspec.sh
+++ jupyter-docker/kernelspec.sh
@@ -4,7 +4,7 @@ TMP_KERNELSPEC_DIR=$1
 KERNELSPEC_HOME=$2
 
 # replace the contents of the kernel scripts
-sed 's/${PY_VERSION}/2/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNELSPEC_HOME}/python2/kernel.json
-sed 's/${PY_VERSION}/3/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNELSPEC_HOME}/python3/kernel.json
+sed 's/${PY_VERSION}/2.7/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNELSPEC_HOME}/python2/kernel.json
+sed 's/${PY_VERSION}/3.4/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNELSPEC_HOME}/python3/kernel.json
 
 
