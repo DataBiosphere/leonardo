be741fc82d3a23fd10de2b87c511021b39bcd4be
Andrea Haessly <ahaessly@broadinstitute.org>
2018-02-28 15:12:43 -0500

    add more logging

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/ProxyService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/ProxyService.scala
@@ -104,6 +104,7 @@ class ProxyService(proxyConfig: ProxyConfig,
     */
   def proxyNotebook(userInfo: UserInfo, googleProject: GoogleProject, clusterName: ClusterName, request: HttpRequest): Future[HttpResponse] = {
     authCheck(userInfo, googleProject, clusterName, ConnectToCluster).flatMap { _ =>
+      logger.warn(s"CHECKIT ${userInfo.userEmail}:${userInfo.userId} ${request}")
       proxyInternal(userInfo, googleProject, clusterName, request)
     }
   }
@@ -163,6 +164,7 @@ class ProxyService(proxyConfig: ProxyConfig,
     // data between client, proxy, and server. However Jupyter is doing something strange and the proxy only
     // works when toStrict is used. Luckily, it's only needed for HTTP requests (which are fairly small) and not
     // WebSocket requests (which could potentially be large).
+    logger.info("CHECKIT")
     val handler: Future[HttpResponse] = Source.single(newRequest).log("handlingRequest")
       .via(flow)
       .runWith(Sink.head)
