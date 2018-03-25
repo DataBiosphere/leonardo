42ea96d72f3177108ee0ebbea42758f5e78dc962
CH Albach <calbach@users.noreply.github.com>
2018-03-25 12:06:37 -0700

    Implement a delocalizing contents manager for GCS (#259)
    * Fix run-jupyter-local to use the Leo docker image
    
    * Implement a delocalizing contents manager for GCS
    
    * Add tests
    
    * rename and cleanup

--- jupyter-docker/jupyter_localize_extension.py
+++ jupyter-docker/jupyter_localize_extension.py
@@ -56,3 +56,4 @@ def load_jupyter_server_extension(nb_server_app):
   host_pattern = '.*$'
   route_pattern = url_path_join(web_app.settings['base_url'], '/api/localize')
   web_app.add_handlers(host_pattern, [(route_pattern, LocalizeHandler)])
+  nb_server_app.log.info('initialized jupyter_localize_extension')
