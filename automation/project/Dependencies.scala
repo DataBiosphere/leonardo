f42bde55b8954e5ead36887daaaedcbcbcdacd82
dvoet <dvoet@users.noreply.github.com>
2018-03-27 22:03:12 -0400

    Pet name change (#286)

--- automation/project/Dependencies.scala
+++ automation/project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
   val akkaHttpV = "10.0.5"
 
   val workbenchModelV   = "0.10-6800f3a"
-  val workbenchGoogleV  = "0.16-16e3aab"
-  val serviceTestV = "0.6-c7e4984"
+  val workbenchGoogleV  = "0.16-847c3ff"
+  val serviceTestV = "0.7-847c3ff"
 
   val excludeWorkbenchModel =   ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-model_2.11")
   val excludeWorkbenchGoogle =   ExclusionRule(organization = "org.broadinstitute.dsde.workbench", name = "workbench-google_2.11")
