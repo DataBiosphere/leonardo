b5d2c3e8b7b65cc41d138bae5483b9bc48e0bb5a
Robert Title <rtitle@broadinstitute.org>
2018-04-06 14:44:49 -0400

    290: Add retries to SwaggerSamClient (#299)
    * 290: Add retries to SwaggerSamClient
    
    * Wasn't testing the right thing

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/auth/sam/SamProvider.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/auth/sam/SamProvider.scala
@@ -2,16 +2,19 @@ package org.broadinstitute.dsde.workbench.leonardo.auth.sam
 
 import java.io.File
 
+import akka.actor.ActorSystem
 import com.typesafe.config.Config
+import com.typesafe.scalalogging.LazyLogging
 import net.ceedubs.ficus.Ficus._
 import org.broadinstitute.dsde.workbench.model.WorkbenchEmail
+import org.broadinstitute.dsde.workbench.util.Retry
 
 import scala.concurrent.duration._
 
 /**
   * Common trait for Leo providers which need a SwaggerSamClient.
   */
-trait SamProvider {
+trait SamProvider extends Retry with LazyLogging {
   val config: Config
 
   protected def getLeoServiceAccountAndKey: (WorkbenchEmail, File)
@@ -21,4 +24,5 @@ trait SamProvider {
   protected lazy val cacheMaxSize = config.getAs[Int]("petTokenCacheMaxSize").getOrElse(1000)
   protected lazy val (leoEmail, leoPemFile) = getLeoServiceAccountAndKey
   protected lazy val samClient = new SwaggerSamClient(samServer, cacheExpiryTime, cacheMaxSize, leoEmail, leoPemFile)
+  override val system = ActorSystem("leonardo-sam-provider")
 }
