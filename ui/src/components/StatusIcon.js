938edfaa4fe9f733bc95013f79757164781bc94d
Evan Parker <evanp@google.com>
2018-04-13 13:05:46 -0700

    User interface: dynamic status icon.
    Status icon for better "quick glance" cluster status
    recognition. See it in gif form here
    https://docs.google.com/document/d/1_EDQR347f7CsTxy5OWiXOn3iV9IKwOv-M9wPmilWUlU

--- ui/src/components/StatusIcon.js
+++ ui/src/components/StatusIcon.js
@@ -1,27 +1,102 @@
 import React from 'react';
 
 import Icon from 'material-ui/Icon';
+
+import { CircularProgress } from 'material-ui/Progress';
 import Tooltip from 'material-ui/Tooltip';
+
+import green from 'material-ui/colors/green';
+import yellow from 'material-ui/colors/yellow';
+import red from 'material-ui/colors/red';
+
+
+const statusColorIconMap = {
+  Creating: {iconType: "progress", iconName: null,       color: green[500]},
+  Updating: {iconType: "progress", iconName: null,       color: yellow[800]},
+  Deleting: {iconType: "progress", iconName: null,       color: red[800]},
+  Running:  {iconType: "icon",     iconName: "computer", color: green[500]},
+  Deleted:  {iconType: "icon",     iconName: "computer", color: red[800]},
+  Unknown:  {iconType: "icon",     iconName: "info",     color: yellow[800]},
+  Error:    {iconType: "icon",     iconName: "info",     color: red[800]}
+}
 
 
 /**
- * Display the status as a tooltip. This is a placeholder for a component whose
- * color and icon change in response to cluster status.
+ * Display the status as an icon or progress/loading circle. Color and icon
+ * are set in response to cluster status.
  * @props clusterStatus string from clusterStatus enumeration.
  */
 class StatusIcon extends React.Component {
+
   render() {
+    console.log(this.props.clusterStatus);
+    var status = this.props.clusterStatus;
+    var statusMessage = "Status: " + status;
+    // Check if status in in the color/icon map. Create an informative status
+    // tooltip and set the status (used for icon selection) to 'Unknown',
+    if (!(status in statusColorIconMap)) {
+      console.log('')
+      statusMessage = "Cannot parse '" + status + "'";
+      status = "Unknown";
+    }
+    var color = statusColorIconMap[status].color;
+    var iconName = statusColorIconMap[status].iconName;
+    var iconType = statusColorIconMap[status].iconType;
+    if (iconType === "progress") {
+      return (
+        <StatusProgress
+            statusToolTip={statusMessage}
+            color={color}
+        />
+      );
+    }
     return (
-      <div style={{paddingTop: 10}}>
-      <Tooltip id="tooltip-icon-bottom" title={this.props.clusterStatus} >
-      <Icon style={{fontSize: 32}}>
-        info
-      </Icon>
-      </Tooltip>
-      </div>
+      <StatusIconIcon
+          statusToolTip={statusMessage}
+          color={color}
+          iconName={iconName}
+      />
     );
   }
 }
 
+
+/**
+ * Display an icon and tooltip for the status.
+ * @props statusToolTip string shown in icon tooltip.
+ * @props color string of hex color code for the icon.
+ * @props iconName string material icon name.
+ */
+class StatusIconIcon extends React.Component {
+  render() {
+    return (
+      <div style={{paddingTop: 10}}>
+      <Tooltip id="tooltip-icon-bottom" title={this.props.statusToolTip} >
+      <Icon style={{fontSize: 32, color: this.props.color}}>
+        {this.props.iconName}
+      </Icon>
+      </Tooltip>
+      </div>
+    )
+  }
+}
+
+
+/**
+ * Display a colored progress spinner and tooltip for the status.
+ * @props statusToolTip string shown in icon tooltip.
+ * @props color string of hex color code for the icon.
+ */
+class StatusProgress extends React.Component {
+  render() {
+    return (
+      <div style={{paddingTop: 10}}>
+        <Tooltip id="tooltip-bottom" title={this.props.statusToolTip} >
+          <CircularProgress style={{color: this.props.color}} />
+        </Tooltip>
+      </div>
+    );
+  }
+}
 
 export default StatusIcon;
