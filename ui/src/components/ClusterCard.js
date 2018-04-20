2f9ad4cd9d2c62ef10431007faf535f9badcaf0b
Evan Parker <evanp@google.com>
2018-04-20 09:28:42 -0700

    First UI pull request (#314)
    * User Interface: template React app and error handler.
    
    * User Interface: added google auth
    
    * User Interface: cluster state and cards.
    
    * User Interface: update package files
    
    * User Interface: app-bar icon and status icon
    
    * User Interface: Add connect button

---
+++ ui/src/components/ClusterCard.js
@@ -0,0 +1,131 @@
+import React from 'react';
+
+import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Grid from 'material-ui/Grid';
+import PropTypes from 'prop-types';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+
+import ConnectButton from './ConnectButton'
+//import DeleteDialog from './DeleteDialog'
+import StatusIcon from './StatusIcon'
+
+
+const styles = {
+  card: {
+    minWidth: 300,
+    maxWidth: 550,
+  },
+  supertitle: {
+    marginBottom: 0,
+    marginTop: 0,
+    fontSize: 16
+  },
+  title: {
+    marginBottom: 10,
+    fontSize: 24
+  },
+  subtitle: {
+    marginBottom: 12,
+  },
+  description: {
+    fontSize: 15,
+  },
+};
+
+
+/**
+ * @props errorHandler function callback displays a string as a dismissable error.
+ * @props googleToken string access token provided by oauth login.
+ * @props oauthClientId string oauth client used for app auth.
+ * @props clusterModel object contains a cluster model returned by Leonardo's API.
+ */
+class ClusterCard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      clusterStatus: this.props.clusterModel.status,
+    };
+  }
+
+  setClusterStatusRunning = () => {
+    this.setState({ clusterStatus: "Running" });
+  }
+
+  setClusterStatusError = () => {
+    this.setState({ clusterStatus: "Error" });
+  }
+
+  setClusterStatusDeleting = () => {
+    this.setState({ clusterStatus: "Deleting" });
+  }
+
+  render() {
+    var classes = this.props.classes;
+    var model = this.props.clusterModel;
+    var machineCfg = model.machineConfig;
+    return (
+      <Grid item xs={12} sm={10}>
+      <Card className={classes.card}>
+      <CardContent>
+        <Grid container justify="space-between">
+          <Grid item xs={10}>
+            <Typography
+              className={classes.supertitle}
+              color="textSecondary"
+            >
+              {model.googleProject}
+            </Typography>
+            <Typography
+              className={classes.title}>
+              {model.clusterName}
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={1}
+          >
+            <StatusIcon clusterStatus={this.state.clusterStatus} />
+          </Grid>
+        </Grid>
+
+        <Typography
+          xs={8}
+          className={classes.description}
+        >
+          Owner: {model.creator}
+          <br/>
+          Master type: {machineCfg.masterMachineType}
+          <br/>
+          Workers: {machineCfg.numberOfWorkers}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <ConnectButton
+          oauthClientId={this.props.oauthClientId}
+          errorHandler={this.props.errorHandler}
+          googleAuthToken={this.props.googleAuthToken}
+          clusterStatus={this.state.clusterStatus}
+          clusterModel={model}
+        />
+
+
+      <div>
+                        Delete Dialog Placeholder
+      </div>
+
+
+      </CardActions>
+      </Card>
+      </Grid>
+    );
+  }
+}
+
+ClusterCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+
+export default withStyles(styles)(ClusterCard);
---
+++ ui/src/components/ClusterCardList.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Grid from 'material-ui/Grid';
+
+import ClusterCard from './ClusterCard' 
+
+
+/**
+ * Manages rendering of a list of cluster cards.
+ * @props errorHandler function callback displays a string as a dismissable error.
+ * @props googleToken string access token provided by oauth login.
+ * @props oauthClientId string oauth client used for app auth.
+ * @props clusterModels list of cluster objects, each object contains an object
+ *        with a cluster model (used by the card component).
+ */
+class ClusterCardList extends React.Component {
+  render() {
+    var clusterCards = [];
+    var models = this.props.clusterModels;
+    for (var i = 0; i < models.length; i++) {
+      var model = models[i];
+      var clusterKey = model.googleProject + "/" + model.clusterName;
+      clusterCards.push(
+        <ClusterCard
+          oauthClientId={this.props.oauthClientId}
+          googleAuthToken={this.props.googleAuthToken}
+          errorHandler={this.props.errorHandler}
+          clusterModel={model}
+          key={clusterKey} />
+      );
+    }
+    return (
+      <div style={{"padding": 24}}>
+        <Grid
+          container
+          spacing={24}
+          direction="column"
+        >
+        {clusterCards}
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default ClusterCardList;
