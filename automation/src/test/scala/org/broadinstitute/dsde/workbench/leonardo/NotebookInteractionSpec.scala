5743a95dea03dedf996cde0afe96472c7de4e862
Rob Title <rtitle@broadinstitute.org>
2018-04-30 15:01:08 -0400

    Use the same NotebookListPage instance

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/NotebookInteractionSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/NotebookInteractionSpec.scala
@@ -302,15 +302,17 @@ class NotebookInteractionSpec extends FreeSpec with LeonardoTestUtils with Befor
 
     Seq(Python2, Python3).foreach { kernel =>
       s"should be able to pip install packages using ${kernel.string}" in withWebDriver { implicit driver =>
-        withNewNotebook(ronCluster, kernel) { notebookPage =>
+        withNotebooksListPage(ronCluster) { notebooksListPage =>
           // install tensorflow
-          pipInstall(notebookPage, kernel, "tensorflow")
-        }
+          notebooksListPage.withNewNotebook(kernel) { notebookPage =>
+            pipInstall(notebookPage, kernel, "tensorflow")
+          }
 
-        // need to restart the kernel for the install to take effect
-        withNewNotebook(ronCluster, kernel) { notebookPage =>
-          // verify that tensorflow is installed
-          verifyTensorFlow(notebookPage, kernel)
+          // need to restart the kernel for the install to take effect
+          notebooksListPage.withNewNotebook(kernel) { notebookPage =>
+            // verify that tensorflow is installed
+            verifyTensorFlow(notebookPage, kernel)
+          }
         }
       }
     }
