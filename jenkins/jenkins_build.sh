ba3b90cb995093ab566cb4be79804525612fbf46
Rob Title <rtitle@broadinstitute.org>
2018-05-11 21:45:42 -0400

    Add support for obtaining a GCR service account from vault

---
+++ jenkins/jenkins_build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux
+
+if [ "$skip_docker_build" = false ]; then
+  GCR_SVCACCT_VAULT="secret/dsde/dsp-techops/common/dspci-wb-gcr-service-account.json"
+  GCR_REPO_PROJ="broad-dsp-gcr-public"
+
+  docker run --rm  -v /etc/vault-token-dsde:/root/.vault-token:ro \
+       broadinstitute/dsde-toolbox:latest vault read --format=json ${GCR_SVCACCT_VAULT} \
+       | jq .data > dspci-wb-gcr-service-account.json
+
+  ./docker/build.sh jar -d push -n "us.gcr.io/broad-dsp-gcr-public" \
+      -k "dspci-wb-gcr-service-account.json"
+
+  # clean up
+  rm -f dspci-wb-gcr-service-account.json
+else
+  GIT_SHA=$(git rev-parse origin/${BRANCH})
+  echo GIT_SHA=$GIT_SHA > env.properties
+fi
+
