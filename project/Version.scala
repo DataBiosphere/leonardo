109f160ae65b0f1151bae8de180533e4b9bd0024
aweng98 <35533885+aweng98@users.noreply.github.com>
2018-05-17 11:31:04 -0400

    update lib versions (#376)
    * update lib versions
    
    * workbench-util ver

--- project/Version.scala
+++ project/Version.scala
@@ -6,12 +6,11 @@ object Version {
   val baseVersion = "0.1"
 
   def getVersionString = {
-    def getLastCommitFromGit = { s"""git rev-parse --short HEAD""" !! }
 
     // either specify git model hash as an env var or derive it
     // if building from the broadinstitute/scala-baseimage docker image use env var
     // (scala-baseimage doesn't have git in it)
-    val lastCommit = sys.env.getOrElse("GIT_HASH", getLastCommitFromGit ).trim()
+    val lastCommit = sys.env.getOrElse("GIT_HASH", sys.process.Process("git rev-parse --short HEAD")!! ).trim()
     val version = baseVersion + "-" + lastCommit
 
     // The project isSnapshot string passed in via command line settings, if desired.
