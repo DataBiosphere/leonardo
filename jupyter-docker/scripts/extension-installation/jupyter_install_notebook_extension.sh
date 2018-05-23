eb292cc350033dacd4af3daa896580fee18d6620
Rob Title <rtitle@broadinstitute.org>
2018-05-22 22:03:26 -0400

    pip install as root, jupyter commands as jupyter-user

--- jupyter-docker/scripts/extension-installation/jupyter_install_notebook_extension.sh
+++ jupyter-docker/scripts/extension-installation/jupyter_install_notebook_extension.sh
@@ -6,6 +6,6 @@ if [ -n "$1" ]; then
   JUPYTER_EXTENSION_NAME=`basename ${JUPYTER_EXTENSION%%.*}`
   mkdir ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   tar -xzf ${JUPYTER_EXTENSION} -C${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
-  jupyter nbextension install ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}/ --user
-  jupyter nbextension enable ${JUPYTER_EXTENSION_NAME}/main
+  sudo -u jupyter-user jupyter nbextension install ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}/ --user
+  sudo -u jupyter-user jupyter nbextension enable ${JUPYTER_EXTENSION_NAME}/main
 fi
