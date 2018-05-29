dc9638ed017634ab12636b5cbc67c1753760cb45
Rob Title <rtitle@broadinstitute.org>
2018-05-29 18:23:04 -0400

    Log jupyter server output to a file

--- src/main/resources/jupyter/init-actions.sh
+++ src/main/resources/jupyter/init-actions.sh
@@ -37,7 +37,7 @@ fi
 if [[ "${ROLE}" == 'Master' ]]; then
     JUPYTER_HOME=/etc/jupyter
     JUPYTER_USER_HOME=/home/jupyter-user
-    JUPYTER_NOTEBOOK="/usr/local/bin/jupyter notebook"
+    JUPYTER_NOTEBOOK_CMD="/usr/local/bin/jupyter notebook > ${JUPYTER_USER_HOME}/jupyter.log 2>&1"
     KERNELSPEC_HOME=/usr/local/share/jupyter/kernels
 
     # The following values are populated by Leo when a cluster is created.
