95fdfec085e03767c7026e2a641c48a44d7b812a
Robert Title <rtitle@broadinstitute.org>
2018-05-31 15:23:46 -0400

    Rt init scripts in foreground (#393)
    * Install init scripts in the foreground
    
    * Do even less in the background
    
    * Don't install extensions as root
    
    * pip install as root, jupyter commands as jupyter-user
    
    * Install sudo
    
    * Log jupyter server output to a file
    
    * Fix typo
    
    * Fix redirect
    
    * Forgot to do 1 of them as root
    
    * Actually don't install it as root
    
    * Use nifty bash trick
    
    * Preserve environment variables in sudo
    
    * --user
    
    * Add mlr test, it works
    
    * Revert "Add mlr test, it works"
    
    This reverts commit 8e33738b808c1493d25dac76485bc08841201774.

--- jupyter-docker/scripts/extension-installation/jupyter_install_combined_extension.sh
+++ jupyter-docker/scripts/extension-installation/jupyter_install_combined_extension.sh
@@ -8,7 +8,7 @@ if [ -n "$1" ]; then
   mkdir ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   tar -xzf ${JUPYTER_EXTENSION} -C${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   pip install -e ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
-  jupyter serverextension enable --py ${JUPYTER_EXTENSION_NAME}
-  jupyter nbextension install --py ${JUPYTER_EXTENSION_NAME}
-  jupyter nbextension enable --py ${JUPYTER_EXTENSION_NAME}
+  sudo -E -u jupyter-user jupyter serverextension enable --py ${JUPYTER_EXTENSION_NAME}
+  sudo -E -u jupyter-user jupyter nbextension install --py ${JUPYTER_EXTENSION_NAME} --user
+  sudo -E -u jupyter-user jupyter nbextension enable --py ${JUPYTER_EXTENSION_NAME}
 fi
