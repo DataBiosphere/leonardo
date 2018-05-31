bd2be14fb07d8b69b4ec61057e67688462684f5d
Robert Title <rtitle@broadinstitute.org>
2018-05-31 15:23:46 -0400

    Rt init scripts in foreground (#393)
    * Install init scripts in the foreground
    
    * Do even less in the background
    
    * Don't install extensions as root
    
    * pip install as root, jupyter commands as jupyter-user
    
    * Install sudo
    
    * Log jupyter server output to a file
    
    * Fix typo
    
    * Fix redirect
    
    * Forgot to do 1 of them as root
    
    * Actually don't install it as root
    
    * Use nifty bash trick
    
    * Preserve environment variables in sudo
    
    * --user
    
    * Add mlr test, it works
    
    * Revert "Add mlr test, it works"
    
    This reverts commit 8e33738b808c1493d25dac76485bc08841201774.

--- jupyter-docker/scripts/extension-installation/jupyter_pip_install_server_extension.sh
+++ jupyter-docker/scripts/extension-installation/jupyter_pip_install_server_extension.sh
@@ -5,5 +5,5 @@ set -e
 if [ -n "$1" ]; then
   JUPYTER_EXTENSION=$1
   pip install ${JUPYTER_EXTENSION}
-  jupyter serverextension enable --py ${JUPYTER_EXTENSION}
+  sudo -E -u jupyter-user jupyter serverextension enable --py ${JUPYTER_EXTENSION}
 fi
