0127df2a02ca23a7a52c278490554fe9638d8e0a
Rob Title <rtitle@broadinstitute.org>
2018-05-31 11:22:31 -0400

    --user

--- jupyter-docker/scripts/extension-installation/jupyter_install_combined_extension.sh
+++ jupyter-docker/scripts/extension-installation/jupyter_install_combined_extension.sh
@@ -9,6 +9,6 @@ if [ -n "$1" ]; then
   tar -xzf ${JUPYTER_EXTENSION} -C${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   pip install -e ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
   sudo -E -u jupyter-user jupyter serverextension enable --py ${JUPYTER_EXTENSION_NAME}
-  sudo -E -u jupyter-user jupyter nbextension install --py ${JUPYTER_EXTENSION_NAME}
+  sudo -E -u jupyter-user jupyter nbextension install --py ${JUPYTER_EXTENSION_NAME} --user
   sudo -E -u jupyter-user jupyter nbextension enable --py ${JUPYTER_EXTENSION_NAME}
 fi
