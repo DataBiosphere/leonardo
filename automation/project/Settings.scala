a109b9888e7a75148b48912d85e1a8ea95267369
jmthibault79 <thibault@broadinstitute.org>
2018-06-15 14:22:16 -0400

    Update Scala version to 2.12 (#428)

--- automation/project/Settings.scala
+++ automation/project/Settings.scala
@@ -32,7 +32,7 @@ object Settings {
   val commonSettings =
     commonBuildSettings ++ testSettings ++ List(
     organization  := "org.broadinstitute.dsde.firecloud",
-    scalaVersion  := "2.11.8",
+    scalaVersion  := "2.12.6",
     resolvers ++= commonResolvers,
     scalacOptions ++= commonCompilerSettings
   )
