17fba780cbc2441dba7e5744d6972da0400d113c
jmthibault79 <thibault@broadinstitute.org>
2018-06-18 16:34:33 -0400

    Enable independent config values (#430)

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoConfig.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoConfig.scala
@@ -1,12 +1,18 @@
 package org.broadinstitute.dsde.workbench.leonardo
 
-import org.broadinstitute.dsde.workbench.config.Config
+import org.broadinstitute.dsde.workbench.config.CommonConfig
 
-object LeonardoConfig extends Config {
-  private val leonardo = Config.config.getConfig("leonardo")
+object LeonardoConfig extends CommonConfig {
+  private val leonardo = config.getConfig("leonardo")
 
   object Leonardo {
     val apiUrl: String = leonardo.getString("apiUrl")
     val notebooksServiceAccountEmail: String = leonardo.getString("notebooksServiceAccountEmail")
   }
+
+  // for qaEmail and pathToQAPem
+  object GCS extends CommonGCS
+
+  // for NotebooksWhitelisted
+  object Users extends CommonUsers
 }
