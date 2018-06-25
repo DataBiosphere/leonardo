22d970261444f29c105fce4202a1c09818ad5e20
Rob Title <rtitle@broadinstitute.org>
2018-06-25 10:18:30 -0400

    Add newline

--- jupyter-docker/scripts/run-jupyter.sh
+++ jupyter-docker/scripts/run-jupyter.sh
@@ -7,4 +7,5 @@ JUPYTER_BASE="/usr/bin/python /usr/local/bin/jupyter-notebook"
 JUPYTER_CMD="$JUPYTER_BASE --NotebookApp.nbserver_extensions=\"{'jupyter_localize_extension':True}\" &> ${HOME}/jupyter.log"
 
 echo $JUPYTER_CMD
+
 eval $JUPYTER_CMD
