3b784ae5fdaac423bc88aae6f2fbef2bafecbbc5
Kaan Yuksel <kyuksel@broadinstitute.org>
2018-06-27 11:33:41 -0400

    Removed remaining assertEquivalent's

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/TestComponent.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/TestComponent.scala
@@ -3,10 +3,9 @@ package org.broadinstitute.dsde.workbench.leonardo.db
 import java.util.UUID
 
 import org.broadinstitute.dsde.workbench.leonardo.TestExecutionContext
-import org.broadinstitute.dsde.workbench.leonardo.model.Cluster
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Seconds, Span}
-import org.scalatest.{Assertion, Matchers}
+import org.scalatest.Matchers
 import slick.dbio.DBIO
 import slick.jdbc.JdbcProfile
 
@@ -34,26 +33,5 @@ trait TestComponent extends Matchers with ScalaFutures with LeoComponent {
 
   protected def getClusterId(googleId: Option[UUID]): Long = {
     dbFutureValue { _.clusterQuery.getIdByGoogleId(googleId) }.get
-  }
-
-  // Equivalence means clusters have the same fields when ignoring the id field
-  protected def assertEquivalent(cs1: Set[Cluster])(cs2: Set[Cluster]): Assertion = {
-    val FixedId = 0
-
-    val (cs1WithFixedId, cs2WithFixedId) = setClusterId(cs1, cs2, FixedId)
-
-    cs1WithFixedId shouldEqual cs2WithFixedId
-  }
-
-  protected def assertEquivalent(c1: Cluster)(c2: Cluster): Assertion = {
-    assertEquivalent(Set(c1))(Set(c2))
-  }
-
-  // Set all clusters' ids to the given value
-  private[db] def setClusterId(cs1: Set[Cluster], cs2: Set[Cluster], fixedId: Long): (Set[Cluster], Set[Cluster]) = {
-    val cs1WithFixedId = cs1 map { _.copy(id = fixedId) }
-    val cs2WithFixedId = cs2 map { _.copy(id = fixedId) }
-
-    (cs1WithFixedId, cs2WithFixedId)
   }
 }
