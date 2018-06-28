39c2fdcb30e9a759f271ccf629a603c319b62a3e
Evan Parker <evanp@google.com>
2018-06-27 23:33:54 -0600

    UI: refresh cluster card when state is in flux.

--- ui/src/components/ClusterCard.js
+++ ui/src/components/ClusterCard.js
@@ -11,6 +11,24 @@ import DeleteDialog from './DeleteDialog'
 import StatusIcon from './StatusIcon'
 
 
+// The following statuses indicate that the status
+// of the cluster is likely to change in the near
+// future.
+const statusInFlux = [
+  "Creating",
+  "Starting",
+  "Stopping",
+  "Updating",
+  "Deleting",
+]
+
+const unknownStatus = "Unknown";
+
+
+const statusCheckInterval = 30000;  // 30 seconds.
+const maxStatusChecks = 30; // 30 checks, or, 15 minutes.
+
+
 const styles = {
   card: {
     minWidth: 300,
@@ -36,7 +54,7 @@ const styles = {
 
 /**
  * @props errorHandler function callback displays a string as a dismissable error.
- * @props googleToken string access token provided by oauth login.
+ * @props googleAuthToken string access token provided by oauth login.
  * @props oauthClientId string oauth client used for app auth.
  * @props clusterModel object contains a cluster model returned by Leonardo's API.
  */
@@ -46,22 +64,73 @@ class ClusterCard extends React.Component {
     super(props);
     this.state = {
       clusterStatus: this.props.clusterModel.status,
+      statusFetches: 0
     };
-  }
-
-  setClusterStatusRunning = () => {
-    this.setState({ clusterStatus: "Running" });
-  }
-
-  setClusterStatusError = () => {
-    this.setState({ clusterStatus: "Error" });
   }
 
   setClusterStatusDeleting = () => {
     this.setState({ clusterStatus: "Deleting" });
   }
 
+  /**
+   * Refresh cluster status if there have been fewer than `maxStatusChecks` prior refreshes.
+   */
+  tryRefreshingClusterStatus = () => {
+    // Exit early if too many checks have been done. Set status to unknown.
+    if (this.state.statusFetches > maxStatusChecks) {
+      this.setState({clusterStatus: unknownStatus});
+      return
+    }
+    // Path to fetch cluster json.
+    var getPath = '/api/cluster/' + this.props.clusterModel.googleProject + '/' + this.props.clusterModel.clusterName;
+    // Begin the GET request and register callbacks.
+    return fetch(
+      getPath,
+      {
+        method: "GET",
+        headers: {
+          "Authorization": "Bearer " + this.props.googleAuthToken
+        },
+        credentials: "include"
+      }
+    )
+    // Validate response content type is application/json.
+    .then((response) => {
+      if (response.headers.get("content-type").indexOf("application/json") <= -1) {
+        console.log("List cluster response not of type 'application/json'")
+        console.log(response);
+        this.setState({clusterStatus: unknownStatus, statusFetches: 0});
+      }
+      return response
+    })
+    // Validate response status and throw if not in 200s.
+    .then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        console.log(response);
+        this.setState({clusterStatus: unknownStatus, statusFetches: 0});
+      }
+      return response;
+    })
+    // Get response json object.
+    .then((response) => response.json())
+    // Update the status only if the status is new, otherwise increment the counter.
+    .then((responseJson) => {
+      if (responseJson.status !== this.state.clusterStatus) {
+        this.setState({clusterStatus: responseJson.status, statusFetches: 0});
+      } else {
+        this.setState({statusFetches: this.state.statusFetches + 1});
+      }
+    })
+    // Handle any errors.
+    .catch((error) => this.props.errorHandler(error.toString()));
+  }
+
   render() {
+    if (statusInFlux.indexOf(this.state.clusterStatus) >= 0) {
+      setTimeout(this.tryRefreshingClusterStatus, statusCheckInterval);
+    }
+
+    // Grab variables used for rendering.
     var classes = this.props.classes;
     var model = this.props.clusterModel;
     var machineCfg = model.machineConfig;
