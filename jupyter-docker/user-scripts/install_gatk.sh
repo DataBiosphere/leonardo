f9005fc7c876f82a761f79caeadb7a98525bda3f
abaumann <abaumann@broadinstitute.org>
2018-07-13 12:03:20 -0400

    Added user install script for gatk that can be used on cluster initialization.  Does not deal with any python packages that are needed for certain tools, only deals with the jars.

---
+++ jupyter-docker/user-scripts/install_gatk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+GATK_VERSION=4.0.6.0
+GATK_ZIP_PATH=/tmp/gatk-$GATK_VERSION.zip
+
+#download the gatk zip if it doesn't already exist
+if ! [ -f $GATK_ZIP_PATH ]; then
+  # curl and follow redirects and output to a temp file
+  curl -L -o $GATK_ZIP_PATH https://github.com/broadinstitute/gatk/releases/download/$GATK_VERSION/gatk-$GATK_VERSION.zip
+fi
+
+# unzip with forced overwrite (if necessary) to /bin
+unzip -o $GATK_ZIP_PATH -d /bin/
+
+# make a symlink to gatk right inside bin so it's available from the existing PATH
+ln -s /bin/gatk-4.0.6.0/gatk /bin/gatk
