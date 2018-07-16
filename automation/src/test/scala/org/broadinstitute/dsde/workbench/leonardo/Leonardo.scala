74068cec452854d0b6c4370e8596eb526df208fc
vkumra-broad <vkumra@broadinstitute.org>
2018-07-16 16:30:24 -0400

    Issue 449: Leo not working in an iframe (#479)

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/Leonardo.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/Leonardo.scala
@@ -6,6 +6,7 @@ import java.util.UUID
 
 import akka.http.scaladsl.model.headers.{Authorization, Cookie, HttpCookiePair, OAuth2BearerToken}
 import akka.Done
+import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.server.Route
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.databind.DeserializationFeature
@@ -147,25 +148,34 @@ object Leonardo extends RestClient with LazyLogging {
       mapper.readValue(response, classOf[ContentItem])
     }
 
-    def notebooksPath(googleProject: GoogleProject, clusterName: ClusterName): String =
+    def notebooksBasePath(googleProject: GoogleProject, clusterName: ClusterName): String =
       s"notebooks/${googleProject.value}/${clusterName.string}"
 
+    def notebooksTreePath(googleProject: GoogleProject, clusterName: ClusterName): String =
+      s"${notebooksBasePath(googleProject, clusterName)}/tree"
+
     def contentsPath(googleProject: GoogleProject, clusterName: ClusterName, contentPath: String): String =
-      s"${notebooksPath(googleProject, clusterName)}/api/contents/$contentPath"
+      s"${notebooksBasePath(googleProject, clusterName)}/api/contents/$contentPath"
 
     def localizePath(googleProject: GoogleProject, clusterName: ClusterName, async: Boolean = false): String =
-      s"${notebooksPath(googleProject, clusterName)}/api/localize${if (async) "?async=true" else ""}"
+      s"${notebooksBasePath(googleProject, clusterName)}/api/localize${if (async) "?async=true" else ""}"
 
     def get(googleProject: GoogleProject, clusterName: ClusterName)(implicit token: AuthToken, webDriver: WebDriver): NotebooksListPage = {
-      val path = notebooksPath(googleProject, clusterName)
+      val path = notebooksBasePath(googleProject, clusterName)
       logger.info(s"Get notebook: GET /$path")
       new NotebooksListPage(url + path)
     }
 
     def getApi(googleProject: GoogleProject, clusterName: ClusterName)(implicit token: AuthToken): String = {
-      val path = notebooksPath(googleProject, clusterName)
+      val path = notebooksBasePath(googleProject, clusterName)
       logger.info(s"Get notebook: GET /$path")
       parseResponse(getRequest(url + path))
+    }
+
+    def getApiHeaders(googleProject: GoogleProject, clusterName: ClusterName)(implicit token: AuthToken): Seq[HttpHeader] = {
+      val path = notebooksTreePath(googleProject, clusterName)
+      logger.info(s"Get notebook: GET /$path")
+      getRequest(url + path).headers
     }
 
     def localize(googleProject: GoogleProject, clusterName: ClusterName, locMap: Map[String, String], async: Boolean = false)(implicit token: AuthToken): String = {
@@ -183,7 +193,7 @@ object Leonardo extends RestClient with LazyLogging {
     }
 
     def setCookie(googleProject: GoogleProject, clusterName: ClusterName)(implicit token: AuthToken, webDriver: WebDriver): String = {
-      val path = notebooksPath(googleProject, clusterName) + "/setCookie"
+      val path = notebooksBasePath(googleProject, clusterName) + "/setCookie"
       logger.info(s"Set cookie: GET /$path")
       parseResponse(getRequest(url + path, httpHeaders = List(Authorization(OAuth2BearerToken(token.value)))))
 
