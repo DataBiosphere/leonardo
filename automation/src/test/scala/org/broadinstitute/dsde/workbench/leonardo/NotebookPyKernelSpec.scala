9bfe867871fa04228766018b0e7df272b8ca5c0b
Azmina Karukappadath <akarukap@broadinstitute.org>
2018-07-31 16:08:45 -0400

    finalizing

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/NotebookPyKernelSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/NotebookPyKernelSpec.scala
@@ -82,7 +82,7 @@ class NotebookPyKernelSpec extends ClusterFixtureSpec {
         withWebDriver { implicit driver =>
           withNewNotebook(clusterFixture.cluster, kernel) { notebookPage =>
             //all other packages cannot be tested for their versions in this manner
-            //warnings about numpy are ignored because they are benign warnings that show up for python2 because of compilation against an older numpy
+            //warnings are ignored because they are benign warnings that show up for python2 because of compilation against an older numpy
             notebookPage.executeCell("import warnings; warnings.simplefilter('ignore')\nfrom google.cloud import bigquery\nprint(bigquery.__version__)") shouldBe Some("1.4.0")
             notebookPage.executeCell("from google.cloud import datastore\nprint(datastore.__version__)") shouldBe Some("1.7.0")
             notebookPage.executeCell("from google.cloud import storage\nprint(storage.__version__)") shouldBe Some("1.10.0")
