6674bc2fb90828c3e21ed26a1e3551f6b9b4dfd4
Azmina Karukappadath <akarukap@broadinstitute.org>
2018-08-27 14:13:24 -0400

    preliminary changes

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/LabelComponentSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/LabelComponentSpec.scala
@@ -16,41 +16,8 @@ import scala.util.Random
 class LabelComponentSpec extends TestComponent with FlatSpecLike with CommonTestData with GcsPathUtils {
 
   "LabelComponent" should "save, get,and delete" in isolatedDbTest {
-    val cluster1 = Cluster(
-      clusterName = name1,
-      googleProject = project,
-      serviceAccountInfo = ServiceAccountInfo(None, Some(serviceAccountEmail)),
-      dataprocInfo = DataprocInfo(Option(UUID.randomUUID()), Option(OperationName("op1")), Some(GcsBucketName("testStagingBucket1")), None),
-      auditInfo = AuditInfo(userEmail, Instant.now(), Option(Instant.now()), Instant.now()),
-      machineConfig = MachineConfig(Some(0),Some(""), Some(500)),
-      clusterUrl = Cluster.getClusterUrl(project, name1, clusterUrlBase),
-      status = ClusterStatus.Creating,
-      labels = Map.empty,
-      jupyterExtensionUri = Some(jupyterExtensionUri),
-      jupyterUserScriptUri = Some(jupyterUserScriptUri),
-      errors = List.empty,
-      instances = Set.empty,
-      userJupyterExtensionConfig = None,
-      autopauseThreshold = 0,
-      defaultClientId = None)
-
-    val cluster2 = Cluster(
-      clusterName = name2,
-      googleProject = project,
-      serviceAccountInfo = ServiceAccountInfo(None, Some(serviceAccountEmail)),
-      dataprocInfo = DataprocInfo(Option(UUID.randomUUID()), Option(OperationName("op2")), Some(GcsBucketName("testStagingBucket2")), Some(IP("sure, this is an IP address"))),
-      auditInfo = AuditInfo(userEmail, Instant.now(), None, Instant.now()),
-      machineConfig = MachineConfig(Some(0),Some(""), Some(500)),
-      clusterUrl = Cluster.getClusterUrl(project, name2, clusterUrlBase),
-      status = ClusterStatus.Unknown,
-      labels = Map.empty,
-      jupyterExtensionUri = None,
-      jupyterUserScriptUri = None,
-      errors = List.empty,
-      instances = Set.empty,
-      userJupyterExtensionConfig = None,
-      autopauseThreshold = 0,
-      defaultClientId = None)
+    val savedCluster1 = makeCluster(1).save()
+    val savedCluster2 = makeCluster(2).save()
 
     val cluster2Map = Map("bam" -> "true", "sample" -> "NA12878")
 
@@ -59,19 +26,11 @@ class LabelComponentSpec extends TestComponent with FlatSpecLike with CommonTest
     dbFutureValue { _.labelQuery.get(missingId, "missing") } shouldEqual None
     dbFailure { _.labelQuery.save(missingId, "key1", "value1") } shouldBe a [SQLException]
 
-    val savedCluster1 = dbFutureValue { _.clusterQuery.save(cluster1, Option(gcsPath("gs://bucket1")), Some(serviceAccountKey.id)) }
-
-    savedCluster1 shouldEqual cluster1
-
     val cluster1Id = savedCluster1.id
 
     dbFutureValue { _.labelQuery.save(cluster1Id, "key1", "value1") } shouldEqual 1
     dbFutureValue { _.labelQuery.getAllForCluster(cluster1Id) } shouldEqual Map("key1" -> "value1")
     dbFutureValue { _.labelQuery.get(cluster1Id, "key1") } shouldEqual Some("value1")
-
-    val savedCluster2 = dbFutureValue { _.clusterQuery.save(cluster2, Option(gcsPath("gs://bucket2")), Some(serviceAccountKey.id)) }
-
-    savedCluster2 shouldEqual cluster2
 
     val cluster2Id = savedCluster2.id
 
