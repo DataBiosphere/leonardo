86e08fb1887d20ccb065e9310e839bed30268f23
Matt Bemis <MatthewBemis@users.noreply.github.com>
2018-10-17 16:27:28 -0400

    #625: Make Dataproc init script timeout configurable (#628)
    * add defaultExecutionTimeout from config
    
    * set the timeout to 30 mins in reference.conf, can override in leo.conf

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/DataprocConfig.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/DataprocConfig.scala
@@ -2,12 +2,15 @@ package org.broadinstitute.dsde.workbench.leonardo.config
 
 import org.broadinstitute.dsde.workbench.model.google.GoogleProject
 
+import scala.concurrent.duration.FiniteDuration
+
 case class DataprocConfig(
                            applicationName: String,
                            dataprocDefaultRegion: String,
                            leoGoogleProject: GoogleProject,
                            dataprocDockerImage: String,
                            clusterUrlBase: String,
+                           defaultExecutionTimeout: FiniteDuration,
                            jupyterServerName: String,
                            firewallRuleName: String,
                            networkTag: String,
