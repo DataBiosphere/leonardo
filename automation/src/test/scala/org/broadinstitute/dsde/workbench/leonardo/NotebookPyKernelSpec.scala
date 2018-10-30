e54406797b9f9915ab78095f3e9a94e7aa14e3b8
Azmina Karukappadath <akarukap@broadinstitute.org>
2018-10-30 11:16:41 -0400

    working on tests

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/NotebookPyKernelSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/NotebookPyKernelSpec.scala
@@ -86,9 +86,9 @@ class NotebookPyKernelSpec extends ClusterFixtureSpec {
           withNewNotebook(clusterFixture.cluster, kernel) { notebookPage =>
             //all other packages cannot be tested for their versions in this manner
             //warnings are ignored because they are benign warnings that show up for python2 because of compilation against an older numpy
-            notebookPage.executeCell("import warnings; warnings.simplefilter('ignore')\nfrom google.cloud import bigquery\nprint(bigquery.__version__)") shouldBe Some("1.4.0")
+            notebookPage.executeCell("import warnings; warnings.simplefilter('ignore')\nfrom google.cloud import bigquery\nprint(bigquery.__version__)") shouldBe Some("1.6.0")
             notebookPage.executeCell("from google.cloud import datastore\nprint(datastore.__version__)") shouldBe Some("1.7.0")
-            notebookPage.executeCell("from google.cloud import storage\nprint(storage.__version__)") shouldBe Some("1.10.0")
+            notebookPage.executeCell("from google.cloud import storage\nprint(storage.__version__)") shouldBe Some("1.13.0")
           }
         }
       }
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sbt assembly
-gcloud compute scp /Users/akarukap/Documents/firecloud/leonardo/target/scala-2.12/leonardo-assembly-0.1-5f9b556-SNAPSHOT.jar fiab-akarukap-secure-glider:/tmp --zone=us-central1-a
-gcloud compute ssh --project broad-dsde-dev --zone us-central1-a fiab-akarukap-secure-glider | sudo docker exec -it firecloud_leonardo-app_1 ls /leonardo/ | sudo docker exec -it firecloud_leonardo-app_1 rm /leonardo/leonardo-assembly-0.1-5f9b556-SNAPSHOT.jar | sudo docker cp /tmp/leonardo-assembly-0.1-5f9b556-SNAPSHOT.jar firecloud_leonardo-app_1:/leonardo/ | sudo docker restart firecloud_leonardo-app_1 | sudo docker restart firecloud_leonardo-proxy_1
