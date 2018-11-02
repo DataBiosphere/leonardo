2890429af7b469e1fcf41876e0de97340cb5d82a
Matt Bemis <mbemis@broadinstitute.org>
2018-11-02 11:35:04 -0400

    more revert

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/google/HttpGoogleDataprocDAO.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/google/HttpGoogleDataprocDAO.scala
@@ -285,13 +285,12 @@ class HttpGoogleDataprocDAO(appName: String,
         )
     }
 
-    val dataprocProps: Map[String, String] = numWorkers match {
-      case Some(0) =>
-        // Set a SoftwareConfig property that makes the cluster have only one node
-        Map("dataproc:dataproc.allow.zero.workers" -> "true")
-      case _ =>
-        Map.empty
+    val dataprocProps: Map[String, String] = if (numWorkers.get == 0) {
+      // Set a SoftwareConfig property that makes the cluster have only one node
+      Map("dataproc:dataproc.allow.zero.workers" -> "true")
     }
+    else
+      Map.empty
 
     val yarnProps = Map(
       // Helps with debugging
