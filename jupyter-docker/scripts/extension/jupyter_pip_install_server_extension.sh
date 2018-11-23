b64f30fc426fc4dfe3a1928d292745e520adff4b
Rob Title <rtitle@broadinstitute.org>
2018-11-22 21:01:34 -0500

    Remove jupyter-docker directory

--- jupyter-docker/scripts/extension/jupyter_pip_install_server_extension.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [ -n "$1" ]; then
-  JUPYTER_EXTENSION=$1
-  pip3 install ${JUPYTER_EXTENSION}
-  sudo -E -u jupyter-user jupyter serverextension enable --py ${JUPYTER_EXTENSION}
-fi
--- jupyter-docker/user-scripts/uninstall_spark_kernels.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-jupyter kernelspec uninstall -f pyspark2
-jupyter kernelspec uninstall -f pyspark3
