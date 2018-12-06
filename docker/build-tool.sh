c7eb98394e87297177247fd3a92310a0f2102df6
Robert Title <rtitle@broadinstitute.org>
2018-12-06 12:26:33 -0500

    RStudio Phase 1 (#690)
    * Add rstudio image and update build scripts
    
    * Remove jupyter-docker directory
    
    * Add rserver.conf and listen on port 8001
    
    * Dockerfire changes didn't carry over in git mv
    
    * Clean up
    
    * 1 more cleanup
    
    * Update rstudio docker and add some README's
    
    * Fix image name in build-tool.sh
    
    * bash fix
    
    * Better scheme for backwards compatible jupyter images
    
    * quote bash vars

---
+++ docker/build-tool.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+# Ensure that commands are run from this directory.
+cd "$(dirname "${0}")"
+
+HELP_TEXT="$(cat <<EOF
+
+ Builds a tool docker image.
+   -p | --push : (default: only build) if set, pushes the image after build.
+           If unset, only perform a build.
+   -i | --image : the tool image name.
+   -r | --repository : the docker repository to push to.  Can be a dockerhub or GCR repo.
+   -t | --tag : the docker tag used for the images.
+   -b | --branch : (default: none) the git branch
+   -h | --help: print help text.
+
+ Examples:
+   Builds and pushes the Jupyter docker image
+    ./docker/build-tool.sh --push -i jupyter -r us.gcr.io/broad-dsp-gcr-public -t 12345 -b my-branch
+   Builds the RStudio docker image tagged with the git branch name
+    ./docker/build-tool.sh -i rstudio -r us.gcr.io/broad-dsp-gcr-public -t my-tag
+\t
+EOF
+)"
+
+# Enable strict evaluation semantics.
+set -e
+
+# Set default variables used while parsing command line options.
+PRINT_HELP=false
+PUSH=false
+IMAGE=""
+REPOSITORY=""
+DOCKER_TAG=""
+BRANCH=""
+
+if [ -z "$1" ]; then
+    echo "No argument supplied!"
+    echo "run '${0} -h' to see available arguments."
+    exit 1
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --push)
+            echo "will perform a PUSH"
+            PUSH=true
+            ;;
+        -i | --image)
+            shift
+            IMAGE=$1
+            ;;
+        -r | --repository)
+            shift
+            REPOSITORY=$1
+            ;;
+        -t | --tag)
+            shift
+            DOCKER_TAG=$1
+            ;;
+        -b | --branch)
+            shift
+            BRANCH=$1
+            ;;
+        -h | --help)
+            PRINT_HELP=true
+            ;;
+        *)
+            echo "Unrecognized argument '${1}'."
+            echo "run '${0} -h' to see available arguments."
+            if grep -Fq "=" <<< "${1}"; then
+                echo "note: separate args from flags with a space, not '='."
+            fi
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ -z "$IMAGE" ]; then
+    echo "image is required"
+    PRINT_HELP=true
+fi
+
+if [ -z "$REPOSITORY" ]; then
+    echo "repository is required"
+    PRINT_HELP=true
+fi
+
+if [ -z "$REPOSITORY" ]; then
+    echo "tag is required"
+    PRINT_HELP=true
+fi
+
+# Print help after all flags are parsed successfully
+if $PRINT_HELP; then
+  echo -e "${HELP_TEXT}"
+  exit 0
+fi
+
+GIT_BRANCH="${BRANCH:-$(git rev-parse --abbrev-ref HEAD)}"
+
+# Set up docker binary - use gcloud docker if pushing to gcr.
+DOCKER_BINARY="docker"
+if grep -Fq "gcr.io" <<< "${REPOSITORY}" ; then
+	DOCKER_BINARY="gcloud docker --"
+fi
+
+
+function build() {
+    echo "building leonardo-$IMAGE docker image..."
+    $DOCKER_BINARY build -t "${REPOSITORY}/leonardo-${IMAGE}:${DOCKER_TAG}" $IMAGE
+}
+
+function push() {
+    echo "pushing leonardo-$IMAGE docker image..."
+    $DOCKER_BINARY push "${REPOSITORY}/leonardo-${IMAGE}:${DOCKER_TAG}"
+    $DOCKER_BINARY tag "${REPOSITORY}/leonardo-${IMAGE}:${DOCKER_TAG}" "${REPOSITORY}/leonardo-${IMAGE}:${GIT_BRANCH}"
+    $DOCKER_BINARY push "${REPOSITORY}/leonardo-${IMAGE}:${GIT_BRANCH}"
+
+    # For backwards compatibility; also tag and push jupyter images named 'leonardo-notebooks'.
+    # Remove this when firecloud-develop leonardo.conf is updated to reference 'leonardo-jupyter'.
+    if [ "$IMAGE" == "jupyter" ]; then
+        $DOCKER_BINARY tag "${REPOSITORY}/leonardo-${IMAGE}:${DOCKER_TAG}" "${REPOSITORY}/leonardo-notebooks:${DOCKER_TAG}"
+        $DOCKER_BINARY push "${REPOSITORY}/leonardo-notebooks:${DOCKER_TAG}"
+        $DOCKER_BINARY tag "${REPOSITORY}/leonardo-${IMAGE}:${GIT_BRANCH}" "${REPOSITORY}/leonardo-notebooks:${GIT_BRANCH}"
+        $DOCKER_BINARY push "${REPOSITORY}/leonardo-notebooks:${GIT_BRANCH}"
+    fi
+}
+
+build
+if $PUSH; then
+    push
+fi
---
+++ docker/jupyter/README.md
@@ -0,0 +1,56 @@
+# Jupyter Docker Image
+
+## Base image
+* [debian:stretch](https://hub.docker.com/r/library/debian/)
+
+## Applications
+* [Jupyter Notebooks](https://jupyter-notebook.readthedocs.io/en/stable/)
+* [JupyterLab](https://jupyterlab.readthedocs.io/en/latest/)
+* [Hail 0.2](https://hail.is/docs/0.2/index.html)
+
+## Kernels
+* Python 2.7
+* Python 3.6
+* R 3.5
+
+## Libraries
+Warning: may be incomplete! Consult the Dockerfile for the source of truth.
+* common
+   * google-cloud-sdk
+   * Unix utilities
+* python
+   * google-cloud-*
+   * numpy
+   * pandas
+   * seaborn
+   * firecloud-api
+   * scikit-learn
+   * statsmodels
+   * ggplot
+   * bokeh
+   * pyfasta
+   * pdoc
+   * biopython
+   * bx-python
+   * fastinterval
+   * matplotlib-venn
+* R
+   * evaluate
+   * pbdZMQ
+   * devtools
+   * uuid
+   * reshape2
+   * bigrquery
+   * googleCloudStorageR
+   * tidyverse
+   * DataBiosphere/Ronaldo
+    
+## Extensions
+* jupyter_localize_extension.py
+   * Server-side extension to provide the `/localize` endpoint to localize/delocalize files between
+     GCS and the Leo cluster.
+* jupyter_delocalize.py
+   * Provides a custom Jupyter [ContentsManager](https://jupyter-notebook.readthedocs.io/en/stable/extending/contents.html)
+     which persists notebooks to a configurable GCS location upon save.
+* google_sign_in.js
+   * Front-end extension which keeps the user signed into Google while using a notebook.
