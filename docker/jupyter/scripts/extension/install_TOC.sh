86c705bff0fdb422d9375f28fa5cfff23a036aa1
vkumra-broad <vkumra@broadinstitute.org>
2018-12-18 12:03:12 -0500

    add code to install script

--- docker/jupyter/scripts/extension/install_TOC.sh
+++ docker/jupyter/scripts/extension/install_TOC.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+set -e
+
+pip3 install jupyter_contrib_nbextensions jupyter_nbextensions_configurator
+sudo -E -u jupyter-user jupyter nbextensions_configurator enable --user
+#sudo -E -u jupyter-user jupyter nbextension install toc2 --user
+sudo -E -u jupyter-user jupyter nbextension enable toc2/main
