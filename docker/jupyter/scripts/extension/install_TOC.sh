eddfc17816e3b118286f456476089f6de29ac452
vkumra-broad <vkumra@broadinstitute.org>
2019-01-04 14:27:04 -0500

    install extension and configurator

--- docker/jupyter/scripts/extension/install_TOC.sh
+++ docker/jupyter/scripts/extension/install_TOC.sh
@@ -4,5 +4,5 @@ set -e
 
 pip3 install jupyter_contrib_nbextensions jupyter_nbextensions_configurator
 sudo -E -u jupyter-user jupyter nbextensions_configurator enable --user
-#sudo -E -u jupyter-user jupyter nbextension install toc2 --user
+sudo -E -u jupyter-user jupyter contrib nbextension install --user
 sudo -E -u jupyter-user jupyter nbextension enable toc2/main
