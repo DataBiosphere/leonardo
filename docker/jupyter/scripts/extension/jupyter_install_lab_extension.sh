52d6519b2dd03833d77f15f92a2c24d4b8b5c929
Azmina Karukappadath <akarukap@broadinstitute.org>
2019-01-17 10:10:47 -0500

    all donegit add .

--- docker/jupyter/scripts/extension/jupyter_install_lab_extension.sh
+++ docker/jupyter/scripts/extension/jupyter_install_lab_extension.sh
@@ -2,9 +2,9 @@
 
 set -e
 
+
 if [ -n "$1" ]; then
   JUPYTER_EXTENSION=$1
-  JUPYTER_EXTENSION_NAME=`basename ${JUPYTER_EXTENSION%%.*}`
-  tar -zxvf ${JUPYTER_EXTENSION} -C ${JUPYTER_HOME}
-  jupyter labextension install ${JUPYTER_HOME}/${JUPYTER_EXTENSION_NAME}
+  jupyter labextension install $JUPYTER_EXTENSION
 fi
+
