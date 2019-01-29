ba52be08e757250b6e4a95924dee2a4ddcb0331f
Robert Title <rtitle@broadinstitute.org>
2019-01-29 08:14:00 -0500

    Build python3.6 from source (#748)
    * Build python3 from source
    
    * path change
    
    * Update Dockerfile
    
    * Fix python install path
    
    * More sane init-actions.sh
    
    * Add ldconfig

--- docker/jupyter/scripts/run-jupyter.sh
+++ docker/jupyter/scripts/run-jupyter.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# Forces python 2
-JUPYTER_BASE="/usr/bin/python3 /usr/local/bin/jupyter-notebook"
+# Forces python 3
+JUPYTER_BASE="/usr/local/bin/python3 /usr/local/bin/jupyter-notebook"
 JUPYTER_CMD="$JUPYTER_BASE --NotebookApp.nbserver_extensions=\"{'jupyter_localize_extension':True}\" &> ${HOME}/jupyter.log"
 
 echo $JUPYTER_CMD
