ca4651a945ddd8a9543dbac5d84288f5f95c0b59
vkumra-broad <vkumra@broadinstitute.org>
2019-02-04 10:25:26 -0500

    pr feedback

--- src/main/resources/jupyter/init-actions.sh
+++ src/main/resources/jupyter/init-actions.sh
@@ -338,7 +338,10 @@ export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
 echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo apt-get update
-sudo apt-get install -y -q gcsfuse
-
 log "Finished installing Python $PYTHON_VERSION"
+
+log "Starting GCSFuse installation"
+retry 5 betterAptGet
+retry 5 apt-get install -y -q gcsfuse
+log "GCSFuse Installed"
+
