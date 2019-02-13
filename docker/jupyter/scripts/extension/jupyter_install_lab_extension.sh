ebfe4b17391e51b33686b08cd4731520cddfbee4
Azmina Karukappadath <akarukap@broadinstitute.org>
2019-02-13 15:03:56 -0500

    yay

--- docker/jupyter/scripts/extension/jupyter_install_lab_extension.sh
+++ docker/jupyter/scripts/extension/jupyter_install_lab_extension.sh
@@ -6,12 +6,14 @@ set -e
 if [ -n "$1" ]; then
   JUPYTER_EXTENSION=$1
   if [[ ${JUPYTER_EXTENSION} == *'.js' ]]; then
+    # use jupyterlab extension template to create an extension using the specified JS file
+    # see https://github.com/jupyterlab/extension-cookiecutter-js
     JUPYTER_EXTENSION_NAME=`basename ${JUPYTER_EXTENSION%%.*}`
-    cookiecutter --no-input https://github.com/jupyterlab/extension-cookiecutter-ts extension_name=${JUPYTER_EXTENSION_NAME}
-    cp ${JUPYTER_EXTENSION} ${JUPYTER_EXTENSION_NAME}/lib/plugin.js
+    cookiecutter --no-input https://github.com/jupyterlab/extension-cookiecutter-js extension_name=${JUPYTER_EXTENSION_NAME}
+    cp -f ${JUPYTER_EXTENSION} ${JUPYTER_EXTENSION_NAME}/lib/plugin.js
     cd ${JUPYTER_EXTENSION_NAME}
     jlpm
-    jupyter labextension install . --no-build
+    jupyter labextension install .
   else
     jupyter labextension install $JUPYTER_EXTENSION
   fi
