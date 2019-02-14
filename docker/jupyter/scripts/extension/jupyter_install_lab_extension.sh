8e6eeb1fd051861b0e06ba7536b1d1cbffe8d8a9
Azmina Karukappadath <akarukap@broadinstitute.org>
2019-02-14 11:46:20 -0500

    ts support

--- docker/jupyter/scripts/extension/jupyter_install_lab_extension.sh
+++ docker/jupyter/scripts/extension/jupyter_install_lab_extension.sh
@@ -14,6 +14,14 @@ if [ -n "$1" ]; then
     cd ${JUPYTER_EXTENSION_NAME}
     jlpm
     jupyter labextension install .
+  elif [[ ${JUPYTER_EXTENSION} == *'.ts' ]]; then
+    # same as above but in typescript, see https://github.com/jupyterlab/extension-cookiecutter-ts
+    JUPYTER_EXTENSION_NAME=`basename ${JUPYTER_EXTENSION%%.*}`
+    cookiecutter --no-input https://github.com/jupyterlab/extension-cookiecutter-ts extension_name=${JUPYTER_EXTENSION_NAME}
+    cp -f ${JUPYTER_EXTENSION} ${JUPYTER_EXTENSION_NAME}/src/index.ts
+    cd ${JUPYTER_EXTENSION_NAME}
+    jlpm
+    jupyter labextension install .
   else
     jupyter labextension install $JUPYTER_EXTENSION
   fi
