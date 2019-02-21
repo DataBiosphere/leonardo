fc7557344597fc7b4add8559231688e16bcb5023
Matt Bemis <mbemis@broadinstitute.org>
2019-02-21 16:28:33 -0500

    discard entity bytes when request fails

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/api/ProxyRoutes.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/api/ProxyRoutes.scala
@@ -51,14 +51,27 @@ trait ProxyRoutes extends UserInfoDirectives with CorsSupport with CookieHelper 
                 // Note ProxyService calls the LeoAuthProvider internally
                 path("api" / "localize") { // route for custom Jupyter server extension
                   complete {
-                    proxyService.proxyLocalize(userInfo, googleProject, clusterName, request)
+                    val proxyFuture = proxyService.proxyLocalize(userInfo, googleProject, clusterName, request)
+                    // we are discarding the request entity here. we have noticed that PUT requests caused by
+                    // saving a notebook when a cluster is stopped correlate perfectly with CPU spikes.
+                    // in that scenario, the requests appear to pile up, causing apache to hog CPU.
+                    proxyFuture.failed.foreach { _ =>
+                      request.entity.discardBytes().future
+                    }
+                    proxyFuture
                   }
                 } ~
                   complete {
-                    proxyService.proxyNotebook(userInfo, googleProject, clusterName, request)
+                    val proxyFuture = proxyService.proxyNotebook(userInfo, googleProject, clusterName, request)
+                    // we are discarding the request entity here. we have noticed that PUT requests caused by
+                    // saving a notebook when a cluster is stopped correlate perfectly with CPU spikes.
+                    // in that scenario, the requests appear to pile up, causing apache to hog CPU.
+                    proxyFuture.failed.foreach { _ =>
+                      request.entity.discardBytes().future
+                    }
+                    proxyFuture
                   }
               }
-            }
       } ~
         // No need to lookup the user or consult the auth provider for this endpoint
         path("invalidateToken") {
