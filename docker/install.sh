7f1a1db716a1ce372fc5bac671fd7b6b4e63350f
Hussein Elgridly <helgridly@users.noreply.github.com>
2019-03-25 14:46:44 -0400

    sbt batch mode so it doesn't hang while building (#837)

--- docker/install.sh
+++ docker/install.sh
@@ -7,8 +7,8 @@ cd $LEONARDO_DIR
 rm -f leonardo*.jar
 
 # Test
-sbt -J-Xms4g -J-Xmx4g test -Dmysql.host=mysql -Dmysql.port=3306
-sbt -J-Xms4g -J-Xmx4g assembly
+sbt -batch -J-Xms4g -J-Xmx4g test -Dmysql.host=mysql -Dmysql.port=3306
+sbt -batch -J-Xms4g -J-Xmx4g assembly
 LEONARDO_JAR=$(find target | grep 'leonardo.*\.jar')
 mv $LEONARDO_JAR .
 sbt clean
