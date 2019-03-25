199506d6e561ad7ac3987fbc106e1d6aba15444a
Hussein Elgridly <hussein@broadinstitute.org>
2019-03-25 12:06:40 -0400

    sbt batch mode so it doesn't hang while building

--- automation/test.sh
+++ automation/test.sh
@@ -5,7 +5,7 @@ echo $SBT_CMD
 
 set -o pipefail
 
-sbt -Djsse.enableSNIExtension=false -Dheadless=true "${SBT_CMD}"
+sbt -batch -Djsse.enableSNIExtension=false -Dheadless=true "${SBT_CMD}"
 TEST_EXIT_CODE=$?
 sbt clean
 
