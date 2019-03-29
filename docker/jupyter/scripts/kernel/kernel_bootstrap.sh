4325dd3b35d65f8ea38ae504357e16ca093ad5f1
Rob Title <rtitle@broadinstitute.org>
2019-03-28 21:35:15 -0400

    Do the bootstrapping in bash

--- docker/jupyter/scripts/kernel/kernel_bootstrap.sh
+++ docker/jupyter/scripts/kernel/kernel_bootstrap.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # This script runs at kernel startup time and sets environment variables for the
 # workspace name and workspace bucket.
 #
@@ -9,15 +11,14 @@
 # It exploits the fact that the CWD of a launching notebook is named after the workspace.
 # If notebooks are ever launched from other directories, this script will break.
 
-import fapi
-import os
-
 # The workspace name is simply the CWD of the running notebook.
-cwd = os.getcwd()
-workspace = os.path.basename(cwd)
-os.environ['WORKSPACE_NAME'] = workspace
+PWD="$(pwd)"
+export WORKSPACE_NAME="$(basename $PWD)"
 
-# Invoke FISS to retrieve the workspace bucket based on the workspace name
-ws = fapi.get_workspace(os.environ['WORKSPACE_NAMESPACE'], os.environ['WORKSPACE_NAME']).json().get('workspace',{})
-if 'bucketName' in ws:
-    os.environ['WORKSPACE_BUCKET'] = ws['bucketName']
+# Parse the .delocalize.json file in the workspace directory to obtain the workspace bucket.
+DELOCALIZE_FILE="$PWD/.delocalize.json"
+if [ -f "$DELOCALIZE_FILE" ]; then
+    export WORKSPACE_BUCKET="$(dirname "$(cat "$DELOCALIZE_FILE" | jq -r '.destination')")"
+fi
+
+exec "$@"
--- docker/jupyter/scripts/kernel/kernelspec.sh
+++ docker/jupyter/scripts/kernel/kernelspec.sh
@@ -14,3 +14,6 @@ sed 's/${PY_VERSION}/3/g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNE
 # Replace the contents of the PySpark kernel scripts
 sed 's/${PY_VERSION}/2/g' ${TMP_KERNELSPEC_DIR}/pyspark_kernelspec.tmpl > ${KERNELSPEC_HOME}/pyspark2/kernel.json
 sed 's/${PY_VERSION}/3/g' ${TMP_KERNELSPEC_DIR}/pyspark_kernelspec.tmpl > ${KERNELSPEC_HOME}/pyspark3/kernel.json
+
+# Replace the contents of the R kernel script
+sed "s/\${JUPYTER_HOME}/${JUPYTER_HOME}/g" ${TMP_KERNELSPEC_DIR}/r_kernelspec.tmpl > ${KERNELSPEC_HOME}/ir/kernel.json
