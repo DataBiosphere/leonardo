94b7b8922945d15984cd9a0255803569aaa81f6f
Azmina Karukappadath <akarukap@broadinstitute.org>
2019-04-17 13:54:11 -0400

    fixing test

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookInstallSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookInstallSpec.scala
@@ -50,8 +50,8 @@ class NotebookInstallSpec extends ClusterFixtureSpec with NotebookTestUtils {
       s"should be able to pip install packages using ${kernel.string}" in { clusterFixture =>
         withWebDriver { implicit driver =>
           withNewNotebook(clusterFixture.cluster, kernel) { notebookPage =>
-            // install pydot
-            pipInstall(notebookPage, kernel, "pydot")
+            // install a random package
+            pipInstall(notebookPage, kernel, "fuzzywuzzy")
             notebookPage.saveAndCheckpoint()
           }
         }
