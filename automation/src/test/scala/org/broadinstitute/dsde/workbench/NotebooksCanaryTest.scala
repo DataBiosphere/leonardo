2072a442e9dd87ecc25e5c638ccc52506094d6d3
jchanbroad <jchan@broadinstitute.org>
2019-04-30 12:04:26 -0400

    added withwebdriver to test localize

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/NotebooksCanaryTest.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/NotebooksCanaryTest.scala
@@ -1,31 +1,41 @@
 package org.broadinstitute.dsde.workbench
 
+import java.util.concurrent.TimeUnit
+
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.config.{Credentials, LeoAuthToken, UserPool}
 import org.broadinstitute.dsde.workbench.fixture.BillingFixtures
 import org.broadinstitute.dsde.workbench.leonardo.notebooks.{Notebook, NotebookTestUtils}
 import org.scalatest.{FreeSpec, Matchers, ParallelTestExecution}
 import org.scalatest.concurrent.ScalaFutures
-import org.broadinstitute.dsde.workbench.ResourceFile
 import org.broadinstitute.dsde.workbench.dao.Google.{googleIamDAO, googleStorageDAO}
 import org.broadinstitute.dsde.workbench.fixture.BillingFixtures
 import org.broadinstitute.dsde.workbench.leonardo.Leonardo.ApiVersion.V2
 import org.broadinstitute.dsde.workbench.leonardo._
-import org.broadinstitute.dsde.workbench.model.google.GcsEntityTypes.Group
-import org.broadinstitute.dsde.workbench.model.google.GcsRoles.Reader
-import org.broadinstitute.dsde.workbench.model.google.{EmailGcsEntity, GcsObjectName, GcsPath, parseGcsPath}
+import org.broadinstitute.dsde.workbench.model.google.{GcsObjectName, GcsPath}
 import org.broadinstitute.dsde.workbench.service.util.Tags
-import org.scalatest.time.{Seconds, Span}
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.scalatest.selenium.WebBrowser
+import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatest.{FreeSpec, ParallelTestExecution}
+import org.scalatest.concurrent.TimeLimitedTests
+
 import scala.concurrent.duration._
 
-class NotebooksCanaryTest extends FreeSpec with Matchers with NotebookTestUtils with ParallelTestExecution with BillingFixtures {
+class NotebooksCanaryTest extends FreeSpec with Matchers with NotebookTestUtils with ParallelTestExecution with
+  BillingFixtures {
+
+  //  val timeLimit = Span(1, Seconds)
+
+  // commented out with TimeLimitedTests
 
   implicit val authToken: AuthToken = hermioneAuthToken
   "Test for creating a cluster and localizing a notebook" - {
 
     "should launch a notebook" in {
-      //      val user = LeoAuthToken.getUserToken("dumbledore.admin@test.firecloud.org")
+
+      //      Thread.sleep(2000)
 
       val localizeFileName = "localize_sync.txt"
       val localizeFileContents = "Sync localize test"
@@ -34,15 +44,25 @@ class NotebooksCanaryTest extends FreeSpec with Matchers with NotebookTestUtils 
       val localizeDataFileName = "localize_data_aync.txt"
       val localizeDataContents = "Hello World"
 
+
       withProject { project =>
         implicit token =>
           withNewCluster(project, monitorDelete = true, apiVersion = V2) { cluster =>
-            Leonardo.cluster.start(project, cluster.clusterName)
-            withLocalizeDelocalizeFiles(cluster, localizeFileName, localizeFileContents, delocalizeFileName, delocalizeFileContents, localizeDataFileName, localizeDataContents) { (localizeRequest, bucketName, notebookPage) =>
-              Notebook.localize(project, cluster.clusterName, localizeRequest, async = false)
-              //Notebook.setCookie(project, cluster.clusterName)
-              //cleanup step
-              Leonardo.cluster.delete(project, cluster.clusterName)
+            // verify that the cluster is running
+            cluster.status shouldBe ClusterStatus.Running
+
+            withWebDriver { implicit driver =>
+              withLocalizeDelocalizeFiles(cluster, localizeFileName, localizeFileContents, delocalizeFileName, delocalizeFileContents, localizeDataFileName, localizeDataContents) { (localizeRequest, bucketName, notebookPage) =>
+                Notebook.localize(project, cluster.clusterName, localizeRequest, async = false)
+                // verify that the files are at their locations
+                verifyLocalizeDelocalize(cluster, localizeFileName, localizeFileContents, GcsPath(bucketName, GcsObjectName(delocalizeFileName)), delocalizeFileContents, localizeDataFileName, localizeDataContents)
+                //cleanup step
+                Leonardo.cluster.delete(project, cluster.clusterName)
+                //check for deleted cluster
+                //                val deletedCluster = Leonardo.cluster.get(project, cluster.clusterName)
+                //                deletedCluster.status shouldBe ClusterStatus.Deleted
+                cluster.status shouldBe ClusterStatus.Deleted
+              }
             }
           }
       }
