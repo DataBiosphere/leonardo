f069b0b65060c811c742548864b18b13a0505ff6
Joel Thibault <thibault@broadinstitute.org>
2019-06-10 11:38:27 -0400

    start welder on cluster resume - but disable for now

--- src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/LeonardoService.scala
+++ src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/LeonardoService.scala
@@ -122,12 +122,22 @@ class LeonardoService(protected val dataprocConfig: DataprocConfig,
 
   // Startup scripts to install on the cluster master node.
   // This allows Jupyter and Welder to start back up after a cluster is resumed.
-  //
-  // The || clause is included because older clusters may not have the run-jupyter.sh script installed,
-  // so we need to fall back running `jupyter notebook` directly. See https://github.com/DataBiosphere/leonardo/issues/481.
   private lazy val masterInstanceStartupScript: immutable.Map[String, String] = {
- //TODO
-    immutable.Map("startup-script" -> s"docker exec -d ${dataprocConfig.jupyterServerName} /bin/bash -c '/etc/jupyter/scripts/run-jupyter.sh || /usr/local/bin/jupyter notebook'")
+    val googleKey = "startup-script"  // required; see https://cloud.google.com/compute/docs/startupscript
+
+    // The || clause is included because older clusters may not have the run-jupyter.sh script installed,
+    // so we need to fall back running `jupyter notebook` directly. See https://github.com/DataBiosphere/leonardo/issues/481.
+    val jupyterStart = s"docker exec -d ${dataprocConfig.jupyterServerName} /bin/bash -c '/etc/jupyter/scripts/run-jupyter.sh || /usr/local/bin/jupyter notebook'"
+
+    // TODO make this flag configurable
+    val enableWelder = false
+    val servicesStart = if (enableWelder) {
+      val welderStart = s"docker exec -u daemon -d ${dataprocConfig.welderServerName} /opt/docker/bin/server start"
+      s"($jupyterStart) && $welderStart"
+    }
+    else jupyterStart
+
+    immutable.Map(googleKey -> servicesStart)
   }
 
   protected def checkProjectPermission(userInfo: UserInfo, action: ProjectAction, project: GoogleProject): Future[Unit] = {
