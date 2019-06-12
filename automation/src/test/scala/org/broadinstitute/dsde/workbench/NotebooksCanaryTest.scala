71541df4641a0e95f2aeda97442f43dffd563133
jchanbroad <jchan@broadinstitute.org>
2019-06-12 15:56:14 -0400

    added path firecloud-automated-testing

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/NotebooksCanaryTest.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/NotebooksCanaryTest.scala
@@ -55,7 +55,7 @@ class NotebooksCanaryTest extends FreeSpec with Matchers with NotebookTestUtils 
 
       if (res) {
         import sys.process._
-        s"./home/jenkins/workspace/notebooks-canary-test-script.sh ${clusterTimeRes.duration.toSeconds}" !!
+        s"./firecloud-automated-testing/notebooks-canary-test-script.sh ${clusterTimeRes.duration.toSeconds}" !!
       }
     }
   }
