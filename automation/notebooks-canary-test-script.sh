432cf323f28e8f22b7c5bfd6f86f3cdcb8782e33
jchanbroad <jchan@broadinstitute.org>
2019-06-17 13:17:18 -0400

    changed clusterTimeRes to time

--- automation/notebooks-canary-test-script.sh
+++ automation/notebooks-canary-test-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Parameters
-clusterTimeRes=$1
+time=$1
 
-echo $clusterTimeRes
+echo $time
 
-      echo "[{\"eventType\":\"NotebooksCanaryTestProd\",\"type\":\"Cluster\",\"timeToComplete (sec)\":\"$clusterTimeRes\"}]" > canary_events.json
+      echo "[{\"eventType\":\"NotebooksCanaryTestProd\",\"type\":\"Cluster\",\"timeToComplete (sec)\":\"$time\"}]" > canary_events.json
       cat canary_events.json | gzip -c | curl --data-binary @- -X POST -H "Content-Type: application/json" -H "X-Insert-Key: $newRelicKey" -H "Content-Encoding: gzip" https://insights-collector.newrelic.com/v1/accounts/1862859/events
 
 exit 0
