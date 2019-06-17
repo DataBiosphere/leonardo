35efa4e7c9ceb8b9003849e609b305d72c05bcbf
CH Albach <calbach@google.com>
2019-06-17 06:42:28 -0700

    Only change notebook root for Welder VMs

--- src/main/resources/jupyter/jupyter_notebook_config.py
+++ src/main/resources/jupyter/jupyter_notebook_config.py
@@ -25,7 +25,10 @@ c.NotebookApp.base_url = '/notebooks/' + fragment + '/'
 # directory (which contains several other files on the image). We don't want
 # to mount there as it effectively deletes existing files on the image.
 # See https://docs.google.com/document/d/1b8wIydzC4D7Sbb6h2zWe-jCvoNq-bbD02CT1cnKLbGk
-c.NotebookApp.notebook_dir = '/home/jupyter-user/notebooks'
+if os.environ['WELDER_ENABLE'] == 'true':
+  # Only enable this along with Welder, as this change is backwards incompatible
+  # for older localization users.
+  c.NotebookApp.notebook_dir = '/home/jupyter-user/notebooks'
 
 # This is also specified in run-jupyter.sh
 c.NotebookApp.nbserver_extensions = {
