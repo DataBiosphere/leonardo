7caf5f84da78b7501c5317e8f36db4c2ccc05e65
Azmina Karukappadath <akarukap@broadinstitute.org>
2019-07-05 12:07:52 -0400

    first steps

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoTestUtils.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoTestUtils.scala
@@ -79,10 +79,10 @@ trait LeonardoTestUtils extends WebBrowserSpec with Matchers with Eventually wit
     serverExtensions = Map("jupyterlab" -> "jupyterlab")
   )
 
-//  implicit val cs = IO.contextShift(global)
-//  implicit val t = IO.timer(global)
-//  implicit def unsafeLogger = Slf4jLogger.getLogger[IO]
-  //val google2StorageResource = GoogleStorageService.resource[IO]("/automation/src/test/resources/firecloud-account.json", scala.concurrent.ExecutionContext.global)
+  implicit val cs = IO.contextShift(global)
+  implicit val t = IO.timer(global)
+  implicit def unsafeLogger = Slf4jLogger.getLogger[IO]
+  val google2StorageResource = GoogleStorageService.resource[IO]("/automation/src/test/resources/firecloud-account.json", scala.concurrent.ExecutionContext.global)
 
   // TODO: move this to NotebookTestUtils and chance cluster-specific functions to only call if necessary after implementing RStudio
   def saveJupyterLogFile(clusterName: ClusterName, googleProject: GoogleProject, suffix: String)(implicit token: AuthToken): Try[File] = {
