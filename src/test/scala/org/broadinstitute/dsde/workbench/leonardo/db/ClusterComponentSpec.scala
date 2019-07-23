2a1cf97260b9731518806a6469e268d36fd3f1c4
Rob Title <rtitle@broadinstitute.org>
2019-07-22 23:29:41 -0400

    Fix tests for real this time

--- src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/ClusterComponentSpec.scala
+++ src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/ClusterComponentSpec.scala
@@ -265,8 +265,8 @@ class ClusterComponentSpec extends TestComponent with FlatSpecLike with CommonTe
     val savedCluster2 = makeCluster(2).copy(dataprocInfo = DataprocInfo(None, None, None, None)).save()
 
     // listMonitored should only return clusters that have google info defined
-    dbFutureValue { _.clusterQuery.listMonitoredClusterOnly() } shouldBe Seq(savedCluster1)
-    dbFutureValue { _.clusterQuery.listMonitored() }.map(_.id) shouldBe Seq(savedCluster1)
-    dbFutureValue { _.clusterQuery.listMonitoredFullCluster() }.map(_.id) shouldBe Seq(savedCluster1)
+    dbFutureValue { _.clusterQuery.listMonitoredClusterOnly() } shouldBe Seq(savedCluster1).map(stripFieldsForListCluster).map(_.copy(labels = Map.empty))
+    dbFutureValue { _.clusterQuery.listMonitored() } shouldBe Seq(savedCluster1).map(stripFieldsForListCluster)
+    dbFutureValue { _.clusterQuery.listMonitoredFullCluster() } shouldBe Seq(savedCluster1)
   }
 }
