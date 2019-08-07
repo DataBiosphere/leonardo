679b70e8b402087149896ef53b80105eb1681519
Joel Thibault <thibault@broadinstitute.org>
2019-08-07 11:29:47 -0400

    Cluster init without R kernel was failing due to this missing directory

--- docker/jupyter/scripts/kernel/kernelspec.sh
+++ docker/jupyter/scripts/kernel/kernelspec.sh
@@ -3,9 +3,12 @@
 TMP_KERNELSPEC_DIR=$1
 KERNELSPEC_HOME=$2
 
-# Create directories for PySpark kernels
-# Python kernel directories are somehow already created at this point.
+# TODO: only run this script for the kernels we're actually installing
+
+# Create kernel directories for those which do not already exist
+mkdir ${KERNELSPEC_HOME}/python{2,3}
 mkdir ${KERNELSPEC_HOME}/pyspark{2,3}
+mkdir ${KERNELSPEC_HOME}/ir
 
 # Replace the contents of the Python kernel scripts
 sed -e 's/${PY_VERSION}/2/g' -e 's|${JUPYTER_HOME}|'${JUPYTER_HOME}'|g' ${TMP_KERNELSPEC_DIR}/python_kernelspec.tmpl > ${KERNELSPEC_HOME}/python2/kernel.json
