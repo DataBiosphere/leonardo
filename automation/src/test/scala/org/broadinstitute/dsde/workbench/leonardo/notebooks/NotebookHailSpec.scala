715f31a7218e2e4f2000ef3d4f34b870d5a72e50
Qi Wang <qi@broadinstitute.org>
2019-09-12 16:06:12 -0400

    fix automation tests

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookHailSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookHailSpec.scala
@@ -55,5 +55,35 @@ class NotebookHailSpec extends ClusterFixtureSpec with NotebookTestUtils {
         }
       }
     }
+<<<<<<< HEAD
+=======
+
+    val sparkJobToSucceed =
+      """import random
+        |import hail as hl
+        |sc = hl.spark_context()
+        |NUM_SAMPLES=20
+        |def inside(p):
+        |    x, y = random.random(), random.random()
+        |    return x*x + y*y < 1
+        |
+        |count = sc.parallelize(range(0, NUM_SAMPLES)) \
+        |             .filter(inside).count()
+        |print("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))""".stripMargin
+
+    Seq(PySpark2, PySpark3).foreach { kernel =>
+
+      s"should be able to run a Spark job with a ${kernel.string} kernel" in { clusterFixture =>
+        withWebDriver { implicit driver =>
+          withNewNotebook(clusterFixture.cluster, kernel) { notebookPage =>
+            val cellResult = notebookPage.executeCell(sparkJobToSucceed).get
+            cellResult should include("Pi is roughly ")
+            cellResult.toLowerCase should not include "error"
+          }
+        }
+      }
+    }
+
+>>>>>>> fix automation tests
   }
 }
