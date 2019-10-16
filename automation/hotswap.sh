64984de5383dccdec011fc9fe9d4ddf808eabcc2
Kaan Yuksel <kyuksel@broadinstitute.org>
2019-10-16 17:19:43 -0400

    Add info statements to the script

--- automation/hotswap.sh
+++ automation/hotswap.sh
@@ -11,14 +11,17 @@ fi
 
 FIAB=$1
 
+echo "Generating the Leo jar..."
 # Example: /Users/qi/workspace/leonardo/http/target/scala-2.12/http-assembly-0.1-437ee4a9-SNAPSHOT.jar
 LEO_JAR_PATH=$(sbt -Dsbt.log.noformat=true "project http" assembly | tail -3 | head -2 | grep -o '/Users[^ ]*')
 LEO_JAR_NAME=$(basename $LEO_JAR_PATH)
 # Rename the jar to leonardo-assembly-0.1-437ee4a9-SNAPSHOT.jar so that the fiab-start Jenkins job will pick up the right jar file
 NEW_LEO_JAR_NAME=$(echo ${LEO_JAR_NAME}|sed 's/http\-/leonardo\-/g' )
 
+echo "Copying ${LEO_JAR_PATH} to /tmp on FIAB '${FIAB}'..."
 gcloud compute scp ${LEO_JAR_PATH} ${FIAB}:/tmp --zone=us-central1-a --project broad-dsde-dev
 
+echo "Copying the Leo jar to the right location on the FIAB and restarting the Leo app and proxy..."
 gcloud compute ssh --project broad-dsde-dev --zone us-central1-a ${FIAB} << EOSSH
     sudo docker exec -it firecloud_leonardo-app_1 sh -c "rm -f /leonardo/*jar"
     sudo docker cp /tmp/${LEO_JAR_NAME} firecloud_leonardo-app_1:/leonardo/${NEW_LEO_JAR_NAME}
