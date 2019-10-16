e36cd3543ed1fde8d35e17dbe37339fff694512f
Kaan Yuksel <kyuksel@broadinstitute.org>
2019-10-16 17:36:34 -0400

    Another attempt at fixing the script issue

--- automation/hotswap.sh
+++ automation/hotswap.sh
@@ -11,19 +11,22 @@ fi
 
 FIAB=$1
 
-echo "Generating the Leo jar..."
+printf "Generating the Leo jar...\n\n"
 # Example: /Users/qi/workspace/leonardo/http/target/scala-2.12/http-assembly-0.1-437ee4a9-SNAPSHOT.jar
 LEO_JAR_PATH=$(sbt -Dsbt.log.noformat=true "project http" assembly | tail -3 | head -2 | grep -o '/Users[^ ]*')
 LEO_JAR_NAME=$(basename $LEO_JAR_PATH)
 # Rename the jar to leonardo-assembly-0.1-437ee4a9-SNAPSHOT.jar so that the fiab-start Jenkins job will pick up the right jar file
 NEW_LEO_JAR_NAME=$(echo ${LEO_JAR_NAME}|sed 's/http\-/leonardo\-/g' )
 
-echo "Copying ${LEO_JAR_PATH} to /tmp on FIAB '${FIAB}'..."
+printf "Copying ${LEO_JAR_PATH} to /tmp on FIAB '${FIAB}'...\n\n"
 gcloud compute scp ${LEO_JAR_PATH} ${FIAB}:/tmp --zone=us-central1-a --project broad-dsde-dev
 
-echo "Copying the Leo jar to the right location on the FIAB and restarting the Leo app and proxy..."
+printf "Removing the old Leo jars on the FIAB...\n\n"
+# For some reason including the rm command to be executed on the FIAB within the EOSSH block below doesn't work
+gcloud compute ssh --project broad-dsde-dev --zone us-central1-a ${FIAB} -- 'sudo docker exec -it firecloud_leonardo-app_1 sh -c "rm -f /leonardo/*jar"'
+
+printf "Copying the Leo jar to the right location on the FIAB, and restarting the Leo app and proxy...\n\n"
 gcloud compute ssh --project broad-dsde-dev --zone us-central1-a ${FIAB} << EOSSH
-    sudo docker exec -it firecloud_leonardo-app_1 sh -c "rm -f /leonardo/*jar"
     sudo docker cp /tmp/${LEO_JAR_NAME} firecloud_leonardo-app_1:/leonardo/${NEW_LEO_JAR_NAME}
     sudo docker restart firecloud_leonardo-app_1
     sudo docker restart firecloud_leonardo-proxy_1
