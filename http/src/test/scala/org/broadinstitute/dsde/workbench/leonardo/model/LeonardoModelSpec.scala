e2083fbd3f460d0129daa934b7c1bc9b872fdfbc
akarukappadath <39962314+akarukappadath@users.noreply.github.com>
2019-10-30 21:52:25 -0400

    [IA-1475] Improve regex for custom docker image (#1131)
    * took from terra
    
    * test added

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/model/LeonardoModelSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/model/LeonardoModelSpec.scala
@@ -222,6 +222,7 @@ class LeonardoModelSpec extends TestComponent with FlatSpecLike with Matchers wi
     ContainerRegistry.DockerHub.regex.pattern.asPredicate().test("asd/asdf") shouldBe (true)
     ContainerRegistry.DockerHub.regex.pattern.asPredicate().test("asd") shouldBe (true)
     ContainerRegistry.DockerHub.regex.pattern.asPredicate().test("asd_sd_as:asdf") shouldBe (true)
+    ContainerRegistry.DockerHub.regex.pattern.asPredicate().test("asd/as:asdf") shouldBe (true)
     ContainerRegistry.DockerHub.regex.pattern.asPredicate().test("asd_as:asdf ") shouldBe (false) //trailing white space
     ContainerRegistry.DockerHub.regex.pattern.asPredicate().test("asd_as: asdf") shouldBe (false) //white space
     ContainerRegistry.DockerHub.regex.pattern
