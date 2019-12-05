9152caffe371de93d1f45fd35add8a68488cd09f
Qi Wang <qi@broadinstitute.org>
2019-12-05 17:11:13 -0500

    use latest google2 lib

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val workbenchUtilV = "0.5-4c7acd5"
   val workbenchModelV = "0.13-6dc016b"
   val workbenchGoogleV = "0.21-890a74f"
-  val workbenchGoogle2V = "0.6-4df42ca"
+  val workbenchGoogle2V = "0.6-ca8509e"
   val workbenchMetricsV = "0.3-c5b80d2"
   val workbenchNewRelicV = "0.3-8bae8e8"
 
