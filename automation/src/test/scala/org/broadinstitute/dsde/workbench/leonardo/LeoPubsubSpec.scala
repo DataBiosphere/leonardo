9ec1e93dd637c6f081dfab53c3fc433a80a31835
jdcanas <jcanas@broadinstitute.com>
2020-01-15 09:17:46 -0500

    committing automation changes

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeoPubsubSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeoPubsubSpec.scala
@@ -1,18 +1,13 @@
 package org.broadinstitute.dsde.workbench.leonardo
 
-import cats.effect.{IO, Resource}
-import ch.qos.logback.core.net.QueueFactory
+import cats.effect.IO
 import fs2.concurrent.InspectableQueue
-import fs2.Stream
-import io.circe.{Decoder, Encoder}
-import org.broadinstitute.dsde.workbench.google2.{Event, GooglePublisher, GoogleSubscriber}
-import org.broadinstitute.dsde.workbench.leonardo.ClusterStatus.ClusterStatus
-import org.broadinstitute.dsde.workbench.leonardo.notebooks.Welder
+import org.broadinstitute.dsde.workbench.google2.GooglePublisher
 import org.broadinstitute.dsde.workbench.service.util.Tags
 import org.scalatest.time.{Minutes, Span}
-import org.scalatest.{BeforeAndAfterAll, DoNotDiscover, fixture}
+import org.scalatest.BeforeAndAfterAll
 
-@DoNotDiscover
+//@DoNotDiscover
 class LeoPubsubSpec extends ClusterFixtureSpec with BeforeAndAfterAll with LeonardoTestUtils {
 
   "Google publisher should be able to auth" taggedAs Tags.SmokeTest in { clusterFixture =>
@@ -23,7 +18,6 @@ class LeoPubsubSpec extends ClusterFixtureSpec with BeforeAndAfterAll with Leona
   }
 
   "Google publisher should publish" in { _ =>
-    import org.broadinstitute.dsde.workbench.leonardo.LeoPubsubCodec._
     val publisher =  GooglePublisher.resource[IO, String](LeonardoConfig.Leonardo.publisherConfig)
     val queue =  InspectableQueue.bounded[IO, String](100).unsafeRunSync()
 
@@ -32,7 +26,7 @@ class LeoPubsubSpec extends ClusterFixtureSpec with BeforeAndAfterAll with Leona
         .compile
         .drain
     }
-      .unsafeRunSync()
+      .unsafeRunAsync(_ => ())
 
     queue.enqueue1("automation-test-message").unsafeRunSync()
 
@@ -42,72 +36,5 @@ class LeoPubsubSpec extends ClusterFixtureSpec with BeforeAndAfterAll with Leona
     }
 
   }
-  
-}
 
-sealed trait LeoPubsubMessage {
-  def messageType: String
-}
-
-final case class ClusterFollowupDetails(clusterId: Long, clusterStatus: ClusterStatus) extends Product with Serializable
-
-abstract class ClusterTransitionRequestedMessage(updatedMachineConfig: MachineConfig, clusterId: Long)  extends LeoPubsubMessage
-
-final case class StopUpdateMessage(updatedMachineConfig: MachineConfig, clusterId: Long)
-  extends ClusterTransitionRequestedMessage(updatedMachineConfig: MachineConfig, clusterId: Long) {
-  val messageType = "stopUpdate"
-}
-
-case class ClusterTransitionFinishedMessage(followupDetails: ClusterFollowupDetails) extends LeoPubsubMessage {
-  val messageType = "transitionFinished"
-}
-
-final case class PubsubException(message: String) extends Exception
-
-object LeoPubsubCodec {
-  implicit val stopUpdateMessageDecoder: Decoder[StopUpdateMessage] =
-    Decoder.forProduct2("updatedMachineConfig", "clusterId")(StopUpdateMessage.apply)
-
-  implicit val clusterFollowupDetailsDecoder: Decoder[ClusterFollowupDetails] =
-    Decoder.forProduct2("clusterId", "clusterStatus")(ClusterFollowupDetails.apply)
-
-  implicit val clusterTransitionFinishedDecoder: Decoder[ClusterTransitionFinishedMessage] =
-    Decoder.forProduct1("followupDetails")(ClusterTransitionFinishedMessage.apply)
-
-  implicit val leoPubsubMessageDecoder: Decoder[LeoPubsubMessage] = {
-    for {
-      messageType <- Decoder[String].prepare(_.downField("messageType"))
-      value <- messageType match {
-        case "stopUpdate" => Decoder[StopUpdateMessage]
-        case "transitionFinished" => Decoder[ClusterTransitionFinishedMessage]
-        case other => throw PubsubException(s"found a message with an unknown type when decoding: ${other}")
-      }
-    } yield value
-  }
-
-  implicit val machineConfigEncoder: Encoder[MachineConfig] =
-    Encoder.forProduct7("numberOfWorkers",
-      "masterMachineType",
-      "masterDiskSize",
-      "workerMachineType",
-      "workerDiskSize",
-      "numberOfWorkerLocalSSDs",
-      "numberOfPreemptibleWorkers")(x => MachineConfig.unapply(x).get)
-
-  implicit val stopUpdateMessageEncoder: Encoder[StopUpdateMessage] =
-    Encoder.forProduct2("messageType", "updatedMachineConfig")(x => (x.updatedMachineConfig, x.clusterId))
-
-  implicit val clusterFollowupDetailsEncoder: Encoder[ClusterFollowupDetails] =
-    Encoder.forProduct2("clusterId", "clusterStatus")(x => (x.clusterId, x.clusterStatus))
-
-  implicit val clusterTransitionFinishedEncoder: Encoder[ClusterTransitionFinishedMessage] =
-    Encoder.forProduct1("followupDetails")(x => x.followupDetails)
-
-  implicit val leoPubsubMessageEncoder: Encoder[LeoPubsubMessage] = (message: LeoPubsubMessage) => {
-    message.messageType match {
-      case "stopUpdate" => stopUpdateMessageEncoder(message.asInstanceOf[StopUpdateMessage])
-      case "transitionFinished" => clusterTransitionFinishedEncoder(message.asInstanceOf[ClusterTransitionFinishedMessage])
-      case other => throw PubsubException(s"found a message with an unknown type when encoding: ${other}")
-    }
-  }
 }
