7f9dab3b78819e02b85a7f7def5d483be0d041d0
Rob Title <rtitle@broadinstitute.org>
2020-01-21 12:05:31 -0500

    Specify legacy image in config as well

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/ImageConfig.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/ImageConfig.scala
@@ -3,6 +3,7 @@ package org.broadinstitute.dsde.workbench.leonardo.config
 final case class ImageConfig(
   welderDockerImage: String,
   jupyterImage: String,
+  legacyJupyterImage: String,
   jupyterImageRegex: String,
   rstudioImageRegex: String,
   broadDockerhubImageRegex: String
