5d2527579dad1253e9021e68bd15cb4fec26812b
Kaan Yuksel <kyuksel@broadinstitute.org>
2020-01-23 12:12:45 -0500

    Remove user-scripts, Dockerfile and README

--- docker/jupyter/user-scripts/enable_download_as_pdf.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-pip install nbconvert
-apt-get update
-apt-get install -yq pandoc texlive-xetex
--- docker/jupyter/user-scripts/install_igv_extension.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pip3 install igv-jupyter
-
-jupyter serverextension enable --py igv --sys-prefix
-jupyter nbextension install --py igv --sys-prefix
-jupyter nbextension enable --py igv --sys-prefix
-
-
