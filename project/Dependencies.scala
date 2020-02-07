08213ef6dfa043a8acebe7fc3d785ba9dde8a675
Qi Wang <qi@broadinstitute.org>
2020-02-07 12:42:40 -0500

    fix a bug in wb-libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies {
   val workbenchUtilV = "0.5-4c7acd5"
   val workbenchModelV = "0.13-db13244-SNAP"
   val workbenchGoogleV = "0.21-890a74f"
-  val workbenchGoogle2V = "0.6-707125c-SNAP"
+  val workbenchGoogle2V = "0.6-6b294a2-SNAP"
   val workbenchMetricsV = "0.3-c5b80d2"
   val workbenchNewRelicV = "0.3-8bae8e8"
 
