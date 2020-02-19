a1f8f771bf171c6cc257f51dd2339cb9b38361f6
jdcanas <jcanas@broadinstitute.com>
2020-02-19 11:04:52 -0500

    widen tolerance

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/cluster/ClusterAutopauseSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/cluster/ClusterAutopauseSpec.scala
@@ -2,7 +2,7 @@ package org.broadinstitute.dsde.workbench.leonardo.cluster
 
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.leonardo.{ClusterStatus, GPAllocFixtureSpec, Leonardo, LeonardoTestUtils}
-import org.scalatest.time.{Seconds, Span}
+import org.scalatest.time.{Minutes, Seconds, Span}
 import org.scalatest.{DoNotDiscover, ParallelTestExecution}
 
 @DoNotDiscover
@@ -20,9 +20,9 @@ class ClusterAutopauseSpec extends GPAllocFixtureSpec with ParallelTestExecution
         .get(cluster.googleProject, cluster.clusterName)
         .autopauseThreshold shouldBe 1
 
-      eventually(timeout(Span(90, Seconds)), interval(Span(10, Seconds))) {
+      //the autopause check interval is 1 minute at the time of creation, but it can be flaky with a tighter window.
+      eventually(timeout(Span(3, Minutes)), interval(Span(10, Seconds))) {
         val dbCluster = Leonardo.cluster.get(cluster.googleProject, cluster.clusterName)
-        dbCluster.autopauseThreshold shouldBe 1
         dbCluster.status shouldBe ClusterStatus.Stopping
       }
     }
