8354f4c32c183500d31447966dc183822ea7e8a4
Kaan Yuksel <kyuksel@broadinstitute.org>
2020-02-19 17:48:15 -0500

    clean up the wrapper script

--- jenkins/gce-custom-images/create_gce_image.sh
+++ jenkins/gce-custom-images/create_gce_image.sh
@@ -1,9 +1,34 @@
 #!/usr/bin/env bash
 
-OUTPUT_IMAGE_NAME="leo-custom-gce-image-200214-10-25"
+set -e -x
 
-docker run -it --rm -v /Users/kyuksel/github/leonardo/jenkins/gce-custom-images:/daisy_source_files \
-  gcr.io/compute-image-tools/daisy \
+# This script is a wrapper around the command that runs the custom GCE image
+# creation tool - Daisy - for local development and documentation purposes.
+# It should be run from the root of the Leonardo repo.
+# It assumes that a Daisy image was pulled; e.g. docker pull gcr.io/compute-image-tools/daisy:latest
+
+# The date-time suffix is in the format yymmdd-hh-mm but it could be anything.
+OUTPUT_IMAGE_NAME="leo-custom-gce-image-200219-16-05"
+
+# Set this to the tag of the Daisy image you had pulled
+DAISY_IMAGE_TAG="latest"
+
+# The source directory should contain `application_default_credentials.json`
+# which can be generated via `gcloud auth application-default login` and is saved at
+# `~/.config/gcloud/application_default_credentials.json` by default.
+SOURCE_DIR="/Users/kyuksel/github/leonardo/jenkins/gce-custom-images"
+
+# Set this to "true" if you want to validate the workflow without actually executing it
+VALIDATE_WORKFLOW="false"
+
+if [[ "$VALIDATE_WORKFLOW" == "true" ]] ; then
+  DAISY_CONTAINER="gcr.io/compute-image-tools/daisy:${DAISY_IMAGE_TAG} -validate"
+else
+  DAISY_CONTAINER="gcr.io/compute-image-tools/daisy:${DAISY_IMAGE_TAG}"
+fi
+
+docker run -it --rm -v "$SOURCE_DIR":/daisy_source_files \
+  $DAISY_CONTAINER \
   -project broad-dsde-dev \
   -zone us-central1-a \
   -default_timeout 60m \
