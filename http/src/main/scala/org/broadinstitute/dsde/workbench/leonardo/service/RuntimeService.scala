2e4e32551477e7731ac3f2198577203ccb4fd4e5
Qi Wang <qi@broadinstitute.org>
2020-03-05 14:25:45 -0500

    comment

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/RuntimeService.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/service/RuntimeService.scala
@@ -10,6 +10,6 @@ import org.broadinstitute.dsde.workbench.model.google.GoogleProject
 trait RuntimeService[F[_]] {
   def createRuntime(userInfo: UserInfo,
                     googleProject: GoogleProject,
-                    runtimeName: RuntimeName, //TODO: rename this to RuntimeName once Rob's PR is in
+                    runtimeName: RuntimeName,
                     req: CreateRuntime2Request)(implicit as: ApplicativeAsk[F, RuntimeServiceContext]): F[Unit]
 }
