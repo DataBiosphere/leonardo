1da0e59d37ca1e802ca46231f0a2e90d3902067f
Rob Title <rtitle@broadinstitute.org>
2020-04-22 13:04:55 -0400

    Bah typo

--- http/src/main/resources/init-resources/init-actions.sh
+++ http/src/main/resources/init-resources/init-actions.sh
@@ -101,7 +101,7 @@ if [[ "${ROLE}" == 'Master' ]]; then
 
     # The following values are populated by Leo when a cluster is created.
     export CLUSTER_NAME=$(clusterName)
-    export RUNTIME_NAME=$(runtimeName)
+    export RUNTIME_NAME=$(clusterName)
     export GOOGLE_PROJECT=$(googleProject)
     export STAGING_BUCKET=$(stagingBucketName)
     export OWNER_EMAIL=$(loginHint)
