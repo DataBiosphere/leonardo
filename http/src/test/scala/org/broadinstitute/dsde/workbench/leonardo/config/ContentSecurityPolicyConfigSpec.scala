84c05117b74aa7ced0baa1aa9e4f356bf9d36a44
Rob Title <rtitle@broadinstitute.org>
2020-04-30 22:39:12 -0400

    Fix spec

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/config/ContentSecurityPolicyConfigSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/config/ContentSecurityPolicyConfigSpec.scala
@@ -29,16 +29,18 @@ class ContentSecurityPolicyConfigSpec extends LeonardoTestSuite with FlatSpecLik
       ScriptSrc(
         List(
           "'self'",
-          "data:text/javascript",
+          "data:",
           "'unsafe-inline'",
           "'unsafe-eval'",
-          "https://apis.google.com"
+          "https://apis.google.com",
+          "https://cdn.jsdelivr.net"
         )
       ),
       StyleSrc(
         List(
           "'self'",
-          "'unsafe-inline'"
+          "'unsafe-inline'",
+          "data:"
         )
       ),
       ConnectSrc(
@@ -63,13 +65,13 @@ class ContentSecurityPolicyConfigSpec extends LeonardoTestSuite with FlatSpecLik
     )
 
     test.asString shouldBe
-      "frame-ancestors 'self' *.terra.bio https://bvdp-saturn-prod.appspot.com https://all-of-us-rw-staging.appspot.com https://all-of-us-rw-stable.appspot.com https://stable.fake-research-aou.org https://workbench.researchallofus.org terra.biodatacatalyst.nhlbi.nih.gov; script-src 'self' data:text/javascript 'unsafe-inline' 'unsafe-eval' https://apis.google.com; style-src 'self' 'unsafe-inline'; connect-src 'self' wss://*.broadinstitute.org:* wss://notebooks.firecloud.org:* *.googleapis.com https://*.npmjs.org https://data.broadinstitute.org https://s3.amazonaws.com/igv.broadinstitute.org/ https://s3.amazonaws.com/igv.org.genomes/ https://raw.githubusercontent.com/PAIR-code/facets/1.0.0/facets-dist/facets-jupyter.html https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.3.3/webcomponents-lite.js; object-src 'none'"
+      "frame-ancestors 'self' *.terra.bio https://bvdp-saturn-prod.appspot.com https://all-of-us-rw-staging.appspot.com https://all-of-us-rw-stable.appspot.com https://stable.fake-research-aou.org https://workbench.researchallofus.org terra.biodatacatalyst.nhlbi.nih.gov; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' data:; connect-src 'self' wss://*.broadinstitute.org:* wss://notebooks.firecloud.org:* *.googleapis.com https://*.npmjs.org https://data.broadinstitute.org https://s3.amazonaws.com/igv.broadinstitute.org/ https://s3.amazonaws.com/igv.org.genomes/ https://raw.githubusercontent.com/PAIR-code/facets/1.0.0/facets-dist/facets-jupyter.html https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.3.3/webcomponents-lite.js; object-src 'none'"
 
   }
 
   it should "parse config values correctly" in {
     CommonTestData.contentSecurityPolicy shouldBe
-      "frame-ancestors 'none'; script-src 'self' data:text/javascript 'unsafe-inline' 'unsafe-eval' https://apis.google.com; style-src 'self' 'unsafe-inline'; connect-src 'self' wss://*.broadinstitute.org:* wss://notebooks.firecloud.org:* *.googleapis.com https://*.npmjs.org https://data.broadinstitute.org https://s3.amazonaws.com/igv.broadinstitute.org/ https://s3.amazonaws.com/igv.org.genomes/ https://raw.githubusercontent.com/PAIR-code/facets/1.0.0/facets-dist/facets-jupyter.html https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.3.3/webcomponents-lite.js; object-src 'none'"
+      "frame-ancestors 'none'; script-src 'self' data: 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' data:; connect-src 'self' wss://*.broadinstitute.org:* wss://notebooks.firecloud.org:* *.googleapis.com https://*.npmjs.org https://data.broadinstitute.org https://s3.amazonaws.com/igv.broadinstitute.org/ https://s3.amazonaws.com/igv.org.genomes/ https://raw.githubusercontent.com/PAIR-code/facets/1.0.0/facets-dist/facets-jupyter.html https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.3.3/webcomponents-lite.js; object-src 'none'"
   }
 
 }
