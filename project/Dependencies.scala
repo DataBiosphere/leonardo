11e359d3791ce639fb0d6320ca0f8d79eae13bb7
Rob Title <rtitle@broadinstitute.org>
2020-05-19 18:02:03 -0400

    Non snap dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,8 +18,7 @@ object Dependencies {
 
   val workbenchUtilV = "0.5-4c7acd5"
   val workbenchModelV = "0.13-31cacc4"
-  // TODO: revert to non-SNAP and remove this comment once https://github.com/broadinstitute/workbench-libs/pull/307 is merged
-  val workbenchGoogleV = "0.21-a6ee6de-SNAP"
+  val workbenchGoogleV = "0.21-2a218f3"
   val workbenchGoogle2V = "0.8-e08439a"
   val workbenchMetricsV = "0.3-c5b80d2"
   val workbenchOpenTelemetryV = "0.1-73d6a64"
