613f2394863961f092811d1a546808bdaeb1a875
Qi Wang <qi@broadinstitute.org>
2020-05-20 21:05:08 -0400

    adjust some numbers

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/Config.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/Config.scala
@@ -256,7 +256,6 @@ object Config {
   implicit private val autoFreezeConfigReader: ValueReader[AutoFreezeConfig] = ValueReader.relative { config =>
     AutoFreezeConfig(
       config.getBoolean("enableAutoFreeze"),
-      toScalaDuration(config.getDuration("dateAccessedMonitorScheduler")),
       toScalaDuration(config.getDuration("autoFreezeAfter")),
       toScalaDuration(config.getDuration("autoFreezeCheckScheduler")),
       toScalaDuration(config.getDuration("maxKernelBusyLimit"))
