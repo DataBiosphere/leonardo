748a14e9941860cd483f644055b81e385ac96d5b
Qi Wang <qi@broadinstitute.org>
2020-05-20 20:17:39 -0400

    fix assembly merge error

--- project/Merging.scala
+++ project/Merging.scala
@@ -1,10 +1,16 @@
 import sbtassembly.{MergeStrategy, PathList}
 
 object Merging {
-  def customMergeStrategy(oldStrategy: (String) => MergeStrategy):(String => MergeStrategy) = {
+  def customMergeStrategy(oldStrategy: (String) => MergeStrategy): (String => MergeStrategy) = {
     case PathList("org", "joda", "time", "base", "BaseDateTime.class") => MergeStrategy.first
-    case "module-info.class" => MergeStrategy.discard  // JDK 8 does not use the file module-info.class so it is safe to discard the file.
+    // For the following error:
+    //[error] java.lang.RuntimeException: deduplicate: different file contents found in the following:
+    //[error] /root/.cache/coursier/v1/https/repo1.maven.org/maven2/com/google/protobuf/protobuf-java/3.11.4/protobuf-java-3.11.4.jar:google/protobuf/field_mask.proto
+    //[error] /root/.cache/coursier/v1/https/repo1.maven.org/maven2/com/typesafe/akka/akka-protobuf-v3_2.12/2.6.5/akka-protobuf-v3_2.12-2.6.5.jar:google/protobuf/field_mask.proto
+    case PathList("google", "protobuf", "field_mask.proto") => MergeStrategy.first
+    case "module-info.class" =>
+      MergeStrategy.discard // JDK 8 does not use the file module-info.class so it is safe to discard the file.
     case "reference.conf" => MergeStrategy.concat
-    case x => oldStrategy(x)
+    case x                => oldStrategy(x)
   }
 }
