0760b54323fee808619d70a4f21bb32a9278d85a
Qi Wang <qi@broadinstitute.org>
2020-06-13 16:43:05 -0400

    small improvement

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/DataprocInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/DataprocInterpreter.scala
@@ -338,16 +338,14 @@ class DataprocInterpreter[F[_]: Timer: Async: Parallel: ContextShift: Logger](
   override protected def setMachineTypeInGoogle(runtime: Runtime, machineType: MachineTypeName)(
     implicit ev: ApplicativeAsk[F, TraceId]
   ): F[Unit] =
-    runtime.dataprocInstances.toList.traverse_ { instance =>
-      // Note: we don't support changing the machine type for worker instances. While this is possible
-      // in GCP, Spark settings are auto-tuned to machine size. Dataproc recommends adding or removing nodes,
-      // and rebuilding the cluster if new worker machine/disk sizes are needed.
-      instance.dataprocRole match {
-        case Master =>
-          googleComputeService.setMachineType(instance.key.project, instance.key.zone, instance.key.name, machineType)
-        case _ => Async[F].unit
-      }
-    }
+    runtime.dataprocInstances
+      .find(_.dataprocRole == Master)
+      .traverse_(instance =>
+        // Note: we don't support changing the machine type for worker instances. While this is possible
+        // in GCP, Spark settings are auto-tuned to machine size. Dataproc recommends adding or removing nodes,
+        // and rebuilding the cluster if new worker machine/disk sizes are needed.
+        googleComputeService.setMachineType(instance.key.project, instance.key.zone, instance.key.name, machineType)
+      )
 
   override def updateDiskSize(params: UpdateDiskSizeParams)(implicit ev: ApplicativeAsk[F, TraceId]): F[Unit] =
     params.runtime.dataprocInstances.toList.traverse_ { instance =>
