6cbfd7ec33c57621d79e9cce1f99607f6c906a63
Qi Wang <qi@broadinstitute.org>
2020-06-26 13:12:25 -0400

    see if this makes any difference

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/page/CookieAuthedPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/page/CookieAuthedPage.scala
@@ -1,5 +1,6 @@
 package org.broadinstitute.dsde.workbench.page
 
+import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.auth.AuthToken
 import org.broadinstitute.dsde.workbench.service.test.WebBrowserUtil
 import org.openqa.selenium.WebDriver
@@ -7,16 +8,17 @@ import org.scalatestplus.selenium.Page
 
 import scala.util.{Failure, Success, Try}
 
-trait CookieAuthedPage[P <: Page] extends Page with PageUtil[P] with WebBrowserUtil { self: P =>
+trait CookieAuthedPage[P <: Page] extends Page with PageUtil[P] with WebBrowserUtil with LazyLogging { self: P =>
   implicit val authToken: AuthToken
 
   // always use open() to access a CookieAuthedPage - `go to` will not set the cookie
   override def open(implicit webDriver: WebDriver): P = {
-    go.to(this)
     addCookie("LeoToken", authToken.value)
     Try(super.open) match {
       case Success(page) => page
-      case Failure(_)    => super.open // anonymously retry open
+      case Failure(e) =>
+        logger.error("Fail to open page", e)
+        super.open // anonymously retry open
     }
   }
 }
