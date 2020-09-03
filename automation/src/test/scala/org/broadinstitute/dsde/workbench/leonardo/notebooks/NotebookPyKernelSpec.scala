3cf220fea1f632e7036f15e3c155597dd55335a5
DSDE Jenkins <dsde-jenkins@broadinstitute.org>
2020-09-03 09:06:05 -0400

    [jenkins-generated-pr] Automated terra-docker image hash update (#1594)
    * automated config hash updates
    
    * add to automation test
    
    Co-authored-by: Jenkins Role account <jenkins@dsp-fc-jenkins-slave-prod210.c.broad-dsp-techops.internal>
    Co-authored-by: Gabriela <gcarrill@broadinstitute.org>

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookPyKernelSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookPyKernelSpec.scala
@@ -16,22 +16,24 @@ class NotebookPyKernelSpec extends RuntimeFixtureSpec with NotebookTestUtils {
 
   "NotebookPyKernelSpec" - {
 
-    "should create a notebook with a working Python 3 kernel and import installed packages" in { runtimeFixture =>
-      withWebDriver { implicit driver =>
-        withNewNotebook(runtimeFixture.runtime, Python3) { notebookPage =>
-          val getPythonVersion =
-            """import platform
-              |print(platform.python_version())""".stripMargin
-          val getBxPython =
-            """import bx.bitset
-              |bx.bitset.sys.copyright""".stripMargin
-          val getPandasLocation = "! pip3 show pandas"
-          notebookPage.executeCell("1+1") shouldBe Some("2")
-          notebookPage.executeCell(getPythonVersion).get should include("3.7")
-          notebookPage.executeCell(getBxPython).get should include("Copyright (c)")
-          notebookPage.executeCell(getPandasLocation).get should include("/usr/local/lib/python3.7/dist-packages")
+    "should create a notebook with a working Python 3 kernel, import installed packages and default to notebooks dir in terminal" in {
+      runtimeFixture =>
+        withWebDriver { implicit driver =>
+          withNewNotebook(runtimeFixture.runtime, Python3) { notebookPage =>
+            val getPythonVersion =
+              """import platform
+                |print(platform.python_version())""".stripMargin
+            val getBxPython =
+              """import bx.bitset
+                |bx.bitset.sys.copyright""".stripMargin
+            val getPandasLocation = "! pip3 show pandas"
+            notebookPage.executeCell("1+1") shouldBe Some("2")
+            notebookPage.executeCell(getPythonVersion).get should include("3.7")
+            notebookPage.executeCell(getBxPython).get should include("Copyright (c)")
+            notebookPage.executeCell(getPandasLocation).get should include("/usr/local/lib/python3.7/dist-packages")
+            notebookPage.executeCell("! pwd").get shouldBe ("/home/jupyter-user/notebooks")
+          }
         }
-      }
     }
 
     Seq(Python3).foreach { kernel =>
