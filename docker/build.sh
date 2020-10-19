de9021233f335a322119490688900aa9e9f899b5
Rob Title <rtitle@broadinstitute.org>
2020-10-19 15:36:57 -0400

    Remove --exit-code 1

--- docker/build.sh
+++ docker/build.sh
@@ -176,7 +176,7 @@ function docker_cmd()
         docker build -t "${DEFAULT_IMAGE}:${DOCKER_TAG}" .
 
         echo "scanning docker image..."
-        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME"/Library/Caches:/root/.cache/ aquasec/trivy --exit-code 1 --severity CRITICAL "${DEFAULT_IMAGE}":"${DOCKER_TAG}"
+        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME"/Library/Caches:/root/.cache/ aquasec/trivy --severity CRITICAL "${DEFAULT_IMAGE}":"${DOCKER_TAG}"
 
 
         echo "building $TESTS_IMAGE docker image..."
