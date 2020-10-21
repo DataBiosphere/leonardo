b70f2a337b23ac43a93f51f1d3bc742058698487
Rob Title <rtitle@broadinstitute.org>
2020-10-21 15:01:47 -0400

    Attempt to fix IA-2279

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GceInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GceInterpreter.scala
@@ -268,7 +268,6 @@ class GceInterpreter[F[_]: Parallel: ContextShift](
   ): F[Option[com.google.cloud.compute.v1.Operation]] =
     for {
       metadata <- getShutdownScript(runtime, blocker)
-      _ <- logger.info("XXX shutdown script is " + metadata)
       _ <- googleComputeService.addInstanceMetadata(runtime.googleProject,
                                                     config.gceConfig.zoneName,
                                                     InstanceName(runtime.runtimeName.asString),
@@ -319,7 +318,6 @@ class GceInterpreter[F[_]: Parallel: ContextShift](
     if (params.runtime.asyncRuntimeFields.isDefined) {
       for {
         metadata <- getShutdownScript(params.runtime, blocker)
-        _ <- logger.info("XXX shutdown script is " + metadata)
         _ <- googleComputeService.addInstanceMetadata(params.runtime.googleProject,
                                                       config.gceConfig.zoneName,
                                                       InstanceName(params.runtime.runtimeName.asString),
