c80029cd2dd4736bbcc2e1726226ba0eaa75cab3
xuscrbw <62293969+xuscrbw@users.noreply.github.com>
2020-11-23 14:25:28 -0500

    IA-697: adding Rstudio smoke test (#1703)
    * adding test and rstudio objects
    
    * removing extra RstudioMainPage
    
    * fix for pr
    
    * removing sleeps

---
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/rstudio/RStudioTestUtils.scala
@@ -0,0 +1,33 @@
+package org.broadinstitute.dsde.workbench.leonardo.rstudio
+
+import org.broadinstitute.dsde.workbench.auth.AuthToken
+import org.broadinstitute.dsde.workbench.leonardo._
+import org.openqa.selenium.WebDriver
+import org.scalatest.Suite
+import scala.concurrent._
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+trait RStudioTestUtils extends LeonardoTestUtils {
+  this: Suite =>
+
+  implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+
+  def withRStudioPage[T](cluster: ClusterCopy)(testCode: RStudioPage => T)(implicit webDriver: WebDriver,
+                                                                           token: AuthToken): T = {
+    val rstudioMainPage = RStudio.get(cluster.googleProject, cluster.clusterName)
+    logger.info(s"rstudio ${rstudioMainPage.url}")
+    testCode(rstudioMainPage.open)
+  }
+
+  def withNewRStudio[T](cluster: ClusterCopy, timeout: FiniteDuration = 5.minutes)(
+    testCode: RStudioPage => T
+  )(implicit webDriver: WebDriver, token: AuthToken): T =
+    withRStudioPage(cluster) { rstudioMainPage =>
+      logger.info(
+        s"Creating new rstudio on cluster ${cluster.googleProject.value} / ${cluster.clusterName.asString}..."
+      )
+      rstudioMainPage.withNewRStudio()(rstudioPage => testCode(rstudioPage))
+    }
+
+}
