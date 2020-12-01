69738240a4e41d52e876780cc6f5dff4c278b1b8
Scala Steward <me@scala-steward.org>
2020-12-01 16:27:06 +0100

    Update liquibase-core to 3.8.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -114,7 +114,7 @@ object Dependencies {
   val slick: ModuleID =           "com.typesafe.slick"  %% "slick"                % slickV excludeAll (excludeTypesafeConfig, excludeReactiveStream)
   val hikariCP: ModuleID =        "com.typesafe.slick"  %% "slick-hikaricp"       % slickV excludeAll (excludeSlf4j)
   val mysql: ModuleID =           "mysql"               % "mysql-connector-java"  % "8.0.18"
-  val liquibase: ModuleID =       "org.liquibase"       % "liquibase-core"        % "3.8.8"
+  val liquibase: ModuleID =       "org.liquibase"       % "liquibase-core"        % "3.8.9"
   val sealerate: ModuleID =       "ca.mrvisser"         %% "sealerate"            % "0.0.6"
   val googleCloudNio: ModuleID =  "com.google.cloud"    % "google-cloud-nio"      % "0.107.0-alpha" % Test // brought in for FakeStorageInterpreter
 
