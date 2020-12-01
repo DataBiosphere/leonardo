2ae67d4d550a39b313960b1e11ae8cbc9ebf3fd0
Scala Steward <me@scala-steward.org>
2020-12-01 16:29:50 +0100

    Update swagger-ui to 3.32.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies {
 
   val logbackClassic: ModuleID =  "ch.qos.logback"              % "logback-classic" % "1.2.3"
   val scalaLogging: ModuleID =    "com.typesafe.scala-logging"  %% "scala-logging"  % scalaLoggingV
-  val swaggerUi: ModuleID =       "org.webjars"                 % "swagger-ui"      % "3.32.1"
+  val swaggerUi: ModuleID =       "org.webjars"                 % "swagger-ui"      % "3.32.5"
   val ficus: ModuleID =           "com.iheart"                  %% "ficus"          % "1.5.0"
   val enumeratum: ModuleID =      "com.beachape"                %% "enumeratum"     % "1.6.1"
 
