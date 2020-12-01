c6ce6a2dee2812dc82f47ad5421fa2cac5a70c04
Scala Steward <me@scala-steward.org>
2020-12-01 16:26:28 +0100

    Update workbench-util to 0.6-8345256-SNAP

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val opencensusV = "0.26.0"
 
   val serviceTestV = "0.18-c5713ac"
-  val workbenchUtilV = "0.6-c5713ac"
+  val workbenchUtilV = "0.6-8345256-SNAP"
   val workbenchModelV = "0.14-c5713ac"
   val workbenchGoogleV = "0.21-c5713ac"
   val workbenchGoogle2V = "0.15-c5713ac"
