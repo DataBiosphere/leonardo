737435c57746ee73ff5310661f12a308061faeb2
Scala Steward <me@scala-steward.org>
2021-01-05 10:12:14 +0100

    Update akka-http-circe from 1.35.2 to 1.35.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -149,7 +149,7 @@ object Dependencies {
     akkaTestKit,
     akkaHttpTestKit,
     akkaStream,
-    "de.heikoseeberger" %% "akka-http-circe" % "1.35.2" excludeAll(excludeAkkaHttp, excludeAkkaStream),
+    "de.heikoseeberger" %% "akka-http-circe" % "1.35.3" excludeAll(excludeAkkaHttp, excludeAkkaStream),
     googleDataproc,
     googleRpc,
     googleErrorReporting, // forcing an older versin of google-cloud-errorreporting because latest version brings in higher version of gax-grpc, which isn't compatible with other google dependencies
