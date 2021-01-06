67c79b00b83f236721956a3433d7de78dec15004
Qi Wang <qi@broadinstitute.org>
2021-01-06 12:47:14 -0500

    ci optimization

--- docker/sql_validate.sh
+++ docker/sql_validate.sh
@@ -3,8 +3,11 @@
 SERVICE=$1
 
 # validate mysql
-echo "sleeping for 60 seconds during mysql boot..."
-sleep 60
+echo "Checking every 3s for mysql to be ready"
+while ! mysqladmin ping --host=mysql --port=3306 --silent; do
+  sleep 3
+done
+
 mysql -uroot -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
 mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()"
 mysql -u${SERVICE}-test -p${SERVICE}-test --host=mysql --port=3306 -e "SELECT VERSION();SELECT NOW()" leotestdb
