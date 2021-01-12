5dea7a6cd69f9277fba83feea484336e0faf298c
Rob Title <rtitle@broadinstitute.org>
2021-01-12 17:58:39 -0500

    Improve test

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeDataprocSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeDataprocSpec.scala
@@ -9,6 +9,7 @@ import org.broadinstitute.dsde.workbench.google2.DataprocRole.{Master, Secondary
 import org.broadinstitute.dsde.workbench.google2.{
   DataprocClusterName,
   GoogleDataprocInterpreter,
+  GoogleDataprocService,
   MachineTypeName,
   RegionName
 }
@@ -16,12 +17,15 @@ import org.broadinstitute.dsde.workbench.leonardo.LeonardoApiClient.defaultCreat
 import org.broadinstitute.dsde.workbench.leonardo.http.RuntimeConfigRequest
 import org.broadinstitute.dsde.workbench.leonardo.notebooks.{NotebookTestUtils, Python3}
 import org.broadinstitute.dsde.workbench.leonardo.{
+  ClusterCopy,
   DataprocClusterStatus,
   DiskSize,
   GPAllocFixtureSpec,
+  LeonardoApiClient,
   LeonardoTestUtils
 }
 import org.broadinstitute.dsde.workbench.model.TraceId
+import org.http4s.client.Client
 import org.http4s.headers.Authorization
 import org.http4s.{AuthScheme, Credentials}
 import org.scalatest.{DoNotDiscover, ParallelTestExecution}
@@ -35,6 +39,11 @@ class RuntimeDataprocSpec
   implicit val authTokenForOldApiClient = ronAuthToken
   implicit val auth: Authorization = Authorization(Credentials.Token(AuthScheme.Bearer, ronCreds.makeAuthToken().value))
   implicit val traceId = Ask.const[IO, TraceId](TraceId(UUID.randomUUID()))
+
+  val dependencies = for {
+    dataprocService <- googleDataprocService
+    httpClient <- LeonardoApiClient.client
+  } yield RuntimeDataprocSpecDependencies(httpClient, dataprocService)
 
   "Workers and preemptible workers should be created" in { project =>
     val runtimeName = randomClusterName
@@ -55,51 +64,56 @@ class RuntimeDataprocSpec
       )
     )
 
-    val res = googleDataprocService.use { dataproc =>
-      withNewRuntime(project, runtimeName, createRuntimeRequest) { runtime =>
-        for {
-          // check cluster status in Dataproc
-          clusterOpt <- dataproc.getCluster(project,
-                                            RegionName("us-central1"),
-                                            DataprocClusterName(runtime.clusterName.asString))
-          cluster <- IO.fromOption(clusterOpt)(
-            fail(s"Cluster not found in dataproc: ${project.value}/${runtime.clusterName.asString}")
-          )
-          status <- IO.fromOption(DataprocClusterStatus.withNameInsensitiveOption(cluster.getStatus.getState.name))(
-            fail(s"Unknown Dataproc status ${cluster.getStatus.getState.name}")
-          )
-          _ <- IO(status shouldBe DataprocClusterStatus.Running)
+    val res = dependencies.use { dep =>
+      implicit val client = dep.httpClient
+      for {
+        // create runtime
+        getRuntimeResponse <- LeonardoApiClient.createRuntimeWithWait(project, runtimeName, createRuntimeRequest)
+        runtime = ClusterCopy.fromGetRuntimeResponseCopy(getRuntimeResponse)
 
-          // check cluster instances in Dataproc
-          instances = GoogleDataprocInterpreter.getAllInstanceNames(cluster)
-          _ <- IO(instances.size shouldBe 3)
-          _ <- instances.toList.traverse {
-            case (k, v) =>
-              IO(
-                k.role match {
-                  case Master => v.size shouldBe 1
-                  case Worker => v.size shouldBe 2
-                  case SecondaryWorker =>
-                    v.size shouldBe 5
-                    k.isPreemptible shouldBe true
-                }
-              )
-          }
+        // check cluster status in Dataproc
+        clusterOpt <- dep.dataproc.getCluster(project,
+                                              RegionName("us-central1"),
+                                              DataprocClusterName(runtime.clusterName.asString))
+        cluster <- IO.fromOption(clusterOpt)(
+          fail(s"Cluster not found in dataproc: ${project.value}/${runtime.clusterName.asString}")
+        )
+        status <- IO.fromOption(DataprocClusterStatus.withNameInsensitiveOption(cluster.getStatus.getState.name))(
+          fail(s"Unknown Dataproc status ${cluster.getStatus.getState.name}")
+        )
+        _ <- IO(status shouldBe DataprocClusterStatus.Running)
 
-          // check output of yarn node -list command
-          _ <- IO(
-            withWebDriver { implicit driver =>
-              withNewNotebook(runtime, Python3) { notebookPage =>
-                val output = notebookPage.executeCell("""!yarn node -list | grep Total""")
-                output.get should include("Total Nodes:7")
+        // check cluster instances in Dataproc
+        instances = GoogleDataprocInterpreter.getAllInstanceNames(cluster)
+        _ <- IO(instances.size shouldBe 3)
+        _ <- instances.toList.traverse {
+          case (k, v) =>
+            IO(
+              k.role match {
+                case Master => v.size shouldBe 1
+                case Worker => v.size shouldBe 2
+                case SecondaryWorker =>
+                  v.size shouldBe 5
+                  k.isPreemptible shouldBe true
               }
+            )
+        }
+
+        // check output of yarn node -list command
+        _ <- IO(
+          withWebDriver { implicit driver =>
+            withNewNotebook(runtime, Python3) { notebookPage =>
+              val output = notebookPage.executeCell("""!yarn node -list | grep Total""")
+              output.get should include("Total Nodes:7")
             }
-          )
-        } yield ()
-      }
+          }
+        )
+      } yield ()
     }
 
     res.unsafeRunSync()
   }
 
 }
+
+final case class RuntimeDataprocSpecDependencies(httpClient: Client[IO], dataproc: GoogleDataprocService[IO])
