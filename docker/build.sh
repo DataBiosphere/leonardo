9c5551702e62de0db6f1542112ab6f4f8455b733
Denis Loginov <dinvlad@gmail.com>
2021-01-12 11:41:36 -0500

    Add AppSec GitHub Trivy Action

--- docker/build.sh
+++ docker/build.sh
@@ -169,10 +169,6 @@ function docker_cmd()
 
         docker build -t "${DEFAULT_IMAGE}:${DOCKER_TAG}" .
 
-        echo "scanning docker image..."
-        docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$HOME"/Library/Caches:/root/.cache/ aquasec/trivy --exit-code 1 --severity CRITICAL "${DEFAULT_IMAGE}":"${DOCKER_TAG}"
-
-
         echo "building $TESTS_IMAGE docker image..."
         docker build -f Dockerfile-tests -t "${TESTS_IMAGE}:${DOCKER_TAG_TESTS}" .
 
