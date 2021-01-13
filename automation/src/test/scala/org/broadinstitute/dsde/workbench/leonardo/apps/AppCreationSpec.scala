acedfaf250445a6eb6f0e221b17742dacd4b2b6c
jdcanas <jcanas@broadinstitute.com>
2021-01-13 13:54:59 -0500

    cleanup

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/apps/AppCreationSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/apps/AppCreationSpec.scala
@@ -134,7 +134,7 @@ class AppCreationSpec extends GPAllocFixtureSpec with LeonardoTestUtils with GPA
             getApp,
             180,
             10 seconds,
-            s"AppCreationSpec: app ${googleProject.value}/${appName.value} did not finish stopping after 20 minutes"
+            s"AppCreationSpec: app ${googleProject.value}/${appName.value} did not finish stopping after 30 minutes"
           )(implicitly, implicitly, appInStateOrError(AppStatus.Stopped))
           _ <- loggerIO.info(
             s"AppCreationSpec: app ${googleProject.value}/${appName.value} stop result: $monitorStopResult"
