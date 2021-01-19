29dbbdaf61e0f1ee907dab9c896b3e696a24ee60
Rob Title <rtitle@broadinstitute.org>
2021-01-19 13:42:45 -0500

    Tolerate gpalloc release errors

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoSuite.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoSuite.scala
@@ -66,7 +66,7 @@ trait GPAllocUtils extends BillingFixtures with LeonardoTestUtils {
                                                       ronEmail,
                                                       BillingProject.BillingProjectRole.User)(hermioneAuthToken)
       )
-      _ <- IO(logger.info(s"Billing project claimed: ${claimedBillingProject.projectName}"))
+      _ <- loggerIO.info(s"Billing project claimed: ${claimedBillingProject.projectName}")
     } yield GoogleProject(claimedBillingProject.projectName)
 
   /**
@@ -80,18 +80,21 @@ trait GPAllocUtils extends BillingFixtures with LeonardoTestUtils {
             hermioneAuthToken
           )
       )
-      _ <- IO(releaseGPAllocProject(project.value, hermioneCreds))
-      _ <- IO(logger.info(s"Billing project released: ${project.value}"))
+      releaseProject <- IO(releaseGPAllocProject(project.value, hermioneCreds)).attempt
+      _ <- releaseProject match {
+        case Left(e) => loggerIO.warn(e)(s"Failed to release billing project: ${project.value}")
+        case _       => loggerIO.info(s"Billing project released: ${project.value}")
+      }
     } yield ()
 
   def withNewProject[T](testCode: GoogleProject => IO[T]): T = {
     val test = for {
-      _ <- IO(logger.info("Allocating a new single-test project"))
+      _ <- loggerIO.info("Allocating a new single-test project")
       project <- claimProject()
-      _ <- IO(logger.info(s"Single test project $project claimed"))
+      _ <- loggerIO.info(s"Single test project $project claimed")
       t <- testCode(project)
+      _ <- loggerIO.info(s"Releasing single-test project: ${project.value}")
       _ <- unclaimProject(project)
-      _ <- IO(logger.info(s"releasing single-test project: ${project.value}"))
     } yield t
 
     test.unsafeRunSync()
@@ -104,7 +107,7 @@ trait GPAllocBeforeAndAfterAll extends GPAllocUtils with BeforeAndAfterAll {
   override def beforeAll(): Unit = {
     val res = for {
       _ <- IO(super.beforeAll())
-      _ <- IO(logger.info(s"Running GPAllocBeforeAndAfterAll beforeAll"))
+      _ <- loggerIO.info(s"Running GPAllocBeforeAndAfterAll beforeAll")
       claimAttempt <- claimProject().attempt
       _ <- claimAttempt match {
         case Left(e) => IO(sys.props.put(gpallocProjectKey, gpallocErrorPrefix + e.getMessage))
@@ -119,12 +122,12 @@ trait GPAllocBeforeAndAfterAll extends GPAllocUtils with BeforeAndAfterAll {
   override def afterAll(): Unit = {
     val res = for {
       shouldUnclaimProp <- IO(sys.props.get(shouldUnclaimProjectsKey))
-      _ <- IO(logger.info(s"Running GPAllocBeforeAndAfterAll afterAll ${shouldUnclaimProjectsKey}: $shouldUnclaimProp"))
+      _ <- loggerIO.info(s"Running GPAllocBeforeAndAfterAll afterAll ${shouldUnclaimProjectsKey}: $shouldUnclaimProp")
       projectProp <- IO(sys.props.get(gpallocProjectKey))
       project = projectProp.filterNot(_.startsWith(gpallocErrorPrefix)).map(GoogleProject)
       _ <- if (shouldUnclaimProp != Some("false")) {
         project.traverse(p => deleteInitialRuntime(p) >> unclaimProject(p))
-      } else IO(logger.info(s"Not going to release project: ${projectProp} due to error happened"))
+      } else loggerIO.info(s"Not going to release project: ${projectProp} due to error happened")
       _ <- IO(sys.props.remove(gpallocProjectKey))
       _ <- IO(super.afterAll())
     } yield ()
@@ -149,13 +152,12 @@ trait GPAllocBeforeAndAfterAll extends GPAllocUtils with BeforeAndAfterAll {
           .attempt
         _ <- res match {
           case Right(_) =>
-            IO(logger.info(s"Created initial runtime ${project.value} / ${initalRuntimeName.asString}"))
+            loggerIO.info(s"Created initial runtime ${project.value} / ${initalRuntimeName.asString}")
           case Left(err) =>
-            IO(
-              logger
-                .warn(s"Failed to create initial runtime ${project.value} / ${initalRuntimeName.asString} with error",
-                      err)
-            )
+            loggerIO
+              .warn(err)(
+                s"Failed to create initial runtime ${project.value} / ${initalRuntimeName.asString} with error"
+              )
         }
       } yield ()
     }
@@ -172,12 +174,12 @@ trait GPAllocBeforeAndAfterAll extends GPAllocUtils with BeforeAndAfterAll {
           .attempt
         _ <- res match {
           case Right(_) =>
-            IO(logger.info(s"Deleted initial runtime ${project.value} / ${initalRuntimeName.asString}"))
+            loggerIO.info(s"Deleted initial runtime ${project.value} / ${initalRuntimeName.asString}")
           case Left(err) =>
             IO(
-              logger
-                .warn(s"Failed to delete initial runtime ${project.value} / ${initalRuntimeName.asString} with error",
-                      err)
+              loggerIO.warn(err)(
+                s"Failed to delete initial runtime ${project.value} / ${initalRuntimeName.asString} with error"
+              )
             )
         }
       } yield ()
