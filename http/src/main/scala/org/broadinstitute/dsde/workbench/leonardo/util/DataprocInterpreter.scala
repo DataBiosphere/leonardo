4fdd95f6c942d37b56fc46bde521ddce295d7587
Justin Canas <justincanas16@gmail.com>
2021-02-03 10:15:24 -0500

    [IA-2441] Use google2 billing (#1852)
    * use billing2
    
    * rename
    
    * fix test
    
    * feedback + wblibs bump
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/DataprocInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/DataprocInterpreter.scala
@@ -23,6 +23,7 @@ import org.broadinstitute.dsde.workbench.google2.{
   GoogleComputeService,
   GoogleDataprocService,
   GoogleDiskService,
+  GoogleResourceService,
   MachineTypeName,
   ZoneName
 }
@@ -49,8 +50,11 @@ final case class ClusterIamSetupException(googleProject: GoogleProject)
 final case class GoogleGroupCreationException(googleGroup: WorkbenchEmail, msg: String)
     extends LeoException(s"Failed to create the Google group '${googleGroup}': $msg", StatusCodes.InternalServerError)
 
+final case class GoogleProjectNotFoundException(project: GoogleProject)
+    extends LeoException(s"Google did not have any record of project: $project.", StatusCodes.NotFound)
+
 final case object ImageProjectNotFoundException
-    extends LeoException("Custom Dataproc image project not found", StatusCodes.NotFound)
+    extends LeoException("Custom Dataproc image project not found.", StatusCodes.NotFound)
 
 final case class ClusterResourceConstaintsException(clusterProjectAndName: RuntimeProjectAndName,
                                                     machineType: MachineTypeName)
@@ -67,7 +71,7 @@ class DataprocInterpreter[F[_]: Timer: Parallel: ContextShift](
   googleDiskService: GoogleDiskService[F],
   googleDirectoryDAO: GoogleDirectoryDAO,
   googleIamDAO: GoogleIamDAO,
-  googleProjectDAO: GoogleProjectDAO,
+  googleResourceService: GoogleResourceService[F],
   welderDao: WelderDAO[F],
   blocker: Blocker
 )(implicit val F: Async[F],
@@ -424,12 +428,9 @@ class DataprocInterpreter[F[_]: Timer: Parallel: ContextShift](
           F.unit
         } else {
           for {
-            projectNumberOptIO <- F.liftIO(
-              IO.fromFuture(IO(googleProjectDAO.getProjectNumber(googleProject.value)))
-            )
-            projectNumber <- F.liftIO(
-              IO.fromEither(projectNumberOptIO.toRight(ClusterIamSetupException(imageProject)))
-            )
+            projectNumberOpt <- googleResourceService.getProjectNumber(googleProject)
+
+            projectNumber <- F.fromEither(projectNumberOpt.toRight(GoogleProjectNotFoundException(googleProject)))
             // Note that the Dataproc service account is used to retrieve the image, and not the user's
             // pet service account. There is one Dataproc service account per Google project. For more details:
             // https://cloud.google.com/dataproc/docs/concepts/iam/iam#service_accounts
