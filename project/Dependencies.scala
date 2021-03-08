3a953feac1074c51c799e615690edc5011f1a747
Scala Steward <me@scala-steward.org>
2021-03-08 20:12:23 +0100

    Update circe-yaml from 0.12.0 to 0.13.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -111,7 +111,7 @@ object Dependencies {
   val googleCloudNio: ModuleID =  "com.google.cloud"    % "google-cloud-nio"      % "0.122.5" % Test // brought in for FakeStorageInterpreter
 
   val http4sCirce =       "org.http4s"        %% "http4s-circe"         % http4sVersion
-  val circeYaml =         "io.circe"          %% "circe-yaml"           % "0.12.0"
+  val circeYaml =         "io.circe"          %% "circe-yaml"           % "0.13.1"
   val http4sBlazeClient = "org.http4s"        %% "http4s-blaze-client"  % http4sVersion
   val http4sDsl =         "org.http4s"        %% "http4s-dsl"           % http4sVersion
   val guava: ModuleID =   "com.google.guava"  % "guava"                 % guavaV
