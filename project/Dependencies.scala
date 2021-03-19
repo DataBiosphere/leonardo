5b0ff83e7c8d952196275a537df7b83faef194df
Qi Wang <qi@broadinstitute.org>
2021-03-19 11:28:57 -0400

    use updated wb-libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val monocleV = "2.1.0"
   val opencensusV = "0.28.3"
 
-  private val workbenchLibsHash = "9d5dce0"
+  private val workbenchLibsHash = "0297f0b"
   val serviceTestV = s"0.18-$workbenchLibsHash"
   val workbenchModelV = s"0.14-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
