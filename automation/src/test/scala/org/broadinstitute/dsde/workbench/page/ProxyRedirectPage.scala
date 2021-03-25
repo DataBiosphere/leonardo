9d47437bb1ce76fb848bdb24007a4486926985fe
Rob Title <rtitle@broadinstitute.org>
2021-03-24 20:50:35 -0400

    Anonymously retry

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/page/ProxyRedirectPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/page/ProxyRedirectPage.scala
@@ -7,24 +7,34 @@ import org.broadinstitute.dsde.workbench.service.test.WebBrowserUtil
 import org.openqa.selenium.WebDriver
 import org.scalatestplus.selenium.Page
 
+import scala.util.{Failure, Success, Try}
+
 trait ProxyRedirectPage[P <: Page] extends Page with PageUtil[P] with WebBrowserUtil with LazyLogging { self: P =>
   implicit val authToken: AuthToken
 
   override def open(implicit webDriver: WebDriver): P = {
-    // 1. Go to the target page. This will initially return 401.
+    // Go to the target page. This will initially return 401.
     go.to(this)
 
-    // 2. Set the LeoToken cookie. Note this needs to be done from the same domain as the target page.
+    // Set the LeoToken cookie. Note this needs to be done from the same domain as the target page.
     addCookie("LeoToken", authToken.value)
 
-    // 3. Go to the proxy redirect page, specifying the target page as the `rurl`. This will automatically
-    //    redirect to the target page. This is done so the Referer is set correctly.
-    go.to(ProxyRedirectClient.get(url))
+    val proxyRedirectPage = ProxyRedirectClient.get(url)
 
-    // 4. Wait for the target page to load.
-    awaitLoaded()
+    Try {
+      // Go to the proxy redirect page, specifying the target page as the `rurl`. This will automatically
+      // redirect to the target page. This is done so the Referer is set correctly.
+      go.to(proxyRedirectPage)
 
-    this
+      // Wait for the target page to load.
+      awaitLoaded()
+    } match {
+      // Anonymously retry failures
+      case Failure(_) =>
+        go.to(proxyRedirectPage)
+        awaitLoaded()
+      case Success(res) => res
+    }
   }
 
 }
