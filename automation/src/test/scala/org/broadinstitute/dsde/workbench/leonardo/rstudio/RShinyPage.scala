27b2b7bdc74b5f5bc8ca7e42bedce6ad62994feb
Rob Title <rtitle@broadinstitute.org>
2021-03-25 15:39:49 -0400

    Revert "[no ticket] Rt revert referer (#1952)"
    This reverts commit 0828f9dd0f06965a99841797824b4819ad552309.

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/rstudio/RShinyPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/rstudio/RShinyPage.scala
@@ -1,13 +1,17 @@
 package org.broadinstitute.dsde.workbench.leonardo.rstudio
 
-import org.broadinstitute.dsde.workbench.auth.AuthToken
-import org.broadinstitute.dsde.workbench.page.CookieAuthedPage
+import org.broadinstitute.dsde.workbench.page.PageUtil
+import org.broadinstitute.dsde.workbench.service.test.WebBrowserUtil
 import org.openqa.selenium.WebDriver
+import org.scalatestplus.selenium.Page
 
 import scala.concurrent.duration._
 
-class RShinyPage(val url: String)(implicit override val authToken: AuthToken, implicit val webDriver: WebDriver)
-    extends CookieAuthedPage[RShinyPage] {
+class RShinyPage(val url: String)(implicit val webDriver: WebDriver)
+    extends Page
+    with PageUtil[RShinyPage]
+    with WebBrowserUtil {
+
   private val exampleHeader: Query = cssSelector("#showcase-app-container > div > h2")
 
   override def awaitLoaded(): RShinyPage = {
---
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/page/ProxyRedirectPage.scala
@@ -0,0 +1,49 @@
+package org.broadinstitute.dsde.workbench.page
+
+import cats.effect.{IO, Timer}
+import com.typesafe.scalalogging.LazyLogging
+import org.broadinstitute.dsde.workbench.auth.AuthToken
+import org.broadinstitute.dsde.workbench.leonardo.ProxyRedirectClient
+import org.broadinstitute.dsde.workbench.service.test.WebBrowserUtil
+import org.openqa.selenium.WebDriver
+import org.scalatestplus.selenium.Page
+
+import scala.concurrent.duration._
+
+trait ProxyRedirectPage[P <: Page] extends Page with PageUtil[P] with WebBrowserUtil with LazyLogging { self: P =>
+  implicit val authToken: AuthToken
+  implicit val timer: Timer[IO]
+
+  override def open(implicit webDriver: WebDriver): P = {
+    val res = for {
+      // Go to the target page. This will initially return 401.
+      _ <- IO(go.to(this))
+
+      // Set the LeoToken cookie. Note this needs to be done from the same domain as the target page.
+      _ <- IO(addCookie("LeoToken", authToken.value))
+
+      // Go to the proxy redirect page, specifying the target page as the `rurl`. This will automatically
+      // redirect to the target page. This is done so the Referer is set correctly.
+      redirect = for {
+        _ <- IO(go.to(ProxyRedirectClient.get(url)))
+
+        // Wait for the target page to load after redirect.
+        res <- IO(awaitLoaded())
+      } yield res
+
+      // Retry above operation
+      res <- fs2.Stream
+        .retry(
+          redirect,
+          2 seconds,
+          _ * 2,
+          3
+        )
+        .compile
+        .lastOrError
+    } yield res
+
+    res.unsafeRunSync()
+  }
+
+}
