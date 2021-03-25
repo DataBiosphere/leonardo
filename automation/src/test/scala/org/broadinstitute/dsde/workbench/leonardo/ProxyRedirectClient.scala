0aa92d77717d9ecdf740714cd13b36a9f9983bee
Rob Title <rtitle@broadinstitute.org>
2021-03-25 12:13:05 -0400

    Add more debugging

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/ProxyRedirectClient.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/ProxyRedirectClient.scala
@@ -5,6 +5,7 @@ import com.typesafe.scalalogging.LazyLogging
 import fs2._
 import org.broadinstitute.dsde.workbench.service.RestClient
 import org.broadinstitute.dsde.workbench.util2.ExecutionContexts
+import org.http4s.client.Client
 import org.http4s.dsl.io._
 import org.http4s.headers.`Content-Type`
 import org.http4s.implicits._
@@ -19,6 +20,29 @@ object ProxyRedirectClient extends RestClient with LazyLogging {
 
   def get(rurl: String): String =
     s"http://$host:$port/proxyRedirectClient?rurl=${rurl}"
+
+  def testConnection(rurl: String)(implicit client: Client[IO]): IO[Unit] =
+    for {
+      r <- client
+        .run(
+          Request[IO](
+            method = Method.GET,
+            uri = Uri.unsafeFromString(get(rurl))
+          )
+        )
+        .use { resp =>
+          if (!resp.status.isSuccess) {
+            onError(s"Could not load proxy redirect page at url: ${get(rurl)}")(resp)
+              .flatMap(IO.raiseError)
+          } else
+            IO.unit
+        }
+    } yield r
+
+  private def onError(message: String)(response: Response[IO]): IO[Throwable] =
+    for {
+      body <- response.bodyText.compile.foldMonoid
+    } yield RestError(message, response.status, Some(body))
 
   private def getContent(rurl: String, blocker: Blocker)(implicit cs: ContextShift[IO]): Stream[IO, Byte] =
     io.readInputStream(IO(getClass.getClassLoader.getResourceAsStream("redirect-proxy-page.html")), 4096, blocker)
--- automation/src/test/scala/org/broadinstitute/dsde/workbench/page/ProxyRedirectPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/page/ProxyRedirectPage.scala
@@ -3,7 +3,7 @@ package org.broadinstitute.dsde.workbench.page
 import cats.effect.{IO, Timer}
 import com.typesafe.scalalogging.LazyLogging
 import org.broadinstitute.dsde.workbench.auth.AuthToken
-import org.broadinstitute.dsde.workbench.leonardo.ProxyRedirectClient
+import org.broadinstitute.dsde.workbench.leonardo.{LeonardoApiClient, ProxyRedirectClient}
 import org.broadinstitute.dsde.workbench.service.test.WebBrowserUtil
 import org.openqa.selenium.WebDriver
 import org.scalatestplus.selenium.Page
@@ -16,19 +16,36 @@ trait ProxyRedirectPage[P <: Page] extends Page with PageUtil[P] with WebBrowser
 
   override def open(implicit webDriver: WebDriver): P = {
     val res = for {
+      _ <- IO(logger.info(s"Setting LeoToken cookie for page ${url}"))
+
       // Go to the target page. This will initially return 401.
       _ <- IO(go.to(this))
 
       // Set the LeoToken cookie. Note this needs to be done from the same domain as the target page.
       _ <- IO(addCookie("LeoToken", authToken.value))
 
+      _ <- IO(logger.info(s"Testing connection to ${ProxyRedirectClient.get(url)}"))
+
+      // Test connection to the proxy redirect server before loading it in WebDriver
+      _ <- LeonardoApiClient.client.use { implicit client =>
+        fs2.Stream.retry(ProxyRedirectClient.testConnection(url), 2 seconds, _ * 2, 5).compile.lastOrError
+      }
+
+      _ <- IO(logger.info(s"Proxy redirect server is up at ${ProxyRedirectClient.get(url)}!"))
+
       // Go to the proxy redirect page, specifying the target page as the `rurl`. This will automatically
       // redirect to the target page. This is done so the Referer is set correctly.
       redirect = for {
+        _ <- IO(logger.info(s"Going to redirect page at url ${ProxyRedirectClient.get(url)}"))
+
         _ <- IO(go.to(ProxyRedirectClient.get(url)))
+
+        _ <- IO(logger.info(s"Waiting for redirect page at url ${ProxyRedirectClient.get(url)} to redirect"))
 
         // Wait for the target page to load after redirect.
         res <- IO(awaitLoaded())
+
+        _ <- IO(logger.info(s"Successfully redirected to ${url}!"))
       } yield res
 
       // Retry above operation
