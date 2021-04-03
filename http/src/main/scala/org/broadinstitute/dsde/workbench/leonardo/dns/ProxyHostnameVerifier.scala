8f42e26a1b1754d63f20226d591817f06b76a582
Robert Title <rtitle@broadinstitute.org>
2021-04-03 09:54:08 -0400

    Comment

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dns/ProxyHostnameVerifier.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dns/ProxyHostnameVerifier.scala
@@ -11,25 +11,32 @@ import sun.security.util.HostnameChecker
 import java.security.cert.X509Certificate
 import javax.net.ssl.{HostnameVerifier, SSLSession}
 
+/**
+ * Implementation of javax.net.ssl.HostnameVerifier which delegates to
+ * sun.security.util.HostnameChecker, but uses the Leo DNS cache to resolve IPs.
+ */
 class ProxyHostnameVerifier extends HostnameVerifier {
   def hostnameChecker: HostnameChecker = HostnameChecker.getInstance(HostnameChecker.TYPE_TLS)
 
   override def verify(hostname: String, session: SSLSession): Boolean = {
     val res = for {
+      // Consult our cache if the host is an IP; otherwise use it directly.
       hostOpt <- if (IPAddressUtil.isIPv4LiteralAddress(hostname) || IPAddressUtil.isIPv6LiteralAddress(hostname)) {
-        IPToHostMapping.ipToHostMapping.get.map(_.get(IP(hostname)).map(_.address))
+        val key = IP(hostname)
+        IPToHostMapping.ipToHostMapping.get.map(_.get(key).map(_.address))
       } else {
         IO.pure(Some(hostname))
       }
 
+      // Resolve the server certificate being used
+      certOpt <- IO(session.getPeerCertificates).attempt.map {
+        case Right(Array(cert: X509Certificate, _*)) => Some(cert)
+        case _                                       => None
+      }
+
+      // Call the JDK HostnameChecker to do the verification
       res <- hostOpt.flatTraverse { host =>
-        val certOpt = session.getPeerCertificates match {
-          case Array(cert: X509Certificate, _*) => Some(cert)
-          case _                                => None
-        }
-
         certOpt.traverse(cert => IO(hostnameChecker.`match`(host, cert)).attempt.map(_.isRight))
-
       }
     } yield res.getOrElse(false)
 
@@ -39,5 +46,4 @@ class ProxyHostnameVerifier extends HostnameVerifier {
 
 object IPToHostMapping {
   private[dns] val ipToHostMapping: Ref[IO, Map[IP, Host]] = Ref.unsafe(Map.empty)
-
 }
