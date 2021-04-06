d6953cf71aa789b7d989f9bbc18fa0996b5e55ba
Kaan Yuksel <kyuksel@broadinstitute.org>
2021-04-06 15:52:42 -0400

    Remove getMinimalByUser that's no longer used

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/db/NodepoolComponent.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/db/NodepoolComponent.scala
@@ -187,15 +187,6 @@ object nodepoolQuery extends TableQuery(new NodepoolTable(_)) {
       nodepools <- findByNodepoolIdQuery(id).result
     } yield nodepools.map(rec => unmarshalNodepool(rec, List())).headOption
 
-  def getMinimalByUser(creator: WorkbenchEmail,
-                       project: GoogleProject)(implicit ec: ExecutionContext): DBIO[Option[Nodepool]] =
-    for {
-      clusters <- kubernetesClusterQuery.joinMinimalClusterAndUnmarshal(
-        kubernetesClusterQuery.findActiveByNameQuery(project),
-        nodepoolQuery.filter(_.destroyedDate === dummyDate).filterNot(_.isDefault).filter(_.creator === creator)
-      )
-    } yield clusters.flatMap(_.nodepools).headOption
-
   def getMinimalByUserAndConfig(creator: WorkbenchEmail, project: GoogleProject, config: KubernetesRuntimeConfig)(
     implicit ec: ExecutionContext
   ): DBIO[Option[Nodepool]] =
