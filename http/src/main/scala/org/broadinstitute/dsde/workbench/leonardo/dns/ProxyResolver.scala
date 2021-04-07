802883c4812a9b6995341d4b34a7dd22e38ef7af
Robert Title <rtitle@broadinstitute.org>
2021-04-07 17:21:25 -0400

    Make ProxyResolver fall through to regular host names

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dns/ProxyResolver.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dns/ProxyResolver.scala
@@ -6,7 +6,6 @@ import cats.effect.concurrent.Ref
 import cats.effect.implicits._
 import cats.implicits._
 import org.broadinstitute.dsde.workbench.leonardo.config.ProxyConfig
-import org.broadinstitute.dsde.workbench.leonardo.model.LeoException
 import org.broadinstitute.dsde.workbench.model.IP
 import org.http4s.client.RequestKey
 
@@ -25,27 +24,20 @@ class ProxyResolverInterp[F[_]](proxyConfig: ProxyConfig, hostToIpMapping: Ref[F
   implicit F: Effect[F]
 ) extends ProxyResolver[F] {
 
-  override def resolveHttp4s(requestKey: RequestKey): Either[Throwable, InetSocketAddress] = {
-    val res = hostToIpMapping.get.map { mapping =>
-      mapping
-        .get(Host(requestKey.authority.host.value))
-        .map(ip =>
-          InetSocketAddress.createUnresolved(ip.asString, requestKey.authority.port.getOrElse(proxyConfig.proxyPort))
-        )
-        .toRight(IpNotFoundException(requestKey.authority.host.value))
+  override def resolveHttp4s(requestKey: RequestKey): Either[Throwable, InetSocketAddress] =
+    resolveInternal(requestKey.authority.host.value, requestKey.authority.port.getOrElse(proxyConfig.proxyPort))
+      .map(_.asRight)
+      .toIO
+      .unsafeRunSync()
+
+  override def resolveAkka(host: String, port: Int): Future[InetSocketAddress] =
+    resolveInternal(host, port).toIO.unsafeToFuture()
+
+  private def resolveInternal(host: String, port: Int): F[InetSocketAddress] =
+    hostToIpMapping.get.map { mapping =>
+      mapping.get(Host(host)) match {
+        case Some(ip) => InetSocketAddress.createUnresolved(ip.asString, port)
+        case _        => InetSocketAddress.createUnresolved(host, port)
+      }
     }
-    res.toIO.unsafeRunSync()
-  }
-
-  override def resolveAkka(host: String, port: Int): Future[InetSocketAddress] = {
-    val res = for {
-      mapping <- hostToIpMapping.get
-      ipOpt = mapping.get(Host(host)).map(ip => InetSocketAddress.createUnresolved(ip.asString, port))
-      res <- F.fromOption(ipOpt, IpNotFoundException(host))
-    } yield res
-    res.toIO.unsafeToFuture()
-  }
 }
-
-case class IpNotFoundException(hostname: String)
-    extends LeoException(s"IP mapping not found for host ${hostname}", traceId = None)
