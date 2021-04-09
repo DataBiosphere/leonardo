c6817cc5170e9af3833e9f0b51f9fd8a3ea381b6
jdcanas <jcanas@broadinstitute.com>
2021-04-09 08:19:26 -0400

    ctx

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/monitor/NonLeoMessageSubscriber.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/monitor/NonLeoMessageSubscriber.scala
@@ -86,7 +86,7 @@ class NonLeoMessageSubscriber[F[_]: Timer](gkeAlg: GKEAlgebra[F],
     for {
       ctx <- ev.ask
       clusterId = msg.clusterId
-      _ <- logger.info(s"Beginning to delete kubernetes cluster ${clusterId} in project ${msg.project} from NonLeoMessageSubscriber | trace id: ${ctx.traceId}")
+      _ <- logger.info(ctx.loggingCtx)(s"Beginning to delete kubernetes cluster ${clusterId} in project ${msg.project} from NonLeoMessageSubscriber")
       _ <- kubernetesClusterQuery.markPendingDeletion(clusterId).transaction
       _ <- gkeAlg
       // TODO: Should we retry failures and with what RetryConfig? If all retries fail, send an alert?
