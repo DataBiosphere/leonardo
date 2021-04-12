3b73c2be6701e6153f781246324adc7570368e8f
Qi Wang <qi@broadinstitute.org>
2021-04-12 11:45:37 -0400

    fix automation test

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeGceSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeGceSpec.scala
@@ -2,12 +2,12 @@ package org.broadinstitute.dsde.workbench.leonardo
 package runtimes
 
 import java.util.UUID
-
 import cats.effect.IO
 import cats.mtl.Ask
+import org.broadinstitute.dsde.workbench.google2.Generators.genDiskName
 import org.broadinstitute.dsde.workbench.google2.{MachineTypeName, ZoneName}
 import org.broadinstitute.dsde.workbench.leonardo.LeonardoApiClient.defaultCreateRuntime2Request
-import org.broadinstitute.dsde.workbench.leonardo.http.RuntimeConfigRequest
+import org.broadinstitute.dsde.workbench.leonardo.http.{PersistentDiskRequest, RuntimeConfigRequest}
 import org.broadinstitute.dsde.workbench.leonardo.notebooks.NotebookTestUtils
 import org.broadinstitute.dsde.workbench.model.TraceId
 import org.http4s.headers.Authorization
@@ -26,6 +26,7 @@ class RuntimeGceSpec
 
   "should create a GCE instance in a non-default zone" in { project =>
     val runtimeName = randomClusterName
+    val diskName = genDiskName.sample.get
     val targetZone = ZoneName(
       "europe-west1-b"
     )
@@ -33,9 +34,14 @@ class RuntimeGceSpec
     // In a europe zone
     val createRuntimeRequest = defaultCreateRuntime2Request.copy(
       runtimeConfig = Some(
-        RuntimeConfigRequest.GceConfig(
+        RuntimeConfigRequest.GceWithPdConfig(
           Some(MachineTypeName("n1-standard-4")),
-          Some(DiskSize(10)),
+          PersistentDiskRequest(
+            diskName,
+            None,
+            None,
+            Map.empty
+          ),
           Some(targetZone)
         )
       )
@@ -44,7 +50,7 @@ class RuntimeGceSpec
       implicit val httpClient = c
       for {
         getRuntimeResponse <- LeonardoApiClient.createRuntimeWithWait(project, runtimeName, createRuntimeRequest)
-        _ = getRuntimeResponse.runtimeConfig.asInstanceOf[RuntimeConfig.GceConfig].zone shouldBe targetZone
+        _ = getRuntimeResponse.runtimeConfig.asInstanceOf[RuntimeConfig.GceWithPdConfig].zone shouldBe targetZone
         disk <- LeonardoApiClient.getDisk(project, getRuntimeResponse.diskConfig.get.name)
         _ = disk.zone shouldBe targetZone
         _ <- LeonardoApiClient.deleteRuntime(project, runtimeName)
