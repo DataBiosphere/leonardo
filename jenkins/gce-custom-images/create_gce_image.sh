c4198b652db0f57bbd83779f26fae4fe308eddfc
Kaan Yuksel <kyuksel@broadinstitute.org>
2021-05-17 09:49:58 -0400

    Rename files

--- jenkins/gce-custom-images/create_gce_image.sh
+++ jenkins/gce-custom-images/create_gce_image.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-# This script is a wrapper around the command that runs the custom GCE image
+# This script is a wrapper around the command that runs the GCE VM snapshot
 # creation tool - Daisy - for local development and documentation purposes.
 #
 # It should be run from the root of the Leonardo repo.
@@ -11,10 +11,7 @@ set -e -x
 #
 # application_default_credentials.json needs to be copied to jenkins/gce-custom-images/ which is mounted on Daisy container
 # Credentials can be refreshed via 'gcloud auth application-default login' with project set to 'broad-dsde-dev' using
-# Broad account
-#
-# Also make sure that you have the right version of the hardening repo via:
-# `git -C jenkins/gce-custom-images/dsp-appsec-base-image-hardening checkout <desired version hash>`
+# Broad account. They are saved at '~/.config/gcloud/application_default_credentials.json' by default.
 
 # Set this to "true" if you want to validate the workflow without actually executing it
 VALIDATE_WORKFLOW="false"
@@ -24,8 +21,8 @@ VALIDATE_WORKFLOW="false"
 # `~/.config/gcloud/application_default_credentials.json` by default.
 SOURCE_DIR="/Users/kyuksel/github/leonardo/jenkins/gce-custom-images"
 
-# Underscores are not accepted as image name
-OUTPUT_IMAGE_NAME=leo-custom-gce-image-$(whoami)-$(date +"%Y-%m-%d-%H-%M-%S")
+# Underscores are not accepted as snapshot name
+OUTPUT_SNAPSHOT_NAME=leo-gce-snapshot-$(whoami)-$(date +"%Y-%m-%d-%H-%M-%S")
 
 PROJECT="broad-dsde-dev"
 REGION="us-central1"
@@ -34,10 +31,17 @@ ZONE="${REGION}-a"
 # The bucket that Daisy uses as scratch area to store source and log files.
 # If it doesn't exist, we create it prior to launching Daisy and
 # the Daisy workflow cleans up all but daisy.log at the end.
-DAISY_BUCKET_PATH="gs://test-leo-custom-gce-image-daisy-scratch-bucket"
+DAISY_BUCKET_PATH="gs://test-leo-gce-snapshot-daisy-scratch-bucket"
 
 # Set this to the tag of the Daisy image you had pulled
 DAISY_IMAGE_TAG="release"
+
+# When updating, to find the resource path:
+#    1. run `gcloud compute images list | grep cos` to get the list of available container-optimized OS images
+#    2. select the image of interest, say, `cos-89-16108-403-22`
+#    3. run `gcloud compute images describe cos-89-16108-403-22 --project cos-cloud | grep selfLink`
+#    4. extract the segments starting with 'projects'
+BASE_IMAGE="projects/cos-cloud/global/images/cos-89-16108-403-22"
 
 # Create the Daisy scratch bucket if it doesn't exist. The Daisy workflow will clean it up at the end.
 gsutil ls $DAISY_BUCKET_PATH || gsutil mb -b on -p $PROJECT -l $REGION $DAISY_BUCKET_PATH
@@ -55,14 +59,11 @@ docker run -it --rm -v "$SOURCE_DIR":/gce-custom-images \
   -gcs_path $DAISY_BUCKET_PATH \
   -default_timeout 60m \
   -oauth /gce-custom-images/application_default_credentials.json \
-  -var:base_image projects/debian-cloud/global/images/debian-9-stretch-v20200210 \
-  -var:output_image "$OUTPUT_IMAGE_NAME" \
-  -var:gce_custom_images_dir /gce-custom-images \
-  -var:installation_script_name prepare_custom_leonardo_gce_image.sh \
-  -var:image_hardening_script cis-harden-images/debian9/harden-images.sh \
-  -var:cis_hardening_playbook_config cis-harden-images/debian9/deb9-cis-playbook.yml \
-  -var:cis_hardening_playbook_requirements cis-harden-images/debian9/requirements.yml \
-  /gce-custom-images/leo_custom_gce_image.wf.json
+  -var:base_image "$BASE_IMAGE" \
+  -var:output_snapshot "$OUTPUT_SNAPSHOT_NAME" \
+  -var:gce_snapshots_dir /gce-custom-images \
+  -var:installation_script_name prepare_gce_image.sh \
+  /gce-custom-images/leo_gce_snapshot.wf.json
 
 # Daisy doesn't clean it up all so we remove the bucket manually
 gsutil rm -r $DAISY_BUCKET_PATH
--- jenkins/gce-custom-images/create_gce_image_old.sh
+++ jenkins/gce-custom-images/create_gce_image_old.sh
@@ -2,7 +2,7 @@
 
 set -e -x
 
-# This script is a wrapper around the command that runs the GCE VM snapshot
+# This script is a wrapper around the command that runs the custom GCE image
 # creation tool - Daisy - for local development and documentation purposes.
 #
 # It should be run from the root of the Leonardo repo.
@@ -11,7 +11,10 @@ set -e -x
 #
 # application_default_credentials.json needs to be copied to jenkins/gce-custom-images/ which is mounted on Daisy container
 # Credentials can be refreshed via 'gcloud auth application-default login' with project set to 'broad-dsde-dev' using
-# Broad account. They are saved at '~/.config/gcloud/application_default_credentials.json' by default.
+# Broad account
+#
+# Also make sure that you have the right version of the hardening repo via:
+# `git -C jenkins/gce-custom-images/dsp-appsec-base-image-hardening checkout <desired version hash>`
 
 # Set this to "true" if you want to validate the workflow without actually executing it
 VALIDATE_WORKFLOW="false"
@@ -21,8 +24,8 @@ VALIDATE_WORKFLOW="false"
 # `~/.config/gcloud/application_default_credentials.json` by default.
 SOURCE_DIR="/Users/kyuksel/github/leonardo/jenkins/gce-custom-images"
 
-# Underscores are not accepted as snapshot name
-OUTPUT_SNAPSHOT_NAME=leo-gce-snapshot-$(whoami)-$(date +"%Y-%m-%d-%H-%M-%S")
+# Underscores are not accepted as image name
+OUTPUT_IMAGE_NAME=leo-custom-gce-image-$(whoami)-$(date +"%Y-%m-%d-%H-%M-%S")
 
 PROJECT="broad-dsde-dev"
 REGION="us-central1"
@@ -31,17 +34,10 @@ ZONE="${REGION}-a"
 # The bucket that Daisy uses as scratch area to store source and log files.
 # If it doesn't exist, we create it prior to launching Daisy and
 # the Daisy workflow cleans up all but daisy.log at the end.
-DAISY_BUCKET_PATH="gs://test-leo-gce-snapshot-daisy-scratch-bucket"
+DAISY_BUCKET_PATH="gs://test-leo-custom-gce-image-daisy-scratch-bucket"
 
 # Set this to the tag of the Daisy image you had pulled
 DAISY_IMAGE_TAG="release"
-
-# When updating, to find the resource path:
-#    1. run `gcloud compute images list | grep cos` to get the list of available container-optimized OS images
-#    2. select the image of interest, say, `cos-89-16108-403-22`
-#    3. run `gcloud compute images describe cos-89-16108-403-22 --project cos-cloud | grep selfLink`
-#    4. extract the segments starting with 'projects'
-BASE_IMAGE="projects/cos-cloud/global/images/cos-89-16108-403-22"
 
 # Create the Daisy scratch bucket if it doesn't exist. The Daisy workflow will clean it up at the end.
 gsutil ls $DAISY_BUCKET_PATH || gsutil mb -b on -p $PROJECT -l $REGION $DAISY_BUCKET_PATH
@@ -59,11 +55,14 @@ docker run -it --rm -v "$SOURCE_DIR":/gce-custom-images \
   -gcs_path $DAISY_BUCKET_PATH \
   -default_timeout 60m \
   -oauth /gce-custom-images/application_default_credentials.json \
-  -var:base_image "$BASE_IMAGE" \
-  -var:output_snapshot "$OUTPUT_SNAPSHOT_NAME" \
-  -var:gce_snapshots_dir /gce-custom-images \
-  -var:installation_script_name prepare_gce_snapshot.sh \
-  /gce-custom-images/leo_gce_snapshot.wf.json
+  -var:base_image projects/debian-cloud/global/images/debian-9-stretch-v20200210 \
+  -var:output_image "$OUTPUT_IMAGE_NAME" \
+  -var:gce_custom_images_dir /gce-custom-images \
+  -var:installation_script_name prepare_custom_leonardo_gce_image.sh \
+  -var:image_hardening_script cis-harden-images/debian9/harden-images.sh \
+  -var:cis_hardening_playbook_config cis-harden-images/debian9/deb9-cis-playbook.yml \
+  -var:cis_hardening_playbook_requirements cis-harden-images/debian9/requirements.yml \
+  /gce-custom-images/leo_custom_gce_image.wf.json
 
 # Daisy doesn't clean it up all so we remove the bucket manually
 gsutil rm -r $DAISY_BUCKET_PATH
