298286dbd155c81e00a6b0241f45372c3f767825
Scala Steward <me@scala-steward.org>
2021-05-17 17:05:04 +0200

    Update scala-library from 2.13.5 to 2.13.6

--- project/Settings.scala
+++ project/Settings.scala
@@ -86,7 +86,7 @@ object Settings {
   val commonSettings =
     commonBuildSettings ++ List(
     organization  := "org.broadinstitute.dsde.workbench",
-    scalaVersion  := "2.13.5",
+    scalaVersion  := "2.13.6",
     resolvers ++= commonResolvers,
     scalacOptions ++= commonCompilerSettings
   )
