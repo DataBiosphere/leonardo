ea0eb97b4c89a3131435b4ce62f8adcd48e2f3e9
Qi Wang <qi@broadinstitute.org>
2021-05-25 07:12:53 -0400

    try larger disk

--- core/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/DiskModelGenerators.scala
+++ core/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/DiskModelGenerators.scala
@@ -3,7 +3,7 @@ package org.broadinstitute.dsde.workbench.leonardo
 import org.scalacheck.{Arbitrary, Gen}
 
 object DiskModelGenerators {
-  val genDiskSize: Gen[DiskSize] = Gen.chooseNum(5, 500).map(DiskSize)
+  val genDiskSize: Gen[DiskSize] = Gen.chooseNum(10, 500).map(DiskSize)
 
   implicit val arbDiskSize: Arbitrary[DiskSize] = Arbitrary(genDiskSize)
 }
