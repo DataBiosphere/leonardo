cf1e258c94d2136bb44e1f840a41561241fd4e6f
Robert Title <rtitle@broadinstitute.org>
2021-05-26 14:59:33 -0400

    chown notebooks dir instead

--- http/src/main/resources/init-resources/gce-init.sh
+++ http/src/main/resources/init-resources/gce-init.sh
@@ -465,7 +465,7 @@ if [ ! -z "$JUPYTER_DOCKER_IMAGE" ] ; then
   docker exec $JUPYTER_SERVER_NAME /bin/bash -c "sed -i 's/127.0.0.1/welder/g' /etc/jupyter/custom/jupyter_delocalize.py"
 
   # make sure home directory is owned by jupyter-user
-  docker exec -u root $JUPYTER_SERVER_NAME /bin/bash -c "chown -R jupyter-user:users ${JUPYTER_USER_HOME}"
+  docker exec -u root $JUPYTER_SERVER_NAME /bin/bash -c "chown -R jupyter-user:users ${NOTEBOOKS_DIR}"
 
   log 'Starting Jupyter Notebook...'
   retry 3 docker exec -d $JUPYTER_SERVER_NAME /bin/bash -c "${JUPYTER_SCRIPTS}/run-jupyter.sh ${NOTEBOOKS_DIR}"
--- http/src/main/resources/init-resources/init-actions.sh
+++ http/src/main/resources/init-resources/init-actions.sh
@@ -481,7 +481,7 @@ END
       docker exec $JUPYTER_SERVER_NAME /bin/bash -c "R -e '1+1'" || true
 
       # make sure home directory is owned by jupyter-user
-      docker exec -u root $JUPYTER_SERVER_NAME /bin/bash -c "chown -R jupyter-user:users $JUPYTER_USER_HOME"
+      docker exec -u root $JUPYTER_SERVER_NAME /bin/bash -c "chown -R jupyter-user:users ${NOTEBOOKS_DIR}"
 
       log 'Starting Jupyter Notebook...'
       retry 3 docker exec -d ${JUPYTER_SERVER_NAME} /bin/bash -c "${JUPYTER_SCRIPTS}/run-jupyter.sh ${NOTEBOOKS_DIR}"
