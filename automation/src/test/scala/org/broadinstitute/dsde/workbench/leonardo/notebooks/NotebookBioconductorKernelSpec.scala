7e6fd4f07041b358417611eca0dc995ef4151d13
Qi Wang <qi@broadinstitute.org>
2021-06-15 14:57:54 -0400

    tmp

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookBioconductorKernelSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookBioconductorKernelSpec.scala
@@ -30,51 +30,6 @@ import scala.concurrent.duration._
 class NotebookBioconductorKernelSpec extends RuntimeFixtureSpec with NotebookTestUtils {
   override val toolDockerImage: Option[String] = Some(LeonardoConfig.Leonardo.bioconductorImageUrl)
   "NotebookBioconductorKernelSpec" - {
-    "should be able to call installed Bioconductor libraries" in { runtimeFixture =>
-      withWebDriver { implicit driver =>
-        withNewNotebook(runtimeFixture.runtime, RKernel) { notebookPage =>
-          // it shouldn't take long to load libraries
-          val callLibraryTimeout = 1.minutes
-          // bioconductor libraries should install to appropriate directory
-          notebookPage.executeCell("find.package('ShortRead')").get should include("/usr/local/lib/R/site-library")
-        }
-      }
-    }
-
-    "should have GenomicFeatures, SingleCellExperiment, GenomicAlignments, ShortRead automatically installed" in {
-      runtimeFixture =>
-        withWebDriver { implicit driver =>
-          withNewNotebook(runtimeFixture.runtime, RKernel) { notebookPage =>
-            notebookPage.executeCell(""""GenomicFeatures" %in% installed.packages()""") shouldBe Some("TRUE")
-            notebookPage.executeCell(""""SingleCellExperiment" %in% installed.packages()""") shouldBe Some("TRUE")
-            notebookPage.executeCell(""""GenomicAlignments" %in% installed.packages()""") shouldBe Some("TRUE")
-            notebookPage.executeCell(""""ShortRead" %in% installed.packages()""") shouldBe Some("TRUE")
-          }
-        }
-    }
-
-    // TODO: this is fixed in the new bioc (R 4.0) image
-    "should have DESeq2 automatically installed" ignore { runtimeFixture =>
-      withWebDriver { implicit driver =>
-        withNewNotebook(runtimeFixture.runtime, RKernel) { notebookPage =>
-          notebookPage.executeCell(""""DESeq2" %in% installed.packages()""") shouldBe Some("TRUE")
-        }
-      }
-    }
-
-    "should be able to install packages that depend on jags, protobuf, Cairo and gtk" in { runtimeFixture =>
-      withWebDriver { implicit driver =>
-        withNewNotebook(runtimeFixture.runtime, RKernel) { notebookPage =>
-          val installTimeout = 15 minutes
-
-          // new packages should install to directory where PD is mounted
-          notebookPage.executeCell("""BiocManager::install('RGtk2')""", installTimeout)
-          notebookPage.executeCell("library(RGtk2)")
-          notebookPage.executeCell("find.package('RGtk2')").get should include("/home/jupyter-user/notebooks/packages")
-        }
-      }
-    }
-
     "should have Java available" in { runtimeFixture =>
       withWebDriver { implicit driver =>
         withNewNotebook(runtimeFixture.runtime, RKernel) { notebookPage =>
