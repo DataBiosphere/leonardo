1f8f67a53d36dadaaf28836096f2c39db8f106e5
xuscrbw <xuw@broadinstitute.org>
2021-06-28 13:58:48 -0400

    adding small logging to help debug

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeCreationDiskSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeCreationDiskSpec.scala
@@ -201,8 +201,10 @@ class RuntimeCreationDiskSpec
         })
         _ <- deleteRuntimeWithWait(googleProject, runtimeWithDataName, deleteDisk = true)
         getDiskAttempt = getDisk(googleProject, diskName).attempt
+
+        logger.info(s"runtime Name ${runtimeWithDataName} googleProject ${googleProject}")
         // Disk deletion may take some time so we're retrying to reduce flaky test failures
-        diskResp <- streamFUntilDone(getDiskAttempt, 20, 5 seconds).compile.lastOrError
+        diskResp <- streamFUntilDone(getDiskAttempt, 15, 5 seconds).compile.lastOrError
       } yield {
         runtime.diskConfig.map(_.name) shouldBe Some(diskName)
         runtime.diskConfig.map(_.size) shouldBe Some(diskSize)
