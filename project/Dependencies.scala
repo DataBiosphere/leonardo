3ac495c3aa57353d1b439b64368a0c9ffd9db61f
Scala Steward <me@scala-steward.org>
2021-07-07 02:18:17 +0200

    Update jackson-module-scala from 2.12.2 to 2.12.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -177,7 +177,7 @@ object Dependencies {
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % serviceTestV % "test" classifier "tests" excludeAll (excludeGuava, excludeStatsD)
 
   val automationDependencies = List(
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.12.2" % "test",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.12.4" % "test",
     "ch.qos.logback" % "logback-classic" % "1.2.3" % "test",
 
     "com.typesafe.akka" %% "akka-http-core" % akkaHttpV,
