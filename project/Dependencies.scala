f0cf6c99ff7d2a0be28373e0e2e857f373ef50e1
Scala Steward <me@scala-steward.org>
2021-09-12 06:12:57 +0200

    Update ficus from 1.5.0 to 1.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
   val logbackClassic: ModuleID =  "ch.qos.logback"              % "logback-classic" % "1.2.5"
   val scalaLogging: ModuleID =    "com.typesafe.scala-logging"  %% "scala-logging"  % scalaLoggingV
   val swaggerUi: ModuleID =       "org.webjars"                 % "swagger-ui"      % "3.51.2"
-  val ficus: ModuleID =           "com.iheart"                  %% "ficus"          % "1.5.0"
+  val ficus: ModuleID =           "com.iheart"                  %% "ficus"          % "1.5.1"
   val enumeratum: ModuleID =      "com.beachape"                %% "enumeratum"     % "1.7.0"
 
   val akkaSlf4j: ModuleID =         "com.typesafe.akka" %% "akka-slf4j"           % akkaV
