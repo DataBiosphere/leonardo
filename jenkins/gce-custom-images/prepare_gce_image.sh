f2420b21c7d084fb32140d41caeb833e025c924b
Jenkins Role account <jenkins@dsp-fc-jenkins-slave-prod217.c.broad-dsp-techops.internal>
2021-09-15 14:59:57 -0400

    automated custom image hash updates for image ID d4bd1c2

--- jenkins/gce-custom-images/prepare_gce_image.sh
+++ jenkins/gce-custom-images/prepare_gce_image.sh
@@ -27,7 +27,7 @@ terra_jupyter_aou="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-aou:2.0.1"
 
 welder_server="us.gcr.io/broad-dsp-gcr-public/welder-server:f411762"
 openidc_proxy="broadinstitute/openidc-proxy:2.3.1_2"
-anvil_rstudio_bioconductor="us.gcr.io/anvil-gcr-public/anvil-rstudio-bioconductor:3.13.0"
+anvil_rstudio_bioconductor="us.gcr.io/anvil-gcr-public/anvil-rstudio-bioconductor:3.13.2"
 
 cos_gpu_installer="gcr.io/cos-cloud/cos-gpu-installer:v2.0.3"
 google_cloud_toolbox="gcr.io/google-containers/toolbox:20200603-00"
