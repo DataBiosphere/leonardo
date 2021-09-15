645d7b5d7d353040b2be700522c11da7285007b5
xuscrbw <xuw@broadinstitute.org>
2021-09-15 17:28:08 -0400

    trigger test

--- jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
+++ jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
@@ -100,6 +100,7 @@ retry 5 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 retry 5 apt-key update
 
 #testing parameter change
+#script check
 retry 5 apt-get --allow-releaseinfo-change update
 
 # install Docker
