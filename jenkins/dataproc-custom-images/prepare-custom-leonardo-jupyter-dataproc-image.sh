2301701b5590f3d142c459dfaaa554a292acf655
xuscrbw <xuw@broadinstitute.org>
2021-09-16 12:14:32 -0400

    adding fixes and comments

--- jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
+++ jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
@@ -99,8 +99,7 @@ log 'Installing prerequisites...'
 retry 5 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 retry 5 apt-key update
 
-#testing parameter 2
-#HELLO
+#TODO: Remove this flag once we migrate to debian11
 retry 5 apt-get --allow-releaseinfo-change update
 
 # install Docker
