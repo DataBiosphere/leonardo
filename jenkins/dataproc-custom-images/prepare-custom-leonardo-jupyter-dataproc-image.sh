cadbe9275090bf16e17a283addb4d09da9071c66
Jenkins Role account <jenkins@dsp-fc-jenkins-slave-prod217.c.broad-dsp-techops.internal>
2021-09-24 19:35:35 -0400

    automated custom image hash updates for image ID 75621ac

--- jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
+++ jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
@@ -22,7 +22,7 @@ terra_jupyter_python="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-python:1.0.1"
 terra_jupyter_r="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-r:2.0.1"
 terra_jupyter_bioconductor="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-bioconductor:2.0.1"
 terra_jupyter_hail="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-hail:1.0.1"
-terra_jupyter_gatk="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-gatk:2.0.2"
+terra_jupyter_gatk="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-gatk:2.0.3"
 terra_jupyter_aou="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-aou:2.0.2"
 welder_server="us.gcr.io/broad-dsp-gcr-public/welder-server:97a2d72"
 openidc_proxy="broadinstitute/openidc-proxy:2.3.1_2"
@@ -41,7 +41,7 @@ docker_image_var_names="welder_server terra_jupyter_base terra_jupyter_base_old 
 
 # The version of python to install
 # Note: this should match the version of python in the terra-jupyter-hail image.
-python_version="3.7.10"
+python_version="3.7.4"
 
 bucket_name="gs://leo-dataproc-image"
 
