22e09ab30a3a2d3aa9317aeec89bee55068c1952
Qi Wang <qi@broadinstitute.org>
2021-10-05 23:34:04 -0400

    give pub/sub threads name

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val opencensusV = "0.28.3"
 
 
-  private val workbenchLibsHash = "ae90d1a7-SNAP"
+  private val workbenchLibsHash = "99bd567c-SNAP"
   val serviceTestV = s"0.20-$workbenchLibsHash"
   val workbenchModelV = s"0.14-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
