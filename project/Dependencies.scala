fa43d4b1443933d6dacb2d2f588d62e9a7231aaa
Scala Steward <me@scala-steward.org>
2021-10-19 02:18:01 +0200

    Update mysql-connector-java from 8.0.22 to 8.0.27

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -120,7 +120,7 @@ object Dependencies {
 
   val slick: ModuleID =           "com.typesafe.slick"  %% "slick"                % slickV excludeAll (excludeTypesafeConfig, excludeReactiveStream)
   val hikariCP: ModuleID =        "com.typesafe.slick"  %% "slick-hikaricp"       % slickV excludeAll (excludeSlf4j)
-  val mysql: ModuleID =           "mysql"               % "mysql-connector-java"  % "8.0.22"
+  val mysql: ModuleID =           "mysql"               % "mysql-connector-java"  % "8.0.27"
   val liquibase: ModuleID =       "org.liquibase"       % "liquibase-core"        % "4.5.0"
   val sealerate: ModuleID =       "ca.mrvisser"         %% "sealerate"            % "0.0.6"
   val googleCloudNio: ModuleID =  "com.google.cloud"    % "google-cloud-nio"      % "0.123.10" % Test // brought in for FakeStorageInterpreter
