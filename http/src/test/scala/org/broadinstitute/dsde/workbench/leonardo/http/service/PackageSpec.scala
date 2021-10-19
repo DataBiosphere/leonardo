d65737885fe9e22be320fc4e74553a319faec86d
Gabriela <gcarrill@broadinstitute.org>
2021-10-19 15:57:02 -0400

    start unit testing

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/PackageSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/PackageSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class PackageSpec extends AnyFlatSpec with Matchers {
-  it should "extract labels properly" in {
+  it should "extract labels to filter by properly" in {
     val input1 = Map("_labels" -> "foo=bar,baz=biz")
     processLabelMap(input1) shouldBe (Right(Map("foo" -> "bar", "baz" -> "biz")))
 
@@ -15,5 +15,13 @@ class PackageSpec extends AnyFlatSpec with Matchers {
 
     val duplicateLabel = Map("_labels" -> "foo=bar,foo=biz")
     processLabelMap(duplicateLabel) shouldBe (Right(Map("foo" -> "biz")))
+  }
+
+  it should "process labels to return properly" in {
+    val input1 = Map("includeLabels" -> "foo,bar")
+    processLabelsToReturn(input1, None) shouldBe Right(List("foo", "bar"))
+
+    val input2 = Map("includeLabels" -> "")
+    processLabelsToReturn(input2, None).isLeft shouldBe true
   }
 }
