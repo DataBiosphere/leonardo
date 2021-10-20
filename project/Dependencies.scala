c4d8e59ed98743598e12eca3a469518ec5041a9b
Qi Wang <qi@broadinstitute.org>
2021-10-20 10:59:21 -0400

    update hash

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val opencensusV = "0.28.3"
 
 
-  private val workbenchLibsHash = "0c87ddaa-SNAP"
+  private val workbenchLibsHash = "959c1b8"
   val serviceTestV = s"0.20-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
