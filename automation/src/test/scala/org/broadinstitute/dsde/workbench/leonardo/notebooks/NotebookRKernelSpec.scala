b9e8f261ca37bc22bf294629402cfc62e9e83148
Justin Canas <justincanas16@gmail.com>
2021-10-27 16:43:51 -0700

    [QA-1514] attempt fix (#2348)
    * attempt fix
    
    * iterate
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookRKernelSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookRKernelSpec.scala
@@ -154,9 +154,9 @@ class NotebookRKernelSpec extends RuntimeFixtureSpec with NotebookTestUtils {
               // TODO: remove when PPW is rolled out to all workspaces
               // and Leo removes the kernel_bootstrap logic.
               // See https://broadworkbench.atlassian.net/browse/IA-2936
-              "WORKSPACE_NAME" -> "Untitled Folder"
+              "WORKSPACE_NAME" -> "jupyter"
             )
-        withNewNotebookInSubfolder(runtimeFixture.runtime, RKernel) { notebookPage =>
+        withNewNotebook(runtimeFixture.runtime, RKernel) { notebookPage =>
           expectedEVs.foreach {
             case (k, v) =>
               val res = notebookPage.executeCell(s"Sys.getenv('$k')")
