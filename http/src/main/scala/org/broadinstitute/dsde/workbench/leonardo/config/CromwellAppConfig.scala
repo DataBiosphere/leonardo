01984e9fb01ed6aba88c32284d9e77ca667edd43
cahrens <roberts@broadinstitute.org>
2021-11-01 13:24:37 -0400

    format updates

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/CromwellAppConfig.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/CromwellAppConfig.scala
@@ -1,7 +1,13 @@
 package org.broadinstitute.dsde.workbench.leonardo.config
 
 import org.broadinstitute.dsde.workbench.google2.KubernetesSerializableName.ServiceAccountName
-import org.broadinstitute.dsde.workbench.leonardo.{Chart, DbPassword, NamespaceNameSuffix, ReleaseNameSuffix, ServiceConfig}
+import org.broadinstitute.dsde.workbench.leonardo.{
+  Chart,
+  DbPassword,
+  NamespaceNameSuffix,
+  ReleaseNameSuffix,
+  ServiceConfig
+}
 import org.broadinstitute.dsp.{ChartName, ChartVersion}
 
 final case class CromwellAppConfig(chartName: ChartName,
--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/CustomAppConfig.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/CustomAppConfig.scala
@@ -3,6 +3,8 @@ package org.broadinstitute.dsde.workbench.leonardo.config
 import org.broadinstitute.dsde.workbench.leonardo.{Chart, ReleaseNameSuffix}
 import org.broadinstitute.dsp.{ChartName, ChartVersion}
 
-final case class CustomAppConfig(chartName: ChartName, chartVersion: ChartVersion, releaseNameSuffix: ReleaseNameSuffix) {
+final case class CustomAppConfig(chartName: ChartName,
+                                 chartVersion: ChartVersion,
+                                 releaseNameSuffix: ReleaseNameSuffix) {
   def chart: Chart = Chart(chartName, chartVersion)
 }
