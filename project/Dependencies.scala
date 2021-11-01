3a46265aadebf3234439187bf9a4b1f36db531aa
Qi Wang <qi@broadinstitute.org>
2021-11-01 10:17:45 -0400

    use published google2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val opencensusV = "0.28.3"
 
 
-  private val workbenchLibsHash = "439fd7cb-SNAP"
+  private val workbenchLibsHash = "78ee616"
   val serviceTestV = s"0.20-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
