d2471f2af80334b7bc8a7ccc0533dbf86d3b2445
Qi Wang <qi@broadinstitute.org>
2021-11-03 16:55:27 -0400

    upgrade wb-libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val opencensusV = "0.28.3"
 
 
-  private val workbenchLibsHash = "f52bf30"
+  private val workbenchLibsHash = "a42799c0-SNAP"
   val serviceTestV = s"0.20-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
