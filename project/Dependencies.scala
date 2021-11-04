3399421c7bd71adab371ef27f50bdd677d269f9a
Qi Wang <32771737+Qi77Qi@users.noreply.github.com>
2021-11-04 08:24:59 -0400

    IA-3037 upgrade wb-libs to make start dataproc cluster more stable (#2366)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val opencensusV = "0.28.3"
 
 
-  private val workbenchLibsHash = "f52bf30"
+  private val workbenchLibsHash = "504ac92"
   val serviceTestV = s"0.20-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
