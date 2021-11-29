a6a4e02ef5802ade51858725b0f73e29e0fae2bd
Qi Wang <qi@broadinstitute.org>
2021-11-29 16:22:34 -0500

    add back hail stuff

--- jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
+++ jenkins/dataproc-custom-images/prepare-custom-leonardo-jupyter-dataproc-image.sh
@@ -142,18 +142,6 @@ log 'Installing Docker...'
 retry 5 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 retry 5 apt-get update
 
-rm -rf /var/lib/docker
-mkdir -p /etc/docker
-touch /etc/docker/daemon.json
-cat > /etc/docker/daemon.json <<EOF
-{
-  "storage-driver": "devicemapper",
-  "storage-opts": [
-     "dm.basesize=60G"
-  ]
-}
-EOF
-
 dpkg --configure -a
 # This line fails consistently, but it does not fail in a fatal way so we add `|| true` to prevent the script from halting execution
 # The message that is non-fatal is `Sub-process /usr/bin/dpkg returned an error code (1).`
