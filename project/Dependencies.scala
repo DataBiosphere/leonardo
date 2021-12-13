bf26f712527cf8a44085bcb8f7c91a513c6f9434
Qi Wang <qi@broadinstitute.org>
2021-12-13 17:02:08 -0500

    bump to latest google2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val monocleV = "2.1.0"
   val opencensusV = "0.28.3"
 
-  private val workbenchLibsHash = "8edd7f48-SNAP"
+  private val workbenchLibsHash = "51ddfe8b-SNAP"
   val serviceTestV = s"0.21-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
