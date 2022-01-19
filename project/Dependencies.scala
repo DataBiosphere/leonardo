583f5bba65912d4f3ee5159a606aae3e4487cd63
Qi Wang <qi@broadinstitute.org>
2022-01-19 09:54:44 -0500

    bump deps

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val monocleV = "2.1.0"
   val opencensusV = "0.29.0"
 
-  private val workbenchLibsHash = "21c84d8"
+  private val workbenchLibsHash = "3b927f8"
   val serviceTestV = s"0.21-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
