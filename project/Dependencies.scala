716a5e5d02cc71a9c181fe4bf8d6dac4ed7082f2
Scala Steward <me@scala-steward.org>
2022-02-14 18:38:07 +0100

    Update akka-http, akka-http-core, ... from 10.2.7 to 10.2.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val scalaV = "2.13"
 
   val akkaV = "2.6.18"
-  val akkaHttpV = "10.2.7"
+  val akkaHttpV = "10.2.8"
   val googleV = "1.23.0"
   val automationGoogleV = "1.30.5"
   val scalaLoggingV = "3.9.4"
