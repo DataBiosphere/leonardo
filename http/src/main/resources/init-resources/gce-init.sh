30d929a278ba03a636424c48e37cd08216f06d4d
Qi Wang <qi@broadinstitute.org>
2022-04-05 09:25:49 -0400

    resize a bit later

--- http/src/main/resources/init-resources/gce-init.sh
+++ http/src/main/resources/init-resources/gce-init.sh
@@ -231,15 +231,6 @@ fi
 
 mount -t ext4 -O discard,defaults /dev/${DISK_DEVICE_ID} ${WORK_DIRECTORY}
 
-# Resize persistent disk if needed.
-# This condition assumes Dataproc's cert directory is different from GCE's cert directory, a better condition would be
-# a dedicated flag that distinguishes gce and dataproc. But this will do for now
-# If it's GCE, we resize the PD. Dataproc doesn't have PD
-if [ -f "/var/certs/jupyter-server.crt" ]; then
-  echo "Resizing persistent disk attached to runtime $GOOGLE_PROJECT / $CLUSTER_NAME if disk size changed..."
-  resize2fs /dev/${DISK_DEVICE_ID}
-fi
-
 # done persistent disk setup
 STEP_TIMINGS+=($(date +%s))
 
@@ -250,6 +241,15 @@ if [ "${GPU_ENABLED}" == "true" ] ; then
   mount -o remount,exec /var/lib/nvidia
 
   $GSUTIL_CMD cp ${GPU_DOCKER_COMPOSE} ${DOCKER_COMPOSE_FILES_DIRECTORY}
+fi
+
+# Resize persistent disk if needed.
+# This condition assumes Dataproc's cert directory is different from GCE's cert directory, a better condition would be
+# a dedicated flag that distinguishes gce and dataproc. But this will do for now
+# If it's GCE, we resize the PD. Dataproc doesn't have PD
+if [ -f "/var/certs/jupyter-server.crt" ]; then
+  echo "Resizing persistent disk attached to runtime $GOOGLE_PROJECT / $CLUSTER_NAME if disk size changed..."
+  resize2fs /dev/${DISK_DEVICE_ID}
 fi
 
 log 'Copying secrets from GCS...'
