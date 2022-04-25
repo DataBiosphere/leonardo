9fccda6dca245656fd486545717d7d051a420b77
Sergiy Getlin <91149690+sergiygetlin@users.noreply.github.com>
2022-04-25 15:11:53 -0700

    Add additional logging

--- http/src/main/resources/init-resources/msdsvmcontent/ms_dsvm_script_v5_cat.sh
+++ http/src/main/resources/init-resources/msdsvmcontent/ms_dsvm_script_v5_cat.sh
@@ -65,12 +65,15 @@ sudo runuser -l $VM_USER -c '/anaconda/bin/jupyter server --ServerApp.certfile=$
 
 # Store Jupyter Server parameters for reboot process
 
-echo "TEST!!!"
+echo "CRON TEST!!!\n"
 echo "@reboot sudo runuser -l $VM_USER -c '/anaconda/bin/jupyter server --ServerApp.certfile=$SERVER_APP_CERTFILE --ServerApp.keyfile=$SERVER_APP_KEYFILE --ServerApp.port=$SERVER_APP_PORT --ServerApp.token=$SERVER_APP_TOKEN --ServerApp.ip=$SERVER_APP_IP --ServerApp.base_url=$SERVER_APP_BASE_URL --ServerApp.websocket_url=$SERVER_APP_WEBSOCKET_URL --ServerApp.allow_origin=$SERVER_APP_ALLOW_ORIGIN --autoreload' >/dev/null 2>&1&"
-echo "TEST!!!"
+echo "END CRON TEST!!!\n"
+
 
 SERVER_APP_WEBSOCKET_URL2="wss://${RELAY_NAME}.servicebus.windows.net/\$hc/${RELAY_CONNECTION_NAME}"
+echo "VAR TEST!!!\n"
 echo $SERVER_APP_WEBSOCKET_URL2
+echo "END VAR TEST!!!\n"
 
 sudo crontab -l 2>/dev/null| cat - <(echo "@reboot sudo runuser -l $VM_USER -c '/anaconda/bin/jupyter server --ServerApp.certfile=$SERVER_APP_CERTFILE --ServerApp.keyfile=$SERVER_APP_KEYFILE --ServerApp.port=$SERVER_APP_PORT --ServerApp.token=$SERVER_APP_TOKEN --ServerApp.ip=$SERVER_APP_IP --ServerApp.base_url=$SERVER_APP_BASE_URL --ServerApp.websocket_url=$SERVER_APP_WEBSOCKET_URL2 --ServerApp.allow_origin=$SERVER_APP_ALLOW_ORIGIN --autoreload' >/dev/null 2>&1&") | crontab -
 
