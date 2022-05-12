673c66a3def69873eeef4c42cb83a57282416eac
broadbot <broadbot@broadinstitute.org>
2022-05-12 12:09:21 +0000

    Update jackson-module-scala from 2.13.1 to 2.13.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -191,7 +191,7 @@ object Dependencies {
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % serviceTestV % "test" classifier "tests" excludeAll (excludeGuava, excludeStatsD)
 
   val automationDependencies = List(
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.1" % "test",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.13.2" % "test",
     logbackClassic % "test",
 
     "com.typesafe.akka" %% "akka-http-core" % akkaHttpV,
