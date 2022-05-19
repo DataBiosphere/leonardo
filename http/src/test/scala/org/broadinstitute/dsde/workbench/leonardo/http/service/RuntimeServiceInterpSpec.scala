fbd55236115c43e213a3c78bcc910c28bedd1567
Gabriela <gcarrill@broadinstitute.org>
2022-05-19 15:52:25 -0400

    add cases

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeServiceInterpSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeServiceInterpSpec.scala
@@ -113,6 +113,8 @@ class RuntimeServiceInterpSpec extends AnyFlatSpec with LeonardoTestSuite with T
     calculateAutopauseThreshold(Some(false), None, autoFreezeConfig) shouldBe autoPauseOffValue
     calculateAutopauseThreshold(Some(true), None, autoFreezeConfig) shouldBe autoFreezeConfig.autoFreezeAfter.toMinutes.toInt
     calculateAutopauseThreshold(Some(true), Some(30), autoFreezeConfig) shouldBe 30
+    calculateAutopauseThreshold(None, Some(40), autoFreezeConfig) shouldBe 40
+    calculateAutopauseThreshold(Some(true), Some(35), autoFreezeConfig) shouldBe 35
   }
 
   it should "throw ClusterAlreadyExistsException when creating a cluster with same name and project as an existing cluster" in isolatedDbTest {
