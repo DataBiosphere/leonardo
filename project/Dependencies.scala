dec6527bf4f1ea9e4b3344a3c38f9e8daba213c8
broadbot <broadbot@broadinstitute.org>
2022-05-26 12:04:57 +0000

    Update http4s-dsl from 1.0.0-M32 to 1.0.0-M33

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   val scalaLoggingV = "3.9.5"
   val scalaTestV = "3.2.11"
   val slickV = "3.3.3"
-  val http4sVersion = "1.0.0-M32"
+  val http4sVersion = "1.0.0-M33"
   val guavaV = "31.0.1-jre"
   val monocleV = "2.1.0"
   val opencensusV = "0.29.0"
