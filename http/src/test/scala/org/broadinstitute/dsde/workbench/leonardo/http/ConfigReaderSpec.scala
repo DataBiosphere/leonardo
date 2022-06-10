8172617dce2579c4bf71da3fac04ba378d1a6a39
Qi Wang <qi@broadinstitute.org>
2022-06-10 15:00:59 -0400

    unit tests

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -60,7 +60,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
                 "https://raw.githubusercontent.com/DataBiosphere/leonardo/68b91c4924e0bb6e8ec95d7af94fafe7b02b7ed3/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
-            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:a91c0e4",
+            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:a3ef8e0",
             VMCredential(username = "username", password = "password")
           )
         ),
