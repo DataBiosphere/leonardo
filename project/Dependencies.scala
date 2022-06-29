1fac847438626d68fe71d9a735ec0777dedebb21
Robert Title <rtitle@broadinstitute.org>
2022-06-29 15:24:10 -0400

    Bump wb-libs again

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   val monocleV = "2.1.0"
   val opencensusV = "0.29.0"
 
-  private val workbenchLibsHash = "35b51165-SNAP"
+  private val workbenchLibsHash = "51eb1287-SNAP"
   val serviceTestV = s"2.0-$workbenchLibsHash"
   val workbenchModelV = s"0.15-$workbenchLibsHash"
   val workbenchGoogleV = s"0.21-$workbenchLibsHash"
