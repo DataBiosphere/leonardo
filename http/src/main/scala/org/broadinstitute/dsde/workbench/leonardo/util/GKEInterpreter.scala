5b30b4a0d6d1c3c4e28466f0ca644812e9d7dbfe
Qi Wang <qi@broadinstitute.org>
2022-08-16 15:52:31 -0400

    fix unit tests

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GKEInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GKEInterpreter.scala
@@ -1523,10 +1523,7 @@ class GKEInterpreter[F[_]](
     List(
       // Storage class configs
       raw"""nfs.storageClass.name=nfs-${release.asString}""",
-      raw"""cvmfs.repositories.cvmfs-gxy-data-${release.asString}=data.galaxyproject.org""",
-      raw"""cvmfs.cache.alienCache.storageClass=nfs-${release.asString}""",
       raw"""galaxy.persistence.storageClass=nfs-${release.asString}""",
-      raw"""galaxy.cvmfs.galaxyPersistentVolumeClaims.data.storageClassName=cvmfs-gxy-data-${release.asString}""",
       // Node selector config: this ensures the app is run on the user's nodepool
       raw"""galaxy.nodeSelector.cloud\.google\.com/gke-nodepool=${nodepoolName.value}""",
       raw"""nfs.nodeSelector.cloud\.google\.com/gke-nodepool=${nodepoolName.value}""",
