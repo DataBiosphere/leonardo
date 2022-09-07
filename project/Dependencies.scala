7936648fc9bd50251f4b07aa74f142dc0221da5a
broadbot <broadbot@broadinstitute.org>
2022-09-07 12:03:53 +0000

    Update akka-slf4j, akka-stream, ... from 2.6.19 to 2.6.20

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   val scalaV = "2.13"
 
-  val akkaV = "2.6.19"
+  val akkaV = "2.6.20"
   val akkaHttpV = "10.2.7"
   val googleV = "1.23.0"
   val automationGoogleV = "1.30.5"
