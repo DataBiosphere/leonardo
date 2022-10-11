0826ad8f63d38b68d549bb7d350cc5d6907c25e5
Qi Wang <qi@broadinstitute.org>
2022-10-11 11:50:38 -0400

    fine

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,7 @@ cd $LEONARDO_DIR
 rm -f leonardo*.jar
 
 # Test
-JAVA_OPTS="-Dmysql.host=mysql -Dmysql.port=3306 -Xmx4G -Xms4G -Xss4m" sbt -v "project http" clean assembly
+JAVA_OPTS="-Dmysql.host=mysql -Dmysql.port=3306 -Xmx6G -Xms6G -Xss4m" sbt -v "project http" clean assembly
 LEONARDO_JAR=$(find http/target | grep 'http-assembly.*\.jar')
 
 # new generated jar name starts with `http`, but renaming it to `leonardo*.jar`
