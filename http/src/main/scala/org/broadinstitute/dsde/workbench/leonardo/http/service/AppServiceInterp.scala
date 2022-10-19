6831a8e5f4095bbba64b39ebcaa579e6ae5dee4f
n8landolt <nlandolt@broadinstitute.org>
2022-10-19 14:31:11 -0400

    Generalized notifyingResourceDeleted and marking as deleted in deleteAppV2.

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/AppServiceInterp.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/AppServiceInterp.scala
@@ -698,12 +698,18 @@ final class LeoAppServiceInterp[F[_]: Parallel](config: AppServiceConfig,
     // Get the disk to delete if specified
     diskOpt = if (deleteDisk) appResult.app.appResources.disk.map(_.id) else None
 
-    googleProjectOpt = LeoLenses.cloudContextToGoogleProject.get(appResult.cluster.cloudContext)
+    projOpt = appResult.cluster.cloudContext match {
+      case gcpCloudContext: org.broadinstitute.dsde.workbench.leonardo.CloudContext.Gcp =>
+        LeoLenses.cloudContextToGoogleProject.get(gcpCloudContext)
+      case azureCloudContext: org.broadinstitute.dsde.workbench.leonardo.CloudContext.Azure =>
+        LeoLenses.cloudContextToManagedResourceGroup.get(azureCloudContext)
+    }
+
     _ <-
       if (appResult.app.status == AppStatus.Error) {
         for {
           // we only need to delete Sam record for clusters in Google. Sam record for Azure is managed by WSM
-          _ <- googleProjectOpt.traverse(g =>
+          _ <- projOpt.traverse(g =>
             authProvider.notifyResourceDeleted(appResult.app.samResourceId, appResult.app.auditInfo.creator, g)
           )
           _ <- appQuery.markAsDeleted(appResult.app.id, ctx.now).transaction
