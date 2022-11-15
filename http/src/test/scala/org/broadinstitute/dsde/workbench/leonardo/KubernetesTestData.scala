f0753c46e235b5daeb9acc1cacbd28ce74d6e6c8
broadbot <broadbot@broadinstitute.org>
2022-11-15 22:34:10 +0000

    Auto-update Cromwell version to 0.2.163

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
@@ -49,7 +49,7 @@ object KubernetesTestData {
   val ingressChart = Chart(ingressChartName, ingressChartVersion)
 
   val coaChartName = ChartName("/leonardo/cromwell-on-azure")
-  val coaChartVersion = ChartVersion("0.2.160")
+  val coaChartVersion = ChartVersion("0.2.163")
   val coaChart = Chart(coaChartName, coaChartVersion)
 
   val serviceKind = KubernetesServiceKindName("ClusterIP")
