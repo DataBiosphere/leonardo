3fbcc94c2b278d66b3ff0853fb1130bf13b7f9a6
Qi Wang <qi@broadinstitute.org>
2022-11-16 11:31:49 -0500

    update debian version

--- jenkins/dataproc-custom-images/create_dataproc_image.sh
+++ jenkins/dataproc-custom-images/create_dataproc_image.sh
@@ -33,7 +33,7 @@ gcloud config set dataproc/region us-central1
 
 python generate_custom_image.py \
     --image-name "$customDataprocImageBaseName-$dp_version_formatted-$imageID" \
-    --dataproc-version "2.0.39-debian10" \
+    --dataproc-version "2.0.51-debian10" \
     --customization-script ../prepare-custom-leonardo-jupyter-dataproc-image.sh \
     --zone $ZONE \
     --gcs-bucket $TEST_BUCKET \
