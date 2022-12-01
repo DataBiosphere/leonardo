61005dbbdfe5e2edc9e7e5513e8343e17bae2934
Justin Canas <justincanas16@gmail.com>
2022-12-01 14:16:49 -0800

    [IA-3735] change rstudio background var name (#2900)
    * change rstudio backgroound var name
    
    * update docker compose
    
    * update welder hash
    
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>
    Co-authored-by: Qi Wang <qi@broadinstitute.org>
    Co-authored-by: Qi Wang <32771737+Qi77Qi@users.noreply.github.com>

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -37,7 +37,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
           Uri.unsafeFromString("https://sam.test.org:443"),
           Uri.unsafeFromString("https://localhost:8000"),
           "terradevacrpublic.azurecr.io/welder-server",
-          "08b3633",
+          "6648f5c",
           PollMonitorConfig(1 seconds, 10, 1 seconds),
           PollMonitorConfig(1 seconds, 20, 1 seconds),
           AzureRuntimeDefaults(
