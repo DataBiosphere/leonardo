be59e2a49ee6d7b1f6e767c17e60bf36731e550c
n8landolt <nlandolt@broadinstitute.org>
2022-12-21 10:06:38 -0500

    Updated getToolFromImages tests.

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeServiceInterpSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeServiceInterpSpec.scala
@@ -2046,14 +2046,14 @@ class RuntimeServiceInterpSpec extends AnyFlatSpec with LeonardoTestSuite with T
     thrown shouldBe ForbiddenError(userInfo.userEmail)
   }
 
-  it should "get the correct tool from list of cluster images" in isolatedDbTest {
+  it should "test getToolFromImages - get the Jupyter tool from list of cluster images" in isolatedDbTest {
     val tool = getToolFromImages(Set(jupyterImage, welderImage, proxyImage, cryptoDetectorImage))
-    tool shouldBe Some(jupyterImage)
+    tool shouldBe Some(Tool.Jupyter)
   }
 
-  it should "test" in isolatedDbTest {
+  it should "test getToolFromImages - get the RStudio tool from list of cluster images" in isolatedDbTest {
     val tool = getToolFromImages(Set(rstudioImage, welderImage, proxyImage, cryptoDetectorImage))
-    tool shouldBe Some(rstudioImage)
+    tool shouldBe Some(Tool.RStudio)
   }
 
   private def withLeoPublisher(
