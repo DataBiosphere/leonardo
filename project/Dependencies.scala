652a22c7192b38cb7891ec012dcb9c9162d3a203
broadbot <broadbot@broadinstitute.org>
2023-01-12 12:05:08 +0000

    Update java-jwt from 4.2.1 to 4.2.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -189,7 +189,7 @@ object Dependencies {
     mysql,
     liquibase,
     "com.github.sebruck" %% "opencensus-scala-akka-http" % "0.7.2",
-    "com.auth0" % "java-jwt" % "4.2.1",
+    "com.auth0" % "java-jwt" % "4.2.2",
     http4sBlazeServer % Test,
     scalaTestSelenium,
     scalaTestMockito
