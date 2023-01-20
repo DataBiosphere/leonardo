e3ce8e44d0de426b546580c50522580ca337ecc9
David An <davidan@broadinstitute.org>
2023-01-20 16:10:49 -0500

    dont need string interpolation

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/util/AKSInterpreterSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/util/AKSInterpreterSpec.scala
@@ -140,7 +140,7 @@ class AKSInterpreterSpec extends AnyFlatSpecLike with TestComponent with Leonard
       "identity.name=identity-name," +
       "identity.resourceId=identity-id," +
       "identity.clientId=identity-client-id," +
-      s"sam.url=https://sam.dsde-dev.broadinstitute.org/," +
+      "sam.url=https://sam.dsde-dev.broadinstitute.org/," +
       "fullnameOverride=coa-rel-1"
   }
 
