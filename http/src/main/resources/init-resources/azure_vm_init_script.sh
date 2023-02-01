66a7dbf7151e3275c43467c2bf8ed197061e6229
erdalcosgun <31049564+erdalcosgun@users.noreply.github.com>
2023-02-01 14:21:24 -0800

    Update azure_vm_init_script.sh

--- http/src/main/resources/init-resources/azure_vm_init_script.sh
+++ http/src/main/resources/init-resources/azure_vm_init_script.sh
@@ -63,18 +63,7 @@ RELAY_CONNECTIONSTRING="Endpoint=sb://${RELAY_NAME}.servicebus.windows.net/;Shar
 
 /anaconda/envs/py38_default/bin/pip3 install seaborn
 
-# Update rbase version 
-#Reference- https://cloud.r-project.org/
-# update indices
-echo "Y"|sudo apt update -qq
-# install two helper packages we need
-echo "Y"|sudo apt install --no-install-recommends software-properties-common dirmngr
-# add the signing key (by Michael Rutter) for these repos
-# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
-# Fingerprint: E298A3A825C0D65DFD57CBB651716619E084DAB9
-echo "Y"|wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
-echo "Y"|sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+#Update rbase version
 echo "Y"|sudo apt install --no-install-recommends r-base
 
 #Update kernel list
