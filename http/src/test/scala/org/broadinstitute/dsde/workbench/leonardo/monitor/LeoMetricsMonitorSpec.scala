70c443da7718f966c9e62059cb2c1e2420ff890a
Robert Title <rtitle@broadinstitute.org>
2023-02-09 22:50:52 -0500

    Finish test

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/monitor/LeoMetricsMonitorSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/monitor/LeoMetricsMonitorSpec.scala
@@ -1,33 +1,50 @@
 package org.broadinstitute.dsde.workbench.leonardo.monitor
 
 import cats.effect.IO
+import cats.effect.unsafe.IORuntime
 import org.broadinstitute.dsde.workbench.azure.{AzureCloudContext, ManagedResourceGroupName, SubscriptionId, TenantId}
+import org.broadinstitute.dsde.workbench.google2.KubernetesSerializableName.ServiceName
+import org.broadinstitute.dsde.workbench.google2.{NetworkName, SubnetworkName}
 import org.broadinstitute.dsde.workbench.leonardo.KubernetesTestData.{
   makeApp,
   makeAzureCluster,
   makeKubeCluster,
   makeNodepool
 }
+import org.broadinstitute.dsde.workbench.leonardo.TestUtils.appContext
 import org.broadinstitute.dsde.workbench.leonardo.config.Config
 import org.broadinstitute.dsde.workbench.leonardo.dao._
 import org.broadinstitute.dsde.workbench.leonardo.db.TestComponent
-import org.broadinstitute.dsde.workbench.leonardo.monitor.LeoMetric.{AppStatusMetric, RuntimeStatusMetric}
+import org.broadinstitute.dsde.workbench.leonardo.monitor.LeoMetric.{
+  AppHealthMetric,
+  AppStatusMetric,
+  RuntimeHealthMetric,
+  RuntimeStatusMetric
+}
 import org.broadinstitute.dsde.workbench.leonardo.{
   AppName,
   AppStatus,
   AppType,
   CloudContext,
   CloudProvider,
+  IpRange,
   KubernetesCluster,
+  KubernetesClusterAsyncFields,
+  KubernetesService,
+  KubernetesServiceKindName,
   LeonardoTestSuite,
+  NetworkFields,
   RuntimeContainerServiceType,
   RuntimeImageType,
   RuntimeMetrics,
   RuntimeName,
   RuntimeStatus,
   RuntimeUI,
+  ServiceConfig,
+  ServiceId,
   WorkspaceId
 }
+import org.broadinstitute.dsde.workbench.model.IP
 import org.broadinstitute.dsde.workbench.model.google.GoogleProject
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
@@ -38,15 +55,13 @@ import java.util.UUID
 import scala.concurrent.duration._
 
 class LeoMetricsMonitorSpec extends AnyFlatSpec with LeonardoTestSuite with TestComponent with MockitoSugar {
-
   val azureContext = AzureCloudContext(
     TenantId("tenant"),
     SubscriptionId("sub"),
     ManagedResourceGroupName("mrg")
   )
-  val googleProject = GoogleProject("goog")
 
-  val config = LeoMetricsMonitorConfig(1 minute, true)
+  // Mocks
   val appDAO = setUpMockAppDAO
   val wdsDAO = setUpMockWdsDAO
   val cbasDAO = setUpMockCbasDAO
@@ -56,10 +71,12 @@ class LeoMetricsMonitorSpec extends AnyFlatSpec with LeonardoTestSuite with Test
   val jupyterDAO = setUpMockJupyterDAO
   val rstudioDAO = setUpMockRStudioDAO
   val welderDAO = setUpMockWelderDAO
+
+  // Test object
   implicit val clusterToolToToolDao =
     ToolDAO.clusterToolToToolDao(jupyterDAO, welderDAO, rstudioDAO)
   implicit val ec = cats.effect.unsafe.IORuntime.global.compute
-
+  val config = LeoMetricsMonitorConfig(1 minute, true)
   val leoMetricsMonitor = new LeoMetricsMonitor[IO](
     config,
     appDAO,
@@ -70,23 +87,174 @@ class LeoMetricsMonitorSpec extends AnyFlatSpec with LeonardoTestSuite with Test
     samDAO
   )
 
-  private def genApp(isAzure: Boolean, appType: AppType, isAou: Boolean): KubernetesCluster = {
+  "LeoMetricsMonitor" should "count apps by status" in {
+    val test = leoMetricsMonitor.countAppsByDbStatus(allApps)
+    // 5 apps
+    test.size shouldBe 5
+    // Cromwell on Azure
+    test.get(
+      AppStatusMetric(CloudProvider.Azure, AppType.Cromwell, AppStatus.Running, RuntimeUI.Terra, Some(azureContext))
+    ) shouldBe Some(1)
+    // Cromwell on GCP on Terra
+    test.get(
+      AppStatusMetric(CloudProvider.Gcp, AppType.Cromwell, AppStatus.Running, RuntimeUI.Terra, None)
+    ) shouldBe Some(1)
+    // Galaxy on GCP
+    test.get(
+      AppStatusMetric(CloudProvider.Gcp, AppType.Galaxy, AppStatus.Running, RuntimeUI.Terra, None)
+    ) shouldBe Some(1)
+    // Custom app on GCP
+    test.get(
+      AppStatusMetric(CloudProvider.Gcp, AppType.Custom, AppStatus.Running, RuntimeUI.Terra, None)
+    ) shouldBe Some(1)
+    // Cromwell on GCP on AoU
+    test.get(
+      AppStatusMetric(CloudProvider.Gcp, AppType.Cromwell, AppStatus.Running, RuntimeUI.AoU, None)
+    ) shouldBe Some(1)
+  }
+
+  it should "count runtimes by status" in {
+    val test = leoMetricsMonitor.countRuntimesByDbStatus(allRuntimes)
+    // 4 runtimes
+    test.size shouldBe 4
+    // Jupyter on GCP on Terra
+    test.get(
+      RuntimeStatusMetric(CloudProvider.Gcp, RuntimeImageType.Jupyter, RuntimeStatus.Running, RuntimeUI.Terra, None)
+    ) shouldBe Some(1)
+    // RStudio on GCP on Terra
+    test.get(
+      RuntimeStatusMetric(CloudProvider.Gcp, RuntimeImageType.RStudio, RuntimeStatus.Running, RuntimeUI.Terra, None)
+    ) shouldBe Some(1)
+    // Jupyter on Azure
+    test.get(
+      RuntimeStatusMetric(CloudProvider.Azure,
+                          RuntimeImageType.Jupyter,
+                          RuntimeStatus.Running,
+                          RuntimeUI.Terra,
+                          Some(azureContext)
+      )
+    ) shouldBe Some(1)
+    // Jupyter on GCP on AoU
+    test.get(
+      RuntimeStatusMetric(CloudProvider.Gcp, RuntimeImageType.Jupyter, RuntimeStatus.Running, RuntimeUI.AoU, None)
+    ) shouldBe Some(1)
+  }
+
+  it should "health check apps" in {
+    val test =
+      leoMetricsMonitor.countAppsByHealth(List(cromwellAppAzure, galaxyAppGcp)).unsafeRunSync()(IORuntime.global)
+    // An up and a down metric for 5 services: wds, cbas, cbas-ui, cromwell galaxy
+    test.size shouldBe 10
+    List("wds", "cromwell", "cbas", "cbas-ui").foreach { s =>
+      test.get(
+        AppHealthMetric(CloudProvider.Azure,
+                        AppType.Cromwell,
+                        ServiceName(s),
+                        RuntimeUI.Terra,
+                        Some(azureContext),
+                        s != "cbas"
+        )
+      ) shouldBe Some(1)
+      test.get(
+        AppHealthMetric(CloudProvider.Azure,
+                        AppType.Cromwell,
+                        ServiceName(s),
+                        RuntimeUI.Terra,
+                        Some(azureContext),
+                        s == "cbas"
+        )
+      ) shouldBe Some(0)
+    }
+    test.get(
+      AppHealthMetric(CloudProvider.Gcp, AppType.Galaxy, ServiceName("galaxy"), RuntimeUI.Terra, None, true)
+    ) shouldBe Some(1)
+    test.get(
+      AppHealthMetric(CloudProvider.Gcp, AppType.Galaxy, ServiceName("galaxy"), RuntimeUI.Terra, None, false)
+    ) shouldBe Some(0)
+  }
+
+  it should "health check runtimes" in {
+    val test = leoMetricsMonitor.countRuntimesByHealth(List(jupyterAzure, rstudioGcp)).unsafeRunSync()(IORuntime.global)
+    // An up and a down for jupyter, rstudio, welder * 2
+    test.size shouldBe 8
+    // Jupyter Azure
+    List("Jupyter", "Welder").foreach { s =>
+      test.get(
+        RuntimeHealthMetric(CloudProvider.Azure,
+                            RuntimeImageType.stringToRuntimeImageType(s),
+                            RuntimeUI.Terra,
+                            Some(azureContext),
+                            true
+        )
+      ) shouldBe Some(1)
+      test.get(
+        RuntimeHealthMetric(CloudProvider.Azure,
+                            RuntimeImageType.stringToRuntimeImageType(s),
+                            RuntimeUI.Terra,
+                            Some(azureContext),
+                            false
+        )
+      ) shouldBe Some(0)
+    }
+    // RStudio GCP
+    List("RStudio", "Welder").foreach { s =>
+      test.get(
+        RuntimeHealthMetric(CloudProvider.Gcp,
+                            RuntimeImageType.stringToRuntimeImageType(s),
+                            RuntimeUI.Terra,
+                            None,
+                            s != "RStudio"
+        )
+      ) shouldBe Some(1)
+      test.get(
+        RuntimeHealthMetric(CloudProvider.Gcp,
+                            RuntimeImageType.stringToRuntimeImageType(s),
+                            RuntimeUI.Terra,
+                            None,
+                            s == "RStudio"
+        )
+      ) shouldBe Some(0)
+    }
+  }
+
+  // Data generators
+
+  private def genApp(isAzure: Boolean,
+                     appType: AppType,
+                     isAou: Boolean,
+                     isCromwell: Boolean,
+                     isGalaxy: Boolean
+  ): KubernetesCluster = {
     val cluster = if (isAzure) makeAzureCluster(1) else makeKubeCluster(1)
-    val nodepool = makeNodepool(1, cluster.id)
+    val clusterWithAsyncFields = cluster.copy(asyncFields =
+      Some(
+        KubernetesClusterAsyncFields(IP("1.2.3.4"),
+                                     IP("2.4.5.6"),
+                                     NetworkFields(NetworkName("network"), SubnetworkName("subnet"), IpRange("ipRange"))
+        )
+      )
+    )
+    val nodepool = makeNodepool(1, clusterWithAsyncFields.id)
     val app = makeApp(1, nodepool.id).copy(
       appType = appType,
       status = AppStatus.Running,
       labels = if (isAou) Map(Config.uiConfig.allOfUsLabel -> "true") else Map(Config.uiConfig.terraLabel -> "true")
     )
-    cluster.copy(nodepools = List(nodepool.copy(apps = List(app))))
+    val services =
+      if (isCromwell) List("wds", "cbas", "cbas-ui", "cromwell") else if (isGalaxy) List("galaxy") else List("custom")
+    val appWithServices = app.copy(appResources = app.appResources.copy(services = services.map(genService)))
+    clusterWithAsyncFields.copy(nodepools = List(nodepool.copy(apps = List(appWithServices))))
   }
 
+  def genService(name: String): KubernetesService =
+    KubernetesService(ServiceId(-1), ServiceConfig(ServiceName(name), KubernetesServiceKindName("ClusterIP")))
+
   private def cromwellAppAzure: KubernetesCluster =
-    genApp(true, AppType.Cromwell, false).copy(cloudContext = CloudContext.Azure(azureContext))
-  private def cromwellAppGcp: KubernetesCluster = genApp(false, AppType.Cromwell, false)
-  private def galaxyAppGcp: KubernetesCluster = genApp(false, AppType.Galaxy, false)
-  private def customAppGcp: KubernetesCluster = genApp(false, AppType.Custom, false)
-  private def cromwellAppGcpAou: KubernetesCluster = genApp(false, AppType.Cromwell, true)
+    genApp(true, AppType.Cromwell, false, true, false).copy(cloudContext = CloudContext.Azure(azureContext))
+  private def cromwellAppGcp: KubernetesCluster = genApp(false, AppType.Cromwell, false, true, false)
+  private def galaxyAppGcp: KubernetesCluster = genApp(false, AppType.Galaxy, false, false, true)
+  private def customAppGcp: KubernetesCluster = genApp(false, AppType.Custom, false, false, false)
+  private def cromwellAppGcpAou: KubernetesCluster = genApp(false, AppType.Cromwell, true, true, false)
 
   private def allApps = List(cromwellAppAzure, cromwellAppGcp, galaxyAppGcp, customAppGcp, cromwellAppGcpAou)
 
@@ -114,50 +282,9 @@ class LeoMetricsMonitorSpec extends AnyFlatSpec with LeonardoTestSuite with Test
   private def jupyterAzure: RuntimeMetrics = genRuntime(true, false, false)
   private def jupyterGcpAou: RuntimeMetrics = genRuntime(true, true, true)
 
-  val allRuntimes = List(jupyterGcp, rstudioGcp, jupyterAzure, jupyterGcpAou)
+  private def allRuntimes = List(jupyterGcp, rstudioGcp, jupyterAzure, jupyterGcpAou)
 
-  "LeoMetricsMonitor" should "count apps by status" in {
-    val test = leoMetricsMonitor.countAppsByDbStatus(allApps)
-    test shouldBe Map(
-      AppStatusMetric(CloudProvider.Azure,
-                      AppType.Cromwell,
-                      AppStatus.Running,
-                      RuntimeUI.Terra,
-                      Some(azureContext)
-      ) -> 1,
-      AppStatusMetric(CloudProvider.Gcp, AppType.Cromwell, AppStatus.Running, RuntimeUI.Terra, None) -> 1,
-      AppStatusMetric(CloudProvider.Gcp, AppType.Galaxy, AppStatus.Running, RuntimeUI.Terra, None) -> 1,
-      AppStatusMetric(CloudProvider.Gcp, AppType.Custom, AppStatus.Running, RuntimeUI.Terra, None) -> 1,
-      AppStatusMetric(CloudProvider.Gcp, AppType.Cromwell, AppStatus.Running, RuntimeUI.AoU, None) -> 1
-    )
-  }
-
-  it should "count runtimes by status" in {
-    val test = leoMetricsMonitor.countRuntimesByDbStatus(allRuntimes)
-    test shouldBe Map(
-      RuntimeStatusMetric(CloudProvider.Gcp,
-                          RuntimeImageType.Jupyter,
-                          RuntimeStatus.Running,
-                          RuntimeUI.Terra,
-                          None
-      ) -> 1,
-      RuntimeStatusMetric(CloudProvider.Gcp,
-                          RuntimeImageType.RStudio,
-                          RuntimeStatus.Running,
-                          RuntimeUI.Terra,
-                          None
-      ) -> 1,
-      RuntimeStatusMetric(CloudProvider.Azure,
-                          RuntimeImageType.Jupyter,
-                          RuntimeStatus.Running,
-                          RuntimeUI.Terra,
-                          Some(azureContext)
-      ) -> 1,
-      RuntimeStatusMetric(CloudProvider.Gcp, RuntimeImageType.Jupyter, RuntimeStatus.Running, RuntimeUI.AoU, None) -> 1
-    )
-  }
-
-  // TODO test health check
+  // Mocks
 
   private def setUpMockSamDAO: SamDAO[IO] = {
     val sam = mock[SamDAO[IO]]
@@ -178,11 +305,12 @@ class LeoMetricsMonitorSpec extends AnyFlatSpec with LeonardoTestSuite with Test
     cromwell
   }
 
+  // CBAS is down
   private def setUpMockCbasDAO: CbasDAO[IO] = {
     val cbas = mock[CbasDAO[IO]]
     when {
       cbas.getStatus(any, any)(any)
-    } thenReturn IO.pure(true)
+    } thenReturn IO.pure(false)
     cbas
   }
 
@@ -218,11 +346,12 @@ class LeoMetricsMonitorSpec extends AnyFlatSpec with LeonardoTestSuite with Test
     jupyter
   }
 
+  // RStudio is down
   private def setUpMockRStudioDAO: RStudioDAO[IO] = {
     val rstudio = mock[RStudioDAO[IO]]
     when {
       rstudio.isProxyAvailable(any, any[String].asInstanceOf[RuntimeName])
-    } thenReturn IO.pure(true)
+    } thenReturn IO.pure(false)
     rstudio
   }
 
