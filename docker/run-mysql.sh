2c1899b631a878a81abe091e27f115338d428de3
Lily Nolting <117680805+mlilynolting@users.noreply.github.com>
2023-03-10 18:33:47 -0500

    Restore docker/run-mysql.sh

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -17,7 +17,6 @@ start() {
                -e MYSQL_DATABASE=leotestdb \
                -d \
                -p 3311:3306 \
-               --platform 'linux/amd64' \
                mysql/mysql-server:$MYSQL_VERSION
 
     # validate mysql
@@ -25,7 +24,6 @@ start() {
     docker run --rm \
                --link $CONTAINER:mysql \
                -v $PWD/docker/sql_validate.sh:/working/sql_validate.sh \
-               --platform 'linux/amd64' \
                mysql:$MYSQL_VERSION \
                /working/sql_validate.sh $TARGET
 
