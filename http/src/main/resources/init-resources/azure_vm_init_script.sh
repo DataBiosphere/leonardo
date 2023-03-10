27862f6d13b12e385041c53421888a7216ba6848
Robert Title <rtitle@broadinstitute.org>
2023-03-10 10:01:33 -0500

    Bugfix

--- http/src/main/resources/init-resources/azure_vm_init_script.sh
+++ http/src/main/resources/init-resources/azure_vm_init_script.sh
@@ -142,7 +142,7 @@ jq --null-input \
 /anaconda/envs/azureml_py38_PT_and_TF/bin/jupyter kernelspec list | awk 'NR>1 {print $2}' | while read line; do jq -s add $line"/kernel.json" wsenv.json > tmpkernel.json && mv tmpkernel.json $line"/kernel.json"; done
 
 # TODO: don't hardcode; allow passing in custom images dynamically
-CUSTOM_DOCKER=us.gcr.io/broad-dsp-gcr-public/rt-custom-scientific-kernel
+$CUSTOM_IMAGE=us.gcr.io/broad-dsp-gcr-public/rt-custom-scientific-kernel
 
 # Install custom kernel
 CUSTOM_KERNEL_DIR=/etc/custom_kernels
@@ -155,7 +155,7 @@ do
   mkdir $KERNEL_DIR
   # Copy kernel.json from inside the image, prepend `docker run ...` to the startup command
   docker run $CUSTOM_IMAGE 2>/dev/null cat $k/kernel.json \
-  | jq --arg custom_docker $CUSTOM_DOCKER '.argv |= ["docker", "run", "--network=host", "-v", "{connection_file}:/connection-spec", $custom_docker] + .' \
+  | jq --arg custom_image $CUSTOM_IMAGE '.argv |= ["docker", "run", "--network=host", "-v", "{connection_file}:/connection-spec", $custom_image] + .' \
   | jq --arg old {connection_file} --arg vol /connection-spec '(.argv[] | select(. == $old)) |= $vol' \
   > $KERNEL_DIR/kernel.json
   # Install the kernelspec on the DSVM
