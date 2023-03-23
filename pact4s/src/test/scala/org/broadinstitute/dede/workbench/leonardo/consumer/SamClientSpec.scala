29af3aa3e5b3907c9b299c9efae6a493c2972937
Ivan <ichang@broadinstitute.org>
2023-03-23 13:44:44 -0400

    Test webhook

--- pact4s/src/test/scala/org/broadinstitute/dede/workbench/leonardo/consumer/SamClientSpec.scala
+++ pact4s/src/test/scala/org/broadinstitute/dede/workbench/leonardo/consumer/SamClientSpec.scala
@@ -155,7 +155,7 @@ class SamClientSpec extends AnyFlatSpec with Matchers with RequestResponsePactFo
   // for Sam provider to generate the expected response
   var pactDslResponse: PactDslResponse = buildInteraction(
     pactProvider,
-    state = "Sam subsystems changes are still alright and ok and ok and ok ok",
+    state = "Sam subsystems changes are still alright and ok and ok and ok ok ok",
     stateParams = subsystems.map(s => s.toString() -> "ok").toMap,
     uponReceiving = "Request to get Sam ok status",
     method = "GET",
