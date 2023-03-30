53732f2f9eebe6ea26d574e4b3e814e25e9a55ad
Ivan <ichang@broadinstitute.org>
2023-03-30 14:34:27 -0400

    Publish changed pact (revision 6)

--- pact4s/src/test/scala/org/broadinstitute/dede/workbench/leonardo/consumer/SamClientSpec.scala
+++ pact4s/src/test/scala/org/broadinstitute/dede/workbench/leonardo/consumer/SamClientSpec.scala
@@ -155,7 +155,7 @@ class SamClientSpec extends AnyFlatSpec with Matchers with RequestResponsePactFo
   // for Sam provider to generate the expected response
   var pactDslResponse: PactDslResponse = buildInteraction(
     pactProvider,
-    state = "Sam is ok revision 7",
+    state = "Sam is ok revision 8",
     stateParams = subsystems.map(s => s.toString() -> "ok").toMap,
     uponReceiving = "Request to get Sam ok status",
     method = "GET",
