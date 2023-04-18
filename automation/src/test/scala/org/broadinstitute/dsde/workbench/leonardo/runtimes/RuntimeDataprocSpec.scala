cc2174864fc4999bcdf271f9e7ab35799d88e839
Yonghao Yu <yonghao@google.com>
2023-04-18 08:02:54 -0400

    increase disk size

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeDataprocSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/runtimes/RuntimeDataprocSpec.scala
@@ -253,9 +253,9 @@ class RuntimeDataprocSpec
         RuntimeConfigRequest.DataprocConfig(
           Some(2),
           Some(MachineTypeName("n1-standard-4")),
-          Some(DiskSize(130)),
+          Some(DiskSize(170)),
           Some(MachineTypeName("n1-standard-4")),
-          Some(DiskSize(150)),
+          Some(DiskSize(170)),
           None,
           None,
           Map.empty,
