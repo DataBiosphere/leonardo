4e1131ebb097497ba75b5052cd8c066816db7a98
Mneme Nolting <mneme@broadinstitute.org>
2023-05-10 21:58:25 -0400

    [IA-4081] move CORS handling inside the auth routes

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/api/HttpRoutes.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/api/HttpRoutes.scala
@@ -112,13 +112,26 @@ class HttpRoutes(
   val route: Route =
     logRequestResult {
       Route.seal(
-        corsSupport.corsHandler {
-          oidcConfig
-            .swaggerRoutes("swagger/api-docs.yaml") ~ oidcConfig.oauth2Routes ~ proxyRoutes.route ~ statusRoutes.route ~
-            pathPrefix("api") {
-              runtimeRoutes.routes ~ runtimeV2Routes.routes ~ diskRoutes.routes ~ kubernetesRoutes.routes ~ appV2Routes.routes ~ diskV2Routes.routes
-            }
-        }
+        concat(
+          oidcConfig.swaggerRoutes("swagger/api-docs.yaml"),
+          oidcConfig.oauth2Routes,
+          corsSupport.corsHandler {
+            concat(
+              proxyRoutes.route,
+              statusRoutes.route,
+              pathPrefix("api") {
+                concat(
+                  runtimeRoutes.routes,
+                  runtimeV2Routes.routes,
+                  diskRoutes.routes,
+                  kubernetesRoutes.routes,
+                  appV2Routes.routes,
+                  diskV2Routes.routes
+                )
+              }
+            )
+          }
+        )
       )
     }
 }
