8e2d21a4a025f875db5cc1d36dc38e34a07e06b0
Yonghao Yu <yonghao@google.com>
2023-05-16 13:27:05 -0400

    add new kernel

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebooksListPage.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebooksListPage.scala
@@ -22,8 +22,13 @@ case object Python3 extends NotebookKernel {
   override def cssSelectorString: String = super.cssSelectorString + "[title='Create a new notebook with Python 3']"
 }
 
+case object Python3Ipykernel extends NotebookKernel {
+  def string: String = "Python 3 (ipykernel)"
+  override def cssSelectorString: String = super.cssSelectorString + "[title='Create a new notebook with Python 3 (ipykernel)']"
+}
+
 case object PySpark2 extends NotebookKernel {
-  def string: String = "PySpark 2"
+  def string: String = "PySpark 2"◊
   override def cssSelectorString: String = super.cssSelectorString + "[title='Create a new notebook with PySpark 2']"
 }
 
