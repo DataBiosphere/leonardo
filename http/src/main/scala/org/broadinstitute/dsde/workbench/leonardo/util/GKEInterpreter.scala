bbe8701bcc0ae8a3b072cc7216d596ed44b0cebc
LizBaldo <baldo@broadinstitute.org>
2023-05-24 09:52:15 -0400

    match galaxy ingress annotations

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GKEInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GKEInterpreter.scala
@@ -1725,11 +1725,11 @@ class GKEInterpreter[F[_]](
 //    ,
 //    raw"""ingress.annotations.nginx\.ingress\.kubernetes\.io/ssl-redirect="true""""
 //    ,
+    raw"""ingress.annotations.nginx\.ingress\.kubernetes\.io/auth-tls-secret=${namespaceName.value}/ca-secret""",
 
     val rewriteTarget = "$2"
     val ingress = List(
       raw"""ingress.enabled=true""",
-      raw"""ingress.annotations.nginx\.ingress\.kubernetes\.io/auth-tls-secret=${namespaceName.value}/ca-secret""",
       raw"""ingress.annotations.nginx\.ingress\.kubernetes\.io/proxy-redirect-from=https://${k8sProxyHost}""",
       raw"""ingress.annotations.nginx\.ingress\.kubernetes\.io/proxy-redirect-to=${leoProxyhost}""",
       raw"""ingress.annotations.nginx\.ingress\.kubernetes\.io/rewrite-target=/${rewriteTarget}""",
