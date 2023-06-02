b5d6dc695d9e0a9ba14c773d095cf6778f4c9c15
n8landolt <nlandolt@broadinstitute.org>
2023-06-02 15:34:12 -0400

    Updated terra-azure-relay-listener hash to the up to date dev version.

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -73,7 +73,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
                 "https://raw.githubusercontent.com/DataBiosphere/leonardo/270bd6aad916344fadc06d1a51629c432da663a8/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
-            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:3d0c8bd",
+            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:89e87c1",
             VMCredential(username = "username", password = "password")
           )
         ),
