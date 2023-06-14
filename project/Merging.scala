95b338c93bc56bf63974b25e7610e718831247a6
lmcnatt <mcnatt@broadinstitute.org>
2023-06-14 13:55:15 -0400

    trying a diff one

--- project/Merging.scala
+++ project/Merging.scala
@@ -11,8 +11,8 @@ object Merging {
     // [error] Deduplicate found different file contents in the following:
     // [error]   Jar name = auto-value-1.10.1.jar, jar org = com.google.auto.value, entry target = META-INF/kotlin-stdlib-common.kotlin_module
     // [error]   Jar name = kotlin-stdlib-1.6.20.jar, jar org = org.jetbrains.kotlin, entry target = META-INF/kotlin-stdlib-common.kotlin_module
-    case PathList("META-INF", "kotlin-stdlib-common.kotlin_module")               => MergeStrategy.preferProject
-    case PathList("org", "joda", "time", "base", "BaseDateTime.class", "jakarta") => MergeStrategy.first
+    case PathList("META-INF", "kotlin-stdlib-common.kotlin_module")    => MergeStrategy.preferProject
+    case PathList("org", "joda", "time", "base", "BaseDateTime.class") => MergeStrategy.first
     // For the following error:
     // [error] java.lang.RuntimeException: deduplicate: different file contents found in the following:
     // [error] /root/.cache/coursier/v1/https/repo1.maven.org/maven2/com/google/protobuf/protobuf-java/3.11.4/protobuf-java-3.11.4.jar:google/protobuf/field_mask.proto
@@ -27,6 +27,12 @@ object Merging {
     case "module-info.class" =>
       MergeStrategy.discard // JDK 8 does not use the file module-info.class so it is safe to discard the file.
     case "reference.conf" => MergeStrategy.concat
+    // For the following error:
+    // [error] Deduplicate found different file contents in the following:
+    // [error]   Jar name = jakarta.activation-1.2.2.jar, jar org = com.sun.activation, entry target = javax/activation/SecuritySupport$5.class
+    // [error]   Jar name = javax.activation-api-1.2.0.jar, jar org = javax.activation, entry target = javax/activation/SecuritySupport$5.class
+    // case PathList("META-INF", "kotlin-stdlib-common.kotlin_module")    => MergeStrategy.preferProject
+    case x if x.contains("activation") => MergeStrategy.first
     case x                => oldStrategy(x)
   }
 }
