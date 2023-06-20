95ba262c629ad9875282034431d64db33e0bea5f
erdalcosgun <31049564+erdalcosgun@users.noreply.github.com>
2023-06-20 15:44:49 -0700

    Update WsmCodecSpec.scala

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/dao/WsmCodecSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/dao/WsmCodecSpec.scala
@@ -97,7 +97,7 @@ class WsmCodecSpec extends AnyFlatSpec with Matchers {
          |      "publisher": "microsoft-dsvm",
          |      "offer": "ubuntu-2004",
          |      "sku": "2004-gen2",
-         |      "version": "23.01.06"
+         |      "version": "23.04.24"
          |    },
          |    "customScriptExtension": {
          |      "name": "vm-custom-script-extension",
