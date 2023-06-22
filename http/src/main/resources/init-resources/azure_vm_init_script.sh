b90f893e496d1f36cee2e58d91955ff6923b25ec
erdalcosgun <31049564+erdalcosgun@users.noreply.github.com>
2023-06-22 12:58:35 -0700

    Update azure_vm_init_script.sh

--- http/src/main/resources/init-resources/azure_vm_init_script.sh
+++ http/src/main/resources/init-resources/azure_vm_init_script.sh
@@ -187,6 +187,8 @@ echo "Y"| /anaconda/envs/py38_default/bin/pip3 install ipykernel pydevd
 
 echo "Y"| /anaconda/envs/py38_default/bin/python3 -m ipykernel install
 
+docker pull nvcr.io/nvidia/clara/clara-parabricks:4.1.1-1
+
 # Start Jupyter server with custom parameters
 sudo runuser -l $VM_JUP_USER -c "mkdir -p /home/$VM_JUP_USER/.jupyter"
 sudo runuser -l $VM_JUP_USER -c "wget -qP /home/$VM_JUP_USER/.jupyter https://raw.githubusercontent.com/DataBiosphere/leonardo/ea519ef899de28e27e2a37ba368433da9fd03b7f/http/src/main/resources/init-resources/jupyter_server_config.py"
