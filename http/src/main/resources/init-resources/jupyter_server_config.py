c8ef259b6ce93c843c80af549595c1991c5bea99
LizBaldo <baldo@broadinstitute.org>
2023-07-06 14:25:49 -0400

    point to the server config file

--- http/src/main/resources/init-resources/jupyter_server_config.py
+++ http/src/main/resources/init-resources/jupyter_server_config.py
@@ -1,3 +1,4 @@
+# Jupyter server config file for Azure VMs only!
 # Need to update the link in the azure_vm_init_script.sh with your commit hash to update this config
 c.ServerApp.quit_button=False
 c.ServerApp.certfile=''
@@ -7,4 +8,4 @@ c.ServerApp.token=''
 c.ServerApp.ip=''
 c.ServerApp.allow_origin="*"
 c.ServerApp.disable_check_xsrf=True # to prevent 'xsrf missing from POST' error https://broadworkbench.atlassian.net/browse/IA-4284
-c.ServerApp.contents_manager_class=jupyter_delocalize.WelderContentsManager
+# c.ServerApp.contents_manager_class=jupyter_delocalize.WelderContentsManager
