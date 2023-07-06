6d649ad057b0d40352d7aab85cab8b1f0555716d
Mneme Nolting <mneme@broadinstitute.org>
2023-07-06 10:35:31 -0400

    [IA-4401] Bump terra-azure-relay-listeners to latest tag

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -68,7 +68,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
                 "https://raw.githubusercontent.com/DataBiosphere/leonardo/270bd6aad916344fadc06d1a51629c432da663a8/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
-            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:7c9a542",
+            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:f74dc62",
             VMCredential(username = "username", password = "password")
           )
         ),
