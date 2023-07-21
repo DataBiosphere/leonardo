d33ebd2bd8568a7f3ba86fa259ba99a0ec83db2a
Mneme Nolting <mneme@broadinstitute.org>
2023-07-20 21:25:59 -0400

    [IA-4401] Bump tarl version to enable x-action header

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -68,7 +68,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
                 "https://raw.githubusercontent.com/DataBiosphere/leonardo/270bd6aad916344fadc06d1a51629c432da663a8/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
-            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:8b92d76",
+            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:bd0b1e5",
             VMCredential(username = "username", password = "password")
           )
         ),
