c4071091ac85d112693bf42df6831f7521bbdaa6
broadbot <broadbot@broadinstitute.org>
2023-08-31 20:21:01 +0000

    Auto-update workflows-app version to 0.27.0

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -139,7 +139,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
         ),
         WorkflowsAppConfig(
           ChartName("/leonardo/workflows-app"),
-          ChartVersion("0.24.0"),
+          ChartVersion("0.27.0"),
           ReleaseNameSuffix("wfa-rls"),
           NamespaceNameSuffix("wfa-ns"),
           KsaName("wfa-ksa"),
