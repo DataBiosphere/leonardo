efbf13771440769b0343946bf8ee214df00cfe3c
Saloni Shah <salonipshah11@gmail.com>
2023-08-31 09:17:00 -0400

    formatter

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/util/AKSInterpreterSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/util/AKSInterpreterSpec.scala
@@ -592,38 +592,38 @@ class AKSInterpreterSpec extends AnyFlatSpecLike with TestComponent with Leonard
     )
     overrides.asString shouldBe
       "config.resourceGroup=mrg," +
-        "config.batchAccountKey=batchKey," +
-        "config.batchAccountName=batch," +
-        "config.batchNodesSubnetId=subnet1," +
-        s"config.drsUrl=${ConfigReader.appConfig.drs.url}," +
-        "config.landingZoneId=5c12f64b-f4ac-4be1-ae4a-4cace5de807d," +
-        "config.subscriptionId=sub," +
-        s"config.region=${azureRegion}," +
-        "config.applicationInsightsConnectionString=applicationInsightsConnectionString," +
-        "relay.path=https://relay.com/app," +
-        "persistence.storageResourceGroup=mrg," +
-        "persistence.storageAccount=storage," +
-        "persistence.blobContainer=sc-container," +
-        "persistence.leoAppInstanceName=app," +
-        s"persistence.workspaceManager.url=${ConfigReader.appConfig.azure.wsm.uri.renderString}," +
-        s"persistence.workspaceManager.workspaceId=${workspaceId.value}," +
-        s"persistence.workspaceManager.containerResourceId=${storageContainer.resourceId.value.toString}," +
-        "identity.enabled=false," +
-        "identity.name=identity-name," +
-        "identity.resourceId=identity-id," +
-        "identity.clientId=identity-client-id," +
-        "workloadIdentity.enabled=true," +
-        "workloadIdentity.serviceAccountName=identity-name," +
-        "cromwell.enabled=true," +
-        "fullnameOverride=cra-rel-1," +
-        "instrumentationEnabled=false," +
-        s"provenance.userAccessToken=${petUserInfo.accessToken.token}," +
-        "postgres.podLocalDatabaseEnabled=false," +
-        s"postgres.host=${lzResources.postgresServer.map(_.name).get}.postgres.database.azure.com," +
-        "postgres.pgbouncer.enabled=true," +
-        "postgres.user=identity-name," +
-        s"postgres.dbnames.cromwell=${databaseNames.cromwellRunner}," +
-        s"postgres.dbnames.tes=${databaseNames.tes}"
+      "config.batchAccountKey=batchKey," +
+      "config.batchAccountName=batch," +
+      "config.batchNodesSubnetId=subnet1," +
+      s"config.drsUrl=${ConfigReader.appConfig.drs.url}," +
+      "config.landingZoneId=5c12f64b-f4ac-4be1-ae4a-4cace5de807d," +
+      "config.subscriptionId=sub," +
+      s"config.region=${azureRegion}," +
+      "config.applicationInsightsConnectionString=applicationInsightsConnectionString," +
+      "relay.path=https://relay.com/app," +
+      "persistence.storageResourceGroup=mrg," +
+      "persistence.storageAccount=storage," +
+      "persistence.blobContainer=sc-container," +
+      "persistence.leoAppInstanceName=app," +
+      s"persistence.workspaceManager.url=${ConfigReader.appConfig.azure.wsm.uri.renderString}," +
+      s"persistence.workspaceManager.workspaceId=${workspaceId.value}," +
+      s"persistence.workspaceManager.containerResourceId=${storageContainer.resourceId.value.toString}," +
+      "identity.enabled=false," +
+      "identity.name=identity-name," +
+      "identity.resourceId=identity-id," +
+      "identity.clientId=identity-client-id," +
+      "workloadIdentity.enabled=true," +
+      "workloadIdentity.serviceAccountName=identity-name," +
+      "cromwell.enabled=true," +
+      "fullnameOverride=cra-rel-1," +
+      "instrumentationEnabled=false," +
+      s"provenance.userAccessToken=${petUserInfo.accessToken.token}," +
+      "postgres.podLocalDatabaseEnabled=false," +
+      s"postgres.host=${lzResources.postgresServer.map(_.name).get}.postgres.database.azure.com," +
+      "postgres.pgbouncer.enabled=true," +
+      "postgres.user=identity-name," +
+      s"postgres.dbnames.cromwell=${databaseNames.cromwellRunner}," +
+      s"postgres.dbnames.tes=${databaseNames.tes}"
   }
 
   it should "create and poll a coa app, then successfully delete it" in isolatedDbTest {
@@ -679,12 +679,16 @@ class AKSInterpreterSpec extends AnyFlatSpecLike with TestComponent with Leonard
     deletion.unsafeRunSync()(cats.effect.unsafe.IORuntime.global)
   }
 
-  for (appType <- List(AppType.Wds, AppType.Cromwell, AppType.CromwellRunnerApp, AppType.HailBatch, AppType.WorkflowsApp))
+  for (
+    appType <- List(AppType.Wds, AppType.Cromwell, AppType.CromwellRunnerApp, AppType.HailBatch, AppType.WorkflowsApp)
+  )
     it should s"create and poll a shared ${appType} app, then successfully delete it" in isolatedDbTest {
       val mockAzureRelayService = setUpMockAzureRelayService
 
       val aksInterp = new AKSInterpreter[IO](
-        config.copy(cromwellRunnerAppConfig = config.cromwellRunnerAppConfig.copy(enabled = true), workflowsAppConfig = config.workflowsAppConfig.copy(enabled = true)),
+        config.copy(cromwellRunnerAppConfig = config.cromwellRunnerAppConfig.copy(enabled = true),
+                    workflowsAppConfig = config.workflowsAppConfig.copy(enabled = true)
+        ),
         MockHelm,
         mockAzureBatchService,
         mockAzureContainerService,
