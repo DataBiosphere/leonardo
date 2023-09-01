8fe01ab0bd52596a3aba473326b3d69e2ba90c1f
Robert Title <rtitle@broadinstitute.org>
2023-09-01 13:52:54 -0400

    upgrade App

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/app/CromwellAppInstall.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/app/CromwellAppInstall.scala
@@ -4,18 +4,11 @@ import cats.effect.Async
 import cats.mtl.Ask
 import cats.syntax.all._
 import org.broadinstitute.dsde.workbench.azure.{AzureApplicationInsightsService, AzureBatchService}
-import org.broadinstitute.dsde.workbench.google2.KubernetesSerializableName.ServiceAccountName
-import org.broadinstitute.dsde.workbench.leonardo.app.AppInstall.Database
 import org.broadinstitute.dsde.workbench.leonardo.config.WorkflowsAppService.{Cbas, CbasUI, Cromwell}
 import org.broadinstitute.dsde.workbench.leonardo.dao._
 import org.broadinstitute.dsde.workbench.leonardo.http._
-import org.broadinstitute.dsde.workbench.leonardo.util.{
-  AKSInterpreterConfig,
-  AppCreationException,
-  AppUpdateException,
-  CreateAKSAppParams
-}
-import org.broadinstitute.dsde.workbench.leonardo.{App, AppContext, PostgresServer}
+import org.broadinstitute.dsde.workbench.leonardo.util.{AppCreationException, AppUpdateException}
+import org.broadinstitute.dsde.workbench.leonardo.{AppContext, PostgresServer}
 import org.broadinstitute.dsp.Values
 import org.http4s.Uri
 import org.http4s.headers.Authorization
@@ -30,19 +23,15 @@ class CromwellAppInstall[F[_]](samDao: SamDAO[F],
   F: Async[F]
 ) extends AppInstall[F] {
 
-  override def databases: List[AppInstall.Database] =
+  override def databases: List[Database] =
     List(
       Database("cromwell", allowAccessForAllWorkspaceUsers = false),
       Database("cbas", allowAccessForAllWorkspaceUsers = false),
       Database("tes", allowAccessForAllWorkspaceUsers = false)
     )
 
-  override def helmValues(params: CreateAKSAppParams,
-                          config: AKSInterpreterConfig,
-                          app: App,
-                          relayPath: Uri,
-                          ksaName: ServiceAccountName,
-                          databaseNames: List[String]
+  override def buildHelmOverrideValues(
+    params: BuildHelmOverrideValuesParams
   )(implicit ev: Ask[F, AppContext]): F[Values] = for {
     ctx <- ev.ask
 
@@ -62,10 +51,10 @@ class CromwellAppInstall[F[_]](samDao: SamDAO[F],
     )
 
     // Get the pet userToken
-    tokenOpt <- samDao.getCachedArbitraryPetAccessToken(app.auditInfo.creator)
+    tokenOpt <- samDao.getCachedArbitraryPetAccessToken(params.app.auditInfo.creator)
     userToken <- F.fromOption(
       tokenOpt,
-      AppUpdateException(s"Pet not found for user ${app.auditInfo.creator}", Some(ctx.traceId))
+      AppUpdateException(s"Pet not found for user ${params.app.auditInfo.creator}", Some(ctx.traceId))
     )
 
     values = List(
@@ -74,56 +63,56 @@ class CromwellAppInstall[F[_]](samDao: SamDAO[F],
       raw"config.batchAccountKey=${batchAccount.getKeys().primary}",
       raw"config.batchAccountName=${params.landingZoneResources.batchAccountName.value}",
       raw"config.batchNodesSubnetId=${params.landingZoneResources.batchNodesSubnetName.value}",
-      raw"config.drsUrl=${config.drsConfig.url}",
+      raw"config.drsUrl=${params.config.drsConfig.url}",
       raw"config.landingZoneId=${params.landingZoneResources.landingZoneId}",
       raw"config.subscriptionId=${params.cloudContext.subscriptionId.value}",
       raw"config.region=${params.landingZoneResources.region}",
       raw"config.applicationInsightsConnectionString=${applicationInsightsComponent.connectionString()}",
 
       // relay configs
-      raw"relay.path=${relayPath.renderString}",
+      raw"relay.path=${params.relayPath.renderString}",
 
       // persistence configs
       raw"persistence.storageResourceGroup=${params.cloudContext.managedResourceGroupName.value}",
       raw"persistence.storageAccount=${params.landingZoneResources.storageAccountName.value}",
       raw"persistence.blobContainer=${storageContainer.name.value}",
-      raw"persistence.leoAppInstanceName=${app.appName.value}",
-      raw"persistence.workspaceManager.url=${config.wsmConfig.uri.renderString}",
+      raw"persistence.leoAppInstanceName=${params.app.appName.value}",
+      raw"persistence.workspaceManager.url=${params.config.wsmConfig.uri.renderString}",
       raw"persistence.workspaceManager.workspaceId=${params.workspaceId.value}",
       raw"persistence.workspaceManager.containerResourceId=${storageContainer.resourceId.value.toString}",
 
       // identity configs
       raw"identity.enabled=false",
       raw"workloadIdentity.enabled=true",
-      raw"workloadIdentity.serviceAccountName=${ksaName.value}",
+      raw"workloadIdentity.serviceAccountName=${params.ksaName.value}",
 
       // Sam configs
-      raw"sam.url=${config.samConfig.server}",
+      raw"sam.url=${params.config.samConfig.server}",
 
       // Leo configs
-      raw"leonardo.url=${config.leoUrlBase}",
+      raw"leonardo.url=${params.config.leoUrlBase}",
 
       // Enabled services configs
-      raw"cbas.enabled=${config.coaAppConfig.coaServices.contains(Cbas)}",
-      raw"cbasUI.enabled=${config.coaAppConfig.coaServices.contains(CbasUI)}",
-      raw"cromwell.enabled=${config.coaAppConfig.coaServices.contains(Cromwell)}",
-      raw"dockstore.baseUrl=${config.coaAppConfig.dockstoreBaseUrl}",
+      raw"cbas.enabled=${params.config.coaAppConfig.coaServices.contains(Cbas)}",
+      raw"cbasUI.enabled=${params.config.coaAppConfig.coaServices.contains(CbasUI)}",
+      raw"cromwell.enabled=${params.config.coaAppConfig.coaServices.contains(Cromwell)}",
+      raw"dockstore.baseUrl=${params.config.coaAppConfig.dockstoreBaseUrl}",
 
       // general configs
-      raw"fullnameOverride=coa-${app.release.asString}",
-      raw"instrumentationEnabled=${config.coaAppConfig.instrumentationEnabled}",
+      raw"fullnameOverride=coa-${params.app.release.asString}",
+      raw"instrumentationEnabled=${params.config.coaAppConfig.instrumentationEnabled}",
       // provenance (app-cloning) configs
       raw"provenance.userAccessToken=${userToken}"
     )
 
-    postgresConfig = (databaseNames, params.landingZoneResources.postgresServer) match {
+    postgresConfig = (params.databaseNames, params.landingZoneResources.postgresServer) match {
       case (List(cromwell, cbas, tes), Some(PostgresServer(dbServerName, pgBouncerEnabled))) =>
         List(
           raw"postgres.podLocalDatabaseEnabled=false",
           raw"postgres.host=$dbServerName.postgres.database.azure.com",
           raw"postgres.pgbouncer.enabled=$pgBouncerEnabled",
           // convention is that the database user is the same as the service account name
-          raw"postgres.user=${ksaName.value}",
+          raw"postgres.user=${params.ksaName.value}",
           raw"postgres.dbnames.cromwell=$cromwell",
           raw"postgres.dbnames.cbas=$cbas",
           raw"postgres.dbnames.tes=$tes"
