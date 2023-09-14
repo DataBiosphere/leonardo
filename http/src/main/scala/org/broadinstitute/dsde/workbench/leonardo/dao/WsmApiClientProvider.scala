818814e9f0dad1c7183ff02ab0163788741b903f
Robert Title <rtitle@broadinstitute.org>
2023-09-14 08:50:38 -0400

    WOR-1250: Update WSM client to use names instead of ids (#3699)

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/WsmApiClientProvider.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/WsmApiClientProvider.scala
@@ -1,7 +1,15 @@
 package org.broadinstitute.dsde.workbench.leonardo.dao
 
+import bio.terra.common.tracing.JerseyTracingFilter
 import bio.terra.workspace.api.{ControlledAzureResourceApi, ResourceApi}
 import bio.terra.workspace.client.ApiClient
+import cats.effect.Async
+import cats.mtl.Ask
+import cats.syntax.all._
+import io.opencensus.trace.Tracing
+import org.broadinstitute.dsde.workbench.leonardo.AppContext
+import org.broadinstitute.dsde.workbench.leonardo.util.WithSpanFilter
+import org.glassfish.jersey.client.ClientConfig
 import org.http4s.Uri
 
 /**
@@ -11,27 +19,33 @@ import org.http4s.Uri
  * Based on `org/broadinstitute/dsde/rawls/dataaccess/workspacemanager/WorkspaceManagerApiClientProvider.scala`
  *
  */
-trait WsmApiClientProvider {
-  def getControlledAzureResourceApi(token: String): ControlledAzureResourceApi
-  def getResourceApi(token: String): ResourceApi
+trait WsmApiClientProvider[F[_]] {
+  def getControlledAzureResourceApi(token: String)(implicit ev: Ask[F, AppContext]): F[ControlledAzureResourceApi]
+  def getResourceApi(token: String)(implicit ev: Ask[F, AppContext]): F[ResourceApi]
 }
 
-class HttpWsmClientProvider(baseWorkspaceManagerUrl: Uri) extends WsmApiClientProvider {
-  private def getApiClient: ApiClient = {
-    val client: ApiClient = new ApiClient()
-    client.setBasePath(baseWorkspaceManagerUrl.renderString)
-    client
-  }
+class HttpWsmClientProvider[F[_]](baseWorkspaceManagerUrl: Uri)(implicit F: Async[F]) extends WsmApiClientProvider[F] {
+  private def getApiClient(token: String)(implicit ev: Ask[F, AppContext]): F[ApiClient] =
+    for {
+      ctx <- ev.ask
+      client = new ApiClient() {
+        override def performAdditionalClientConfiguration(clientConfig: ClientConfig): Unit = {
+          super.performAdditionalClientConfiguration(clientConfig)
+          ctx.span.foreach { span =>
+            clientConfig.register(new WithSpanFilter(span))
+            clientConfig.register(new JerseyTracingFilter(Tracing.getTracer))
+          }
+        }
+      }
+      _ = client.setBasePath(baseWorkspaceManagerUrl.renderString)
+      _ = client.setAccessToken(token)
+    } yield client
 
-  def getControlledAzureResourceApi(token: String): ControlledAzureResourceApi = {
-    val apiClient = getApiClient
-    apiClient.setAccessToken(token)
-    new ControlledAzureResourceApi(apiClient)
-  }
+  override def getControlledAzureResourceApi(token: String)(implicit
+    ev: Ask[F, AppContext]
+  ): F[ControlledAzureResourceApi] =
+    getApiClient(token).map(apiClient => new ControlledAzureResourceApi(apiClient))
 
-  def getResourceApi(token: String): ResourceApi = {
-    val apiClient = getApiClient
-    apiClient.setAccessToken(token)
-    new ResourceApi(apiClient)
-  }
+  override def getResourceApi(token: String)(implicit ev: Ask[F, AppContext]): F[ResourceApi] =
+    getApiClient(token).map(apiClient => new ResourceApi(apiClient))
 }
