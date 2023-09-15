f805208178d1e0cd487d339adfb0b327ec77e21d
broadbot <broadbot@broadinstitute.org>
2023-09-15 12:03:04 +0000

    Revert commit(s) 944a5a4fd

--- project/Settings.scala
+++ project/Settings.scala
@@ -87,7 +87,7 @@ object Settings {
   val commonSettings =
     commonBuildSettings ++ List(
     organization  := "org.broadinstitute.dsde.workbench",
-    scalaVersion  := "2.13.12",
+    scalaVersion  := "2.13.11",
     resolvers ++= commonResolvers,
     scalacOptions ++= commonCompilerSettings
   )
