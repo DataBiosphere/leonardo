1b2f6556f3ad33df8897574430bdee8d92b62acd
LizBaldo <baldo@broadinstitute.org>
2023-09-19 09:59:50 -0400

    address old TODOs to finally use the latest workbench-libs

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,11 +18,9 @@ object Dependencies {
   val pact4sV = "0.10.0"
 
   private val workbenchLibsHash = "7362eef"
-  val serviceTestV = s"4.1-ce68967"
+  val serviceTestV = s"4.1-$workbenchLibsHash"
   val workbenchModelV = s"0.19-$workbenchLibsHash"
-
-  // TODO update to 0.26 - DataprocInterpreter relies on deprecated class MemberType
-  val workbenchGoogleV = s"0.29-8376167"
+  val workbenchGoogleV = s"0.29-$workbenchLibsHash"
   val workbenchGoogle2V = s"0.33-$workbenchLibsHash"
   val workbenchOpenTelemetryV = s"0.7-$workbenchLibsHash"
   val workbenchOauth2V = s"0.5-$workbenchLibsHash"
