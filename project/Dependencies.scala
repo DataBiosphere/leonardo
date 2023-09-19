ec816f6ed76f4e55c962f3fe3b94908ab6d1d63c
LizBaldo <baldo@broadinstitute.org>
2023-09-19 11:57:18 -0400

    remove pre SNAP version comment for leo client library

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -222,7 +222,7 @@ object Dependencies {
   )
 
   val workbenchServiceTest: ModuleID = "org.broadinstitute.dsde.workbench" %% "workbench-service-test" % serviceTestV % "test" classifier "tests" excludeAll (excludeGuava, excludeStatsD)
-  val leonardoClient: ModuleID =  "org.broadinstitute.dsde.workbench" %% "leonardo-client" % "1.3.6-7579402"//"1.3.6-9d5d754"
+  val leonardoClient: ModuleID =  "org.broadinstitute.dsde.workbench" %% "leonardo-client" % "1.3.6-7579402"
 
   val automationDependencies = List(
     "com.fasterxml.jackson.module" %% "jackson-module-scala"   % "2.15.2" % "test",
