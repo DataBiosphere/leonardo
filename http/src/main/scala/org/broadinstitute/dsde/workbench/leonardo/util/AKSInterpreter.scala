dc473a6d80eb7f6967301d9d0ac55c3aec664bda
Michael Spector <michael.l.spector@gmail.com>
2023-09-26 13:00:12 -0400

    fix enums

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/AKSInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/AKSInterpreter.scala
@@ -135,7 +135,7 @@ class AKSInterpreter[F[_]](config: AKSInterpreterConfig,
       wsmResourceApi <- buildWsmResourceApiClient
       wsmDatabasesFromResourcesApi <- F.delay(
         wsmResourceApi
-          .enumerateResources(params.workspaceId.value, 0, 100, WsmResourceType.AzureDatabase, "CONTROLLED") // formerly StewardshipType.CONTROLLED; where does that come from?
+          .enumerateResources(params.workspaceId.value, 0, 100, ResourceType.AZURE_DATABASE, StewardshipType.CONTROLLED)
           .getResources
           .asScala
       )
