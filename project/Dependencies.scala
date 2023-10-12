f4e2bf85458be858129b40f1d3ec962bc12015df
Robert Title <rtitle@broadinstitute.org>
2023-10-12 12:23:56 -0400

    Update wb-libs to non-SNAP

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val munitCatsEffectV = "1.0.7"
   val pact4sV = "0.10.0"
 
-  private val workbenchLibsHash = "557248c4-SNAP"
+  private val workbenchLibsHash = "f31b537"
   val serviceTestV = s"4.1-$workbenchLibsHash"
   val workbenchModelV = s"0.19-$workbenchLibsHash"
   val workbenchGoogleV = s"0.29-$workbenchLibsHash"
