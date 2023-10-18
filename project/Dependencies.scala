d48fce1d3883c7d5b9b72f2c68b717b02ed2c34e
Robert Title <rtitle@broadinstitute.org>
2023-10-18 12:46:05 -0400

    IA-4607: Address IO blocking calls for GCE, Dataproc, and GKE

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val munitCatsEffectV = "1.0.7"
   val pact4sV = "0.10.0"
 
-  private val workbenchLibsHash = "f31b537"
+  private val workbenchLibsHash = "8711e331-SNAP"
   val serviceTestV = s"4.1-$workbenchLibsHash"
   val workbenchModelV = s"0.19-$workbenchLibsHash"
   val workbenchGoogleV = s"0.29-$workbenchLibsHash"
