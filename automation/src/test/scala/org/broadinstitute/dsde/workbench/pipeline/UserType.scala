88c9217db0b97e2357d13734bfc742240a22d8e5
ichengchang <50877414+ichengchang@users.noreply.github.com>
2023-10-24 11:36:51 -0400

    IA-4501: Use the new GHA step for billing project creation outside of tests (#3796)
    Co-authored-by: lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>
    Co-authored-by: Justin Canas <justincanas16@gmail.com>

---
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/pipeline/UserType.scala
@@ -0,0 +1,82 @@
+package org.broadinstitute.dsde.workbench.pipeline
+
+import io.circe.{Decoder, Encoder}
+
+/**
+ * Enum-like sealed trait representing the user type.
+ */
+sealed trait UserType { def title: String }
+
+/**
+ * Enum-like user type for title 'Owner', 'Student'
+ *
+ * The user types assignment came directly from the original test horde users below.
+ *
+ * @example
+ *
+ * {
+ *   "admins": {
+ *     "dumbledore": "dumbledore.admin@quality.firecloud.org",
+ *     "voldemort": "voldemort.admin@quality.firecloud.org"
+ *   },
+ *   "owners": {
+ *     "hermione": "hermione.owner@quality.firecloud.org",
+ *     "sirius": "sirius.owner@quality.firecloud.org",
+ *     "tonks": "tonks.owner@quality.firecloud.org"
+ *   },
+ *   "curators": {
+ *     "mcgonagall": "mcgonagall.curator@quality.firecloud.org",
+ *     "snape": "snape.curator@quality.firecloud.org",
+ *     "hagrid": "hagrid.curator@quality.firecloud.org",
+ *     "lupin": "lupin.curator@quality.firecloud.org",
+ *     "flitwick": "flitwick.curator@quality.firecloud.org"
+ *   },
+ *   "authdomains": {
+ *     "fred": "fred.authdomain@quality.firecloud.org",
+ *     "george": "george.authdomain@quality.firecloud.org",
+ *     "bill": "bill.authdomain@quality.firecloud.org",
+ *     "percy": "percy.authdomain@quality.firecloud.org",
+ *     "molly": "molly.authdomain@quality.firecloud.org",
+ *     "arthur": "arthur.authdomain@quality.firecloud.org"
+ *   },
+ *   "students": {
+ *     "harry": "harry.potter@quality.firecloud.org",
+ *     "ron": "ron.weasley@quality.firecloud.org",
+ *     "lavender": "lavender.brown@quality.firecloud.org",
+ *     "cho": "cho.chang@quality.firecloud.org",
+ *     "oliver": "oliver.wood@quality.firecloud.org",
+ *     "cedric": "cedric.diggory@quality.firecloud.org",
+ *     "crabbe": "vincent.crabbe@quality.firecloud.org",
+ *     "goyle": "gregory.goyle@quality.firecloud.org",
+ *     "dean": "dean.thomas@quality.firecloud.org",
+ *     "ginny": "ginny.weasley@quality.firecloud.org"
+ *   },
+ *   "temps": {
+ *     "luna": "luna.temp@quality.firecloud.org",
+ *     "neville": "neville.temp@quality.firecloud.org"
+ *   },
+ *   "notebookswhitelisted": {
+ *     "hermione": "hermione.owner@quality.firecloud.org",
+ *     "ron": "ron.weasley@quality.firecloud.org"
+ *   },
+ *   "campaignManagers": {
+ *     "dumbledore": "dumbledore.admin@quality.firecloud.org",
+ *     "voldemort": "voldemort.admin@quality.firecloud.org"
+ *   }
+ * }
+ */
+case object Owner extends UserType { def title = "owner" }
+case object Student extends UserType { def title = "student" }
+
+/**
+ * Companion object containing some useful methods for UserType.
+ */
+object UserType {
+  implicit val userTypeDecoder: Decoder[UserType] = Decoder.decodeString.emap {
+    case "owner"   => Right(Owner)
+    case "student" => Right(Student)
+    case other     => Left(s"Unknown user type: $other")
+  }
+
+  implicit val userTypeEncoder: Encoder[UserType] = Encoder.encodeString.contramap(_.title)
+}
