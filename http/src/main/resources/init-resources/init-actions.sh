d5baeb2ce153591e9ddf4a4ca59e02b048b8520f
LizBaldo <baldo@broadinstitute.org>
2023-11-02 16:41:20 -0400

    enable welder background sync always

--- http/src/main/resources/init-resources/init-actions.sh
+++ http/src/main/resources/init-resources/init-actions.sh
@@ -183,11 +183,14 @@ if [[ "${ROLE}" == 'Master' ]]; then
     export DOCKER_COMPOSE_FILES_DIRECTORY='/etc'
     PROXY_SITE_CONF=$(proxySiteConf)
     export HOST_PROXY_SITE_CONF_FILE_PATH=${DOCKER_COMPOSE_FILES_DIRECTORY}/`basename ${PROXY_SITE_CONF}`
-    if [ ! -z "$RSTUDIO_DOCKER_IMAGE" ] ; then
-      export SHOULD_BACKGROUND_SYNC="true"
-    else
-      export SHOULD_BACKGROUND_SYNC="false"
-    fi
+#    if [ ! -z "$RSTUDIO_DOCKER_IMAGE" ] ; then
+#      export SHOULD_BACKGROUND_SYNC="true"
+#    else
+#      export SHOULD_BACKGROUND_SYNC="false"
+#    fi
+
+    # We want to always enable background sync, and welder will omit `.ipynb` files
+    export SHOULD_BACKGROUND_SYNC="true"
 
     SERVER_CRT=$(proxyServerCrt)
     SERVER_KEY=$(proxyServerKey)
