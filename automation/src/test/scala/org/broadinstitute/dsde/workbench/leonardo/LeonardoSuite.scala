506953b5e30587e3eef7671366180d90381be352
jdcanas <jcanas@broadinstitute.com>
2023-11-14 13:56:21 -0800

    comment out tests for now

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoSuite.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/LeonardoSuite.scala
@@ -245,15 +245,15 @@ trait NewBillingProjectAndWorkspaceBeforeAndAfterAll extends BillingProjectUtils
 
 final class LeonardoSuite
     extends Suites(
-      new RuntimeCreationDiskSpec,
-      new RuntimeAutopauseSpec,
-      new RuntimePatchSpec,
+//      new RuntimeCreationDiskSpec,
+//      new RuntimeAutopauseSpec,
+//      new RuntimePatchSpec,
 //      new RuntimeSystemSpec,
-      new RuntimeStatusTransitionsSpec,
-      new NotebookGCECustomizationSpec,
-      new NotebookGCEDataSyncingSpec,
-      new RuntimeDataprocSpec,
-      new RuntimeGceSpec
+//      new RuntimeStatusTransitionsSpec,
+      new NotebookGCECustomizationSpec
+//      new NotebookGCEDataSyncingSpec,
+//      new RuntimeDataprocSpec,
+//      new RuntimeGceSpec
     )
     with TestSuite
     with NewBillingProjectAndWorkspaceBeforeAndAfterAll
@@ -261,10 +261,10 @@ final class LeonardoSuite
 
 final class LeonardoTerraDockerSuite
     extends Suites(
-      new NotebookHailSpec,
-      new NotebookPyKernelSpec,
-      new NotebookRKernelSpec,
-      new RStudioSpec
+//      new NotebookHailSpec,
+//      new NotebookPyKernelSpec,
+//      new NotebookRKernelSpec,
+//      new RStudioSpec
     )
     with TestSuite
     with NewBillingProjectAndWorkspaceBeforeAndAfterAll
