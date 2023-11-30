33e80d4b3cb4b89b4773fc60db8c3e36bdf32e7b
Eric Rollins <erollins@broadinstitute.org>
2023-11-30 10:59:32 -0500

    Added correct mixins.

--- pact4s/src/test/scala/org/broadinstitute/dede/workbench/leonardo/provider/LeoProvider.scala
+++ pact4s/src/test/scala/org/broadinstitute/dede/workbench/leonardo/provider/LeoProvider.scala
@@ -1,39 +1,48 @@
 package org.broadinstitute.dede.workbench.leonardo.provider
 
+import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import cats.effect.IO
-import cats.effect.unsafe.implicits._
-import com.typesafe.scalalogging.LazyLogging
+import cats.effect.unsafe.implicits.global
 import org.broadinstitute.dsde.workbench.leonardo.CommonTestData._
-import org.broadinstitute.dsde.workbench.leonardo._
+import org.broadinstitute.dsde.workbench.leonardo.LeonardoTestSuite
+import org.broadinstitute.dsde.workbench.leonardo.db.TestComponent
 import org.broadinstitute.dsde.workbench.leonardo.http.api.{HttpRoutes, TestLeoRoutes}
 import org.broadinstitute.dsde.workbench.leonardo.http.service._
+import org.broadinstitute.dsde.workbench.openTelemetry.{FakeOpenTelemetryMetricsInterpreter, OpenTelemetryMetrics}
 import org.scalatest.BeforeAndAfterAll
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
+import org.scalatestplus.mockito.MockitoSugar
+import org.typelevel.log4cats.StructuredLogger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 import pact4s.provider.{PactSource, ProviderInfoBuilder}
 import pact4s.scalatest.PactVerifier
 
 import java.io.File
 import java.lang.Thread.sleep
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
 
-class ScalaTestVerifyPacts
+
+class LeoProvider
   extends AnyFlatSpec
   with ScalatestRouteTest
-  with BeforeAndAfterAll
   with PactVerifier
-  with LazyLogging
-  with TestLeoRoutes
-  with LeonardoTestSuite {
+    with Matchers
+    with ScalaFutures
+    with LeonardoTestSuite
+    with TestComponent
+    with MockitoSugar
+  with TestLeoRoutes {
 
 
-  override def beforeAll(): Unit = {
-    startLeo.unsafeToFuture()
-    startLeo.start
-    sleep(5000)
-  }
-
+//override implicit val metrics: OpenTelemetryMetrics[IO] = FakeOpenTelemetryMetricsInterpreter
+//override implicit val loggerIO: StructuredLogger[IO] = Slf4jLogger.getLogger[IO]
+implicit val ec: ExecutionContextExecutor = ExecutionContext.global
+override implicit val system: ActorSystem = ActorSystem("leotests")
   val routes =
     new HttpRoutes(
       openIdConnectionConfiguration,
@@ -55,17 +64,17 @@ class ScalaTestVerifyPacts
     binding <- IO
       .fromFuture(IO(Http().newServerAt("localhost",8080).bind(routes.route)))
       .onError { t: Throwable =>
-        IO(logger.error("FATAL - failure starting http server", t)) *> IO.raiseError(t)
+        loggerIO.error(t.toString())
       }
     _ <- IO.fromFuture(IO(binding.whenTerminated))
     _ <- IO(system.terminate())
   } yield binding
 
   val provider: ProviderInfoBuilder = ProviderInfoBuilder(
-    name = "sam",
+    name = "y",
     pactSource = PactSource
       .FileSource(
-        Map("munit-consumer" -> new File("./example/resources/pacts/munit-consumer-munit-provider.json"))
+        Map("x" -> new File("./example/resources/pacts/x-y.json"))
       )).withHost("localhost")
     .withPort(8080)
 
