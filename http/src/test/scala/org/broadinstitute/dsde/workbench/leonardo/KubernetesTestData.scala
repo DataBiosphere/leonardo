12ea795fc68dc7d220a56290a6289def5dfb8c82
LizBaldo <LizBaldo@users.noreply.github.com>
2023-12-11 14:38:29 -0500

    [IA-4679] Move some AKS helm chart pull out of leo dockerfile (#3987)

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
@@ -57,7 +57,7 @@ object KubernetesTestData {
   val ingressChartVersion = ChartVersion("1.41.3")
   val ingressChart = Chart(ingressChartName, ingressChartVersion)
 
-  val coaChartName = ChartName("/leonardo/cromwell-on-azure")
+  val coaChartName = ChartName("cromwell-helm/cromwell-on-azure")
   val coaChartVersion = ChartVersion("0.2.397")
 
   val coaChart = Chart(coaChartName, coaChartVersion)
