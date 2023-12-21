123a74a51defed13437b193eb96a3f2197107dc8
Chip Pate <pate@broadinstitute.org>
2023-12-21 05:56:13 -0500

    fix(IA-4721): upgrade logback to latest version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
 
   val jose4j: ModuleID =  "org.bitbucket.b_c" % "jose4j" % "0.9.3"
 
-  val logbackClassic: ModuleID =  "ch.qos.logback"              % "logback-classic" % "1.4.11"
+  val logbackClassic: ModuleID =  "ch.qos.logback"              % "logback-classic" % "1.4.14"
   val scalaLogging: ModuleID =    "com.typesafe.scala-logging"  %% "scala-logging"  % scalaLoggingV
   val ficus: ModuleID =           "com.iheart"                  %% "ficus"          % "1.5.2"
   val enumeratum: ModuleID =      "com.beachape"                %% "enumeratum"     % "1.7.3"
