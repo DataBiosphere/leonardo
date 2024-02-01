a693a1645c8198a7daf3f0905b633bc8bb837826
Eric Rollins <erollins@broadinstitute.org>
2024-02-01 13:45:23 -0500

    Add provider tags back.

--- pact4s/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/provider/LeoProvider.scala
+++ pact4s/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/provider/LeoProvider.scala
@@ -105,6 +105,7 @@ class LeoProvider extends AnyFlatSpec with BeforeAndAfterAll with PactVerifier {
                           .PactBrokerWithSelectors(pactBrokerUrl)
                           .withAuth(BasicAuth(pactBrokerUser, pactBrokerPass))
                           .withPendingPacts(true)
+                          .withProviderTags(ProviderTags("develop"))
                           .withConsumerVersionSelectors(ConsumerVersionSelectors().matchingBranch)
     )
       .withStateManagementFunction(
