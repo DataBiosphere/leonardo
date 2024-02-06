79735ab1f17e3536bafa571e08ae1ee82a4e16ff
Eric Rollins <erollins@broadinstitute.org>
2024-02-06 16:31:06 -0500

    Temp commit

--- pact4s/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/provider/RuntimeStateManager.scala
+++ pact4s/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/provider/RuntimeStateManager.scala
@@ -3,26 +3,16 @@ package org.broadinstitute.dsde.workbench.leonardo.provider
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import cats.mtl.Ask
-import org.broadinstitute.dsde.workbench.google2.DiskName
-import org.broadinstitute.dsde.workbench.leonardo.CommonTestData._
 import org.broadinstitute.dsde.workbench.leonardo._
 import org.broadinstitute.dsde.workbench.leonardo.http.service._
-import org.broadinstitute.dsde.workbench.leonardo.http.{
-  CreateRuntimeRequest,
-  CreateRuntimeResponse,
-  DiskConfig,
-  GetRuntimeResponse
-}
+import org.broadinstitute.dsde.workbench.leonardo.http.{CreateRuntimeRequest, UpdateRuntimeRequest}
 import org.broadinstitute.dsde.workbench.leonardo.model.{RuntimeAlreadyExistsException, RuntimeNotFoundException}
-import org.broadinstitute.dsde.workbench.model.google.{GcsBucketName, GcsObjectName, GcsPath, GoogleProject}
-import org.broadinstitute.dsde.workbench.model.{TraceId, UserInfo}
+import org.broadinstitute.dsde.workbench.model.UserInfo
+import org.broadinstitute.dsde.workbench.model.google.GoogleProject
 import org.mockito.ArgumentMatchers.{any, anyString}
 import org.mockito.Mockito.when
-import org.scalacheck.Gen.uuid
 import pact4s.provider._
-
-import java.net.URL
-import java.time.Instant
+import org.mockito.ArgumentMatchers.{eq => eqTo}
 
 object RuntimeStateManager {
   object States {
@@ -30,7 +20,7 @@ object RuntimeStateManager {
     final val RuntimeDoesNotExist = "there is not a runtime in a Google project"
   }
 
-  private def mockRuntimeDoesNotExist(mockRuntimeService: RuntimeService[IO]): IO[Unit] = for {
+  private def mockGetNonexistentRuntime(mockRuntimeService: RuntimeService[IO]): IO[Unit] = for {
     _ <- IO(
       when {
         mockRuntimeService.getRuntime(any[UserInfo], any[CloudContext.Gcp], RuntimeName(anyString()))(
@@ -47,13 +37,36 @@ object RuntimeStateManager {
     )
   } yield ()
 
+  private def mockUpdateNonexistentRuntime(mockRuntimeService: RuntimeService[IO]): IO[Unit] = for {
+    _ <- IO(
+      when {
+        mockRuntimeService.updateRuntime(
+          any[UserInfo],
+          eqTo(GoogleProject("googleProject")),
+          eqTo(RuntimeName("runtimename")),
+          any[UpdateRuntimeRequest]
+        )(
+          any[Ask[IO, AppContext]]
+        )
+      } thenReturn {
+        IO.raiseError(
+          RuntimeNotFoundException(CloudContext.Gcp(GoogleProject("123")),
+                                   RuntimeName("nonexistentruntimename"),
+                                   "OOOPS"
+          )
+        )
+      }
+    )
+  } yield ()
+
   private def mockRuntimeConflict(mockRuntimeService: RuntimeService[IO]): IO[Unit] = for {
     _ <- IO(
       when {
-        mockRuntimeService.createRuntime(any[UserInfo],
-                                         any[CloudContext.Gcp],
-                                         RuntimeName(anyString()),
-                                         any[CreateRuntimeRequest]
+        mockRuntimeService.createRuntime(
+          any[UserInfo],
+          any[CloudContext.Gcp],
+          any[RuntimeName],
+          any[CreateRuntimeRequest]
         )(
           any[Ask[IO, AppContext]]
         )
@@ -69,53 +82,72 @@ object RuntimeStateManager {
   } yield ()
 
   def handler(mockRuntimeService: RuntimeService[IO]): PartialFunction[ProviderState, Unit] = {
-    case ProviderState(States.RuntimeExists, _) =>
-      val date = Instant.parse("2020-11-20T17:23:24.650Z")
-      when(
-        mockRuntimeService.getRuntime(any[UserInfo], any[CloudContext.Gcp], RuntimeName(anyString()))(
-          any[Ask[IO, AppContext]]
-        )
-      )
-        .thenReturn(IO {
-          GetRuntimeResponse(
-            -1,
-            runtimeSamResource,
-            name1,
-            cloudContextGcp,
-            serviceAccountEmail,
-            Some(makeAsyncRuntimeFields(1).copy(proxyHostName = ProxyHostName(uuid.toString))),
-            auditInfo.copy(createdDate = date, dateAccessed = date),
-            Some(date),
-            defaultGceRuntimeConfig,
-            new URL("https://leo.org/proxy"),
-            RuntimeStatus.Running,
-            Map("foo" -> "bar"),
-            Some(UserScriptPath.Gcs(GcsPath(GcsBucketName("bucket-name"), GcsObjectName("userScript")))),
-            Some(UserScriptPath.Gcs(GcsPath(GcsBucketName("bucket-name"), GcsObjectName("startScript")))),
-            List.empty[RuntimeError],
-            None,
-            30,
-            Some("clientId"),
-            Set(jupyterImage, welderImage, proxyImage, cryptoDetectorImage).map(_.copy(timestamp = date)),
-            defaultScopes,
-            welderEnabled = true,
-            patchInProgress = true,
-            Map("ev1" -> "a", "ev2" -> "b"),
-            Some(DiskConfig(DiskName("disk"), DiskSize(100), DiskType.Standard, BlockSize(1024)))
-          )
-        })
-      mockRuntimeConflict(mockRuntimeService).unsafeRunSync()
+//    case ProviderState(States.RuntimeExists, _) =>
+//      val date = Instant.parse("2020-11-20T17:23:24.650Z")
+//      when(
+//        mockRuntimeService.getRuntime(any[UserInfo], any[CloudContext.Gcp], RuntimeName(anyString()))(
+//          any[Ask[IO, AppContext]]
+//        )
+//      )
+//        .thenReturn(IO {
+//          GetRuntimeResponse(
+//            -1,
+//            runtimeSamResource,
+//            name1,
+//            cloudContextGcp,
+//            serviceAccountEmail,
+//            Some(makeAsyncRuntimeFields(1).copy(proxyHostName = ProxyHostName(uuid.toString))),
+//            auditInfo.copy(createdDate = date, dateAccessed = date),
+//            Some(date),
+//            defaultGceRuntimeConfig,
+//            new URL("https://leo.org/proxy"),
+//            RuntimeStatus.Running,
+//            Map("foo" -> "bar"),
+//            Some(UserScriptPath.Gcs(GcsPath(GcsBucketName("bucket-name"), GcsObjectName("userScript")))),
+//            Some(UserScriptPath.Gcs(GcsPath(GcsBucketName("bucket-name"), GcsObjectName("startScript")))),
+//            List.empty[RuntimeError],
+//            None,
+//            30,
+//            Some("clientId"),
+//            Set(jupyterImage, welderImage, proxyImage, cryptoDetectorImage).map(_.copy(timestamp = date)),
+//            defaultScopes,
+//            welderEnabled = true,
+//            patchInProgress = true,
+//            Map("ev1" -> "a", "ev2" -> "b"),
+//            Some(DiskConfig(DiskName("disk"), DiskSize(100), DiskType.Standard, BlockSize(1024)))
+//          )
+//        })
+//      mockRuntimeConflict(mockRuntimeService).unsafeRunSync()
+////      when(
+////        mockRuntimeService.updateRuntime(
+////          UserInfo(OAuth2BearerToken(anyString()),
+////                   WorkbenchUserId(anyString()),
+////                   WorkbenchEmail(anyString()),
+////                   anyLong()
+////          ),
+////          GoogleProject(anyString()),
+////          RuntimeName(anyString()),
+////          UpdateRuntimeRequest(any[Option[UpdateRuntimeConfigRequest]],
+////                               anyBoolean(),
+////                               any[Option[Boolean]],
+////                               any[Option[FiniteDuration]],
+////                               any[LabelMap],
+////                               any[Set[String]]
+////          )
+////        )(any[Ask[IO, AppContext]])
+////      ).thenReturn(IO.unit)
     case ProviderState(States.RuntimeDoesNotExist, _) =>
-      when(
-        mockRuntimeService.createRuntime(any[UserInfo],
-                                         any[CloudContext.Gcp],
-                                         RuntimeName(anyString()),
-                                         any[CreateRuntimeRequest]
-        )(
-          any[Ask[IO, AppContext]]
-        )
-      ).thenReturn(IO(CreateRuntimeResponse(TraceId("test"))))
-      mockRuntimeDoesNotExist(mockRuntimeService).unsafeRunSync()
+//      when(
+//        mockRuntimeService.createRuntime(any[UserInfo],
+//                                         any[CloudContext.Gcp],
+//                                         RuntimeName(anyString()),
+//                                         any[CreateRuntimeRequest]
+//        )(
+//          any[Ask[IO, AppContext]]
+//        )
+//      ).thenReturn(IO(CreateRuntimeResponse(TraceId("test"))))
+//      mockGetNonexistentRuntime(mockRuntimeService).unsafeRunSync()
+      mockUpdateNonexistentRuntime(mockRuntimeService).unsafeRunSync()
 
   }
 }
