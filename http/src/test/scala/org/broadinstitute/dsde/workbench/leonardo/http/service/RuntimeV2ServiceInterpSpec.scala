229c5fa045c2133b7beb5fb1d4c8c6054e7a46a5
erdalcosgun <31049564+erdalcosgun@users.noreply.github.com>
2024-02-07 12:27:49 -0800

    Update RuntimeV2ServiceInterpSpec.scala

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeV2ServiceInterpSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeV2ServiceInterpSpec.scala
@@ -349,7 +349,7 @@ class RuntimeV2ServiceInterpSpec extends AnyFlatSpec with LeonardoTestSuite with
       val expectedRuntimeImage = Set(
         RuntimeImage(
           RuntimeImageType.Azure,
-          "microsoft-dsvm, ubuntu-2004, 2004-gen2, 23.12.11",
+          "microsoft-dsvm, ubuntu-2004, 2004-gen2, 23.12.18",
           None,
           context.now
         ),
