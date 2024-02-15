251746fc696814aae2ce292725af988a8522f6ec
aherbst-broad <aherbst@broadinstitute.org>
2024-02-15 09:40:32 -0500

    Bump relay image

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -68,7 +68,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
                 "https://raw.githubusercontent.com/DataBiosphere/leonardo/52aab3b7f252667f73b23682062ab3e0d9d533b9/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
-            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:7aa15b4",
+            "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:1f48b97",
             VMCredential(username = "username", password = "password")
           )
         ),
