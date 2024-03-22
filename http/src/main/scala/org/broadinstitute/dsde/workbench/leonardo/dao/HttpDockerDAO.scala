3ac6266f5d0d71ba92aa75e343cdf85359238872
Denis Loginov <denis@broadinstitute.org>
2024-03-22 19:01:03 -0400

    Remove GAR case from parseImage

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/HttpDockerDAO.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/dao/HttpDockerDAO.scala
@@ -160,16 +160,6 @@ class HttpDockerDAO[F[_]] private (httpClient: Client[F])(implicit logger: Logge
             F.pure(ParsedImage(GCR, Uri.unsafeFromString(s"https://$registry/v2"), imageName, i))
           )
         } yield res
-      case GAR.regex(registry, imageName, tagOpt, shaOpt) =>
-        val version = Option(tagOpt)
-          .map(Tag)
-          .orElse(Option(shaOpt).map(Sha))
-        for {
-          traceId <- ev.ask
-          res <- version.fold(F.raiseError[ParsedImage](ImageParseException(traceId, image)))(i =>
-            F.pure(ParsedImage(GAR, Uri.unsafeFromString(s"https://$registry/v2"), imageName, i))
-          )
-        } yield res
       case DockerHub.regex(imageName, tagOpt, shaOpt) =>
         val identifier = Option(tagOpt)
           .map(Tag)
