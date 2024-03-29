8d171ffc7adb4e4d483f2c476b8245b04d577e6c
broadbot <broadbot@broadinstitute.org>
2024-03-29 13:40:59 +0000

    Auto-update cromwell-runner-app version to 0.110.0

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -147,7 +147,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
         ),
         CromwellRunnerAppConfig(
           ChartName("terra-helm/cromwell-runner-app"),
-          ChartVersion("0.109.0"),
+          ChartVersion("0.110.0"),
           ReleaseNameSuffix("cra-rls"),
           NamespaceNameSuffix("cra-ns"),
           KsaName("cra-ksa"),
