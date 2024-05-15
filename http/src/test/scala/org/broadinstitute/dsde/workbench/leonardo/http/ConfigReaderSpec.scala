1c326595731cd0bb1a3c1621534752f73356f552
Janet Gainer-Dewar <jdewar@broadinstitute.org>
2024-05-15 16:29:01 -0400

    Revert "WX-1615: Update Cromwell version to 0.2.490" (#4556)

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -86,7 +86,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
         AzureAppRegistrationConfig(ClientId(""), ClientSecret(""), ManagedAppTenantId("")),
         CoaAppConfig(
           ChartName("cromwell-helm/cromwell-on-azure"),
-          ChartVersion("0.2.490"),
+          ChartVersion("0.2.471"),
           ReleaseNameSuffix("coa-rls"),
           NamespaceNameSuffix("coa-ns"),
           KsaName("coa-ksa"),
