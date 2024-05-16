ec6d34dda156a0d043105a188f9ec5814b51b159
Mneme Nolting <mneme@broadinstitute.org>
2024-05-16 17:41:51 -0400

    [IA-4938] upgrade workbench-libs to get the new bouncycastle 1.78

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val munitCatsEffectV = "1.0.7"
   val pact4sV = "0.10.0"
 
-  private val workbenchLibsHash = "5762674"
+  private val workbenchLibsHash = "3b2d0f4"
   val serviceTestV = s"4.3-$workbenchLibsHash"
   val workbenchModelV = s"0.19-$workbenchLibsHash"
   val workbenchGoogleV = s"0.30-$workbenchLibsHash"
