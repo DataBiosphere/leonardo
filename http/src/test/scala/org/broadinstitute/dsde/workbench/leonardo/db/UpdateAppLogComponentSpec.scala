5d21d9fc0149f188f53003e1083a0f9777e58620
jdcanas <jcanas@broadinstitute.com>
2024-06-13 10:32:43 -0700

    fix db tables

---
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/db/UpdateAppLogComponentSpec.scala
@@ -0,0 +1,44 @@
+package org.broadinstitute.dsde.workbench.leonardo
+package http
+package db
+
+import cats.effect.IO
+import cats.implicits._
+import org.broadinstitute.dsde.workbench.leonardo.KubernetesTestData.{makeApp, makeKubeCluster, makeNodepool}
+import org.broadinstitute.dsde.workbench.leonardo.UpdateAppJobId
+import org.broadinstitute.dsde.workbench.leonardo.db.LeoProfile.dummyDate
+import org.scalatest.flatspec.AnyFlatSpecLike
+import org.broadinstitute.dsde.workbench.leonardo.db.LeoProfile.api._
+import org.broadinstitute.dsde.workbench.leonardo.db.LeoProfile.mappedColumnImplicits._
+import org.broadinstitute.dsde.workbench.leonardo.db.{updateAppLogQuery, TestComponent}
+
+import java.time.Instant
+import java.util.UUID
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class UpdateAppLogComponentSpec extends AnyFlatSpecLike with TestComponent {
+  it should "save, get" in isolatedDbTest {
+    val savedCluster1 = makeKubeCluster(1).save()
+    val savedNodepool1 = makeNodepool(1, savedCluster1.id).save()
+
+    val app = makeApp(1, savedNodepool1.id)
+    val savedApp = app.save()
+
+    val jobId1 = UpdateAppJobId(UUID.randomUUID())
+    val startTime = Instant.now()
+
+    val test = for {
+      _ <- updateAppLogQuery.save(jobId1, savedApp.id, startTime)
+      appUpdateLogRecordOpt <- updateAppLogQuery.get(savedApp.id, jobId1)
+    } yield {
+      appUpdateLogRecordOpt.isDefined shouldBe true
+      appUpdateLogRecordOpt.map(_.errorId) shouldBe Some(None)
+      appUpdateLogRecordOpt.map(_.endTime) shouldBe Some(None)
+      appUpdateLogRecordOpt.map(_.jobId) shouldBe Some(jobId1)
+      appUpdateLogRecordOpt.map(_.appId) shouldBe Some(savedApp.id)
+      appUpdateLogRecordOpt.map(_.status) shouldBe Some(UpdateAppJobStatus.Running)
+      appUpdateLogRecordOpt.map(_.startTime) shouldBe Some(startTime)
+    }
+    test.transaction.unsafeRunSync()(cats.effect.unsafe.IORuntime.global)
+  }
+}
