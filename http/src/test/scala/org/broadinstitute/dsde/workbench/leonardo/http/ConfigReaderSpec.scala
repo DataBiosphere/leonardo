4da66b754c2d50e9b2e21bba37b70c76b5f23342
Justin Canas <justincanas16@gmail.com>
2024-06-18 18:36:25 -0700

    [Prod-968] Unpin r version but install latest (#4652)
    Co-authored-by: jdcanas <jcanas@broadinstitute.com>
    Co-authored-by: Chip Pate <cpate4@users.noreply.github.com>

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -73,7 +73,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
               "2.1",
               true,
               List(
-                "https://raw.githubusercontent.com/DataBiosphere/leonardo/f54556501b9ec88c970b140887df850379e47b39/http/src/main/resources/init-resources/azure_vm_init_script.sh"
+                "https://raw.githubusercontent.com/DataBiosphere/leonardo/8390d25ccd761fb206cf388560a571be77a42bbd/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
             "terradevacrpublic.azurecr.io/terra-azure-relay-listeners:76d982c",
