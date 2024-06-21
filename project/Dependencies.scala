d22efa9d0182b47ac3432ae304de0d5737bf6d9d
dvoet <dvoet@users.noreply.github.com>
2024-06-21 13:01:34 -0400

    ID-1307 swagger oauth updates (#4639)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,10 +17,10 @@ object Dependencies {
   val munitCatsEffectV = "1.0.7"
   val pact4sV = "0.10.0"
 
-  private val workbenchLibsHash = "3b2d0f4"
-  val serviceTestV = s"4.3-$workbenchLibsHash"
+  private val workbenchLibsHash = "a6ad7dc"
+  val serviceTestV = s"4.4-$workbenchLibsHash"
   val workbenchModelV = s"0.19-$workbenchLibsHash"
-  val workbenchGoogleV = s"0.30-$workbenchLibsHash"
+  val workbenchGoogleV = s"0.32-$workbenchLibsHash"
   val workbenchGoogle2V = s"0.36-$workbenchLibsHash"
   val workbenchOpenTelemetryV = s"0.8-$workbenchLibsHash"
   val workbenchOauth2V = s"0.7-$workbenchLibsHash"
