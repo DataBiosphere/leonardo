609ba73cf335cfd341512f323b05a8433b9377bb
aherbst-broad <aherbst@broadinstitute.org>
2024-06-21 12:38:45 -0400

    Testing

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/AzurePubsubHandler.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/AzurePubsubHandler.scala
@@ -1183,7 +1183,9 @@ class AzurePubsubHandlerInterp[F[_]: Parallel](
       ctx <- ev.ask
       params = CreateAKSAppParams(appId, appName, workspaceId, cloudContext, billingProfileId)
       _ <- aksAlgebra.createAndPollApp(params).adaptError { case e =>
-        logger.error("*** error creating app", e)
+        logger.error(ctx.loggingCtx, e)(
+          s"*** error creating app"
+        )
         PubsubKubernetesError(
           AppError(
             s"Error creating Azure app with id ${appId.id} and cloudContext ${cloudContext.asString}: ${e.getMessage}",
