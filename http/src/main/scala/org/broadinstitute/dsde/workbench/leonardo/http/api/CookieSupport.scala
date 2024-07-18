793a76f569d5e8cdbbe0a0c1d568569e9afdd41e
Mneme Nolting <mneme@broadinstitute.org>
2024-07-18 16:06:29 -0400

    [IA-4997] set top and partitioned cookies

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/api/CookieSupport.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/api/CookieSupport.scala
@@ -11,46 +11,61 @@ import org.broadinstitute.dsde.workbench.model.UserInfo
 
 object CookieSupport {
   val tokenCookieName = "LeoToken"
-  val proxyTokenCookieName = "LeoProxyToken"
+  val partitionedTokenCookieName = "TerraToken"
+  val partitionedProxyTokenCookieName = "LeoProxyToken"
   val proxyUrl = new URL(Config.proxyConfig.proxyUrlBase)
 
   /**
    * Sets a token cookie in the HTTP response.
    */
   def setTokenCookie(userInfo: UserInfo): Directive0 =
-    respondWithHeaders(buildRawCookie(userInfo), buildRawProxyCookie(userInfo))
+    respondWithHeaders(buildRawCookie(userInfo), buildRawTerraCookie(userInfo), buildRawProxyCookie(userInfo))
 
   /**
    * Unsets a token cookie in the HTTP response.
    */
   def unsetTokenCookie(): Directive0 =
-    respondWithHeaders(buildRawUnsetCookie(), buildRawUnsetProxyCookie())
+    respondWithHeaders(buildRawUnsetCookie(), buildRawUnsetTerraCookie(), buildRawUnsetProxyCookie())
 
   private def buildRawCookie(userInfo: UserInfo) =
     RawHeader(
       name = "Set-Cookie",
       value =
-        s"$tokenCookieName=${userInfo.accessToken.token}; Max-Age=${userInfo.tokenExpiresIn.toString}; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
+        s"$tokenCookieName=${userInfo.accessToken.token}; Max-Age=${userInfo.tokenExpiresIn.toString}; Path=/; Secure; SameSite=None; HttpOnly"
     )
 
   private def buildRawUnsetCookie(): RawHeader =
     RawHeader(
       name = "Set-Cookie",
       value =
-        s"$tokenCookieName=unset; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
+        s"$tokenCookieName=unset; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; SameSite=None; HttpOnly"
+    )
+
+  private def buildRawTerraCookie(userInfo: UserInfo) =
+    RawHeader(
+      name = "Set-Cookie",
+      value =
+        s"$partitionedTokenCookieName=${userInfo.accessToken.token}; Max-Age=${userInfo.tokenExpiresIn.toString}; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
+    )
+
+  private def buildRawUnsetTerraCookie(): RawHeader =
+    RawHeader(
+      name = "Set-Cookie",
+      value =
+        s"$partitionedTokenCookieName=unset; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
     )
 
   private def buildRawProxyCookie(userInfo: UserInfo) =
     RawHeader(
       name = "Set-Cookie",
       value =
-        s"$proxyTokenCookieName=${userInfo.accessToken.token}; Domain=${proxyUrl.getHost}; Max-Age=${userInfo.tokenExpiresIn.toString}; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
+        s"$partitionedProxyTokenCookieName=${userInfo.accessToken.token}; Domain=${proxyUrl.getHost}; Max-Age=${userInfo.tokenExpiresIn.toString}; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
     )
 
   private def buildRawUnsetProxyCookie(): RawHeader =
     RawHeader(
       name = "Set-Cookie",
       value =
-        s"$proxyTokenCookieName=unset; Domain=${proxyUrl.getHost}; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
+        s"$partitionedProxyTokenCookieName=unset; Domain=${proxyUrl.getHost}; Expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; Secure; SameSite=None; HttpOnly; Partitioned"
     )
 }
