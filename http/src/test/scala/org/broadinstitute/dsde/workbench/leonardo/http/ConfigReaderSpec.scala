78e723d4cc2d2dc54bc6d2f5e23af68438e0abe0
broadbot <broadbot@broadinstitute.org>
2024-08-13 16:26:39 +0000

    Auto-update wds version to 0.93.0

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -191,7 +191,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
         ),
         WdsAppConfig(
           ChartName("terra-helm/wds"),
-          ChartVersion("0.92.0"),
+          ChartVersion("0.93.0"),
           ReleaseNameSuffix("wds-rls"),
           NamespaceNameSuffix("wds-ns"),
           KsaName("wds-ksa"),
