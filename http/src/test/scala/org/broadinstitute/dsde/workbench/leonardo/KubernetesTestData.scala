608950bfae28cf8290ebc68d01ed5639e431654f
broadbot <broadbot@broadinstitute.org>
2024-08-15 15:35:17 +0000

    Auto-update Cromwell version to 0.2.531

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
@@ -58,7 +58,7 @@ object KubernetesTestData {
   val ingressChart = Chart(ingressChartName, ingressChartVersion)
 
   val coaChartName = ChartName("cromwell-helm/cromwell-on-azure")
-  val coaChartVersion = ChartVersion("0.2.523")
+  val coaChartVersion = ChartVersion("0.2.531")
 
   val coaChart = Chart(coaChartName, coaChartVersion)
 
