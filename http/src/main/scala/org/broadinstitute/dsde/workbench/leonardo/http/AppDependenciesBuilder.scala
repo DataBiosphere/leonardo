8f51ac78789de8449c85a2b9e177d09beb5381c1
Jonathon Saunders <jsaun@users.noreply.github.com>
2024-08-24 22:41:36 -0700

    Fix

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/AppDependenciesBuilder.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/AppDependenciesBuilder.scala
@@ -196,7 +196,8 @@ class AppDependenciesBuilder(baselineDependenciesBuilder: BaselineDependenciesBu
       baselineDependencies.cromwellDAO,
       baselineDependencies.cbasDAO,
       baselineDependencies.azureBatchService,
-      baselineDependencies.azureApplicationInsightsService
+      baselineDependencies.azureApplicationInsightsService,
+      baselineDependencies.authProvider
     )
 
     val cromwellRunnerAppInstall =
@@ -218,7 +219,8 @@ class AppDependenciesBuilder(baselineDependenciesBuilder: BaselineDependenciesBu
       ConfigReader.appConfig.azure.tdr,
       baselineDependencies.samDAO,
       baselineDependencies.wdsDAO,
-      baselineDependencies.azureApplicationInsightsService
+      baselineDependencies.azureApplicationInsightsService,
+      baselineDependencies.authProvider
     )
     val workflowsAppInstall =
       new WorkflowsAppInstall[IO](
