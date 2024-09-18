a931fecfb325945c7d122e0d896e83784f883677
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2024-09-18 17:48:38 -0400

    cant default to 0

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# The CloudSQL console simply states "MySQL 5.6" so we may not match the minor version number
+# The CloudSQL console simply states "MySQL 5.7" so we may not match the minor version number
 MYSQL_VERSION=5.7
 start() {
 
