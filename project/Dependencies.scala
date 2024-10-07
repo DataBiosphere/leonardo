db62e034748cf82e3fc92690be2eaa62ac3546ef
Jonathon Saunders <jsaun@users.noreply.github.com>
2024-10-07 10:48:45 -0700

    Update Dependencies.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
   val workbenchGoogle2V = s"0.36-$workbenchLibsHash"
   val workbenchOpenTelemetryV = s"0.8-$workbenchLibsHash"
   val workbenchOauth2V = s"0.7-$workbenchLibsHash"
-  val workbenchAzureV = s"0.9-9b3a6467-SNAP"
+  val workbenchAzureV = s"0.9-d44b0ed0-SNAP"
 
   val helmScalaSdkV = "0.0.8.5"
 
