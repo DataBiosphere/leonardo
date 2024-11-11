87b3434904002fefc434c9d2587d4a5f746c8227
LizBaldo <baldo@broadinstitute.org>
2024-11-11 10:52:04 -0500

    do not stop the rstudio container otherwise it gets deleted by the docker compose down

--- http/src/main/resources/init-resources/shutdown.sh
+++ http/src/main/resources/init-resources/shutdown.sh
@@ -22,10 +22,6 @@ export RSTUDIO_DOCKER_IMAGE=$(rstudioDockerImage)
 export RSTUDIO_SERVER_NAME=$(rstudioServerName)
 export SHOULD_DELETE_JUPYTER_DIR=$(shouldDeleteJupyterDir)
 
-# If RStudio is installed, cleanly shut it down
-if [ ! -z "$RSTUDIO_DOCKER_IMAGE" ] ; then
-    docker exec -u rstudio -i $RSTUDIO_SERVER_NAME rstudio-server stop
-fi
 
 # Remove jupyter related files if user decides to delete the VM
 if [ -d '/mnt/disks/work/.jupyter' ] && [ "SHOULD_DELETE_JUPYTER_DIR" = "true" ] ; then
