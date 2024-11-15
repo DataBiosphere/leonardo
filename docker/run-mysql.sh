5fa7148c1750157c8226f3afd6e2e1efcdc65ca2
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2024-11-15 17:29:40 -0500

    Run unit tests with upgraded mysql

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # The CloudSQL console simply states "MySQL 5.6" so we may not match the minor version number
-MYSQL_VERSION=5.6
+MYSQL_VERSION=5.7
 start() {
 
     echo "attempting to remove old $CONTAINER container..."
