fc5466c7a2b5c2671e69b88d453922adb5b38df1
bennettn4 <nibennett@microsoft.com>
2024-12-02 10:32:57 -0500

    Update AzurePubsubHandler.scala

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/AzurePubsubHandler.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/AzurePubsubHandler.scala
@@ -229,7 +229,7 @@ class AzurePubsubHandlerInterp[F[_]: Parallel](
       applicationConfig.leoUrlBase,
       params.runtime.runtimeName.asString,
       s"'${refererConfig.validHosts.mkString("','")}'",
-      ${AzureEnvironmentConverter.relaySuffixFromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)}
+      AzureEnvironmentConverter.relaySuffixFromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)
     )
 
     val cmdToExecute = s"touch /var/log/azure_vm_init_script.log && chmod 400 /var/log/azure_vm_init_script.log &&" +
