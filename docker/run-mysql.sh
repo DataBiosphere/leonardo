109a9a7428887cc75bb7b61d4d83a4696c11860b
LizBaldo <LizBaldo@users.noreply.github.com>
2024-12-19 10:29:10 -0500

    [AN-257] upgrade mysql from 5.7 to 8.0 (#4816)

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# The CloudSQL console simply states "MySQL 5.7" so we may not match the minor version number
-MYSQL_VERSION=5.7
+# The CloudSQL console simply states "MySQL 8.0" so we may not match the minor version number
+MYSQL_VERSION=8.0.40
 start() {
 
     echo "attempting to remove old $CONTAINER container..."
