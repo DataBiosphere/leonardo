51f0fce03020471d87e4df610dd5b4739ce88bbc
LizBaldo <baldo@broadinstitute.org>
2024-12-19 10:58:02 -0500

    pin mysql to 8.4 for local leo and unit tests

--- docker/run-mysql.sh
+++ docker/run-mysql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# The CloudSQL console simply states "MySQL 8.0" so we may not match the minor version number
-MYSQL_VERSION=8.0.40
+# The CloudSQL console simply states "MySQL 8.4" so we may not match the minor version number
+MYSQL_VERSION=8.4
 start() {
 
     echo "attempting to remove old $CONTAINER container..."
