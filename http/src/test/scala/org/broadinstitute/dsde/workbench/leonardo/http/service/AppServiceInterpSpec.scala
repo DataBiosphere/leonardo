59e0cc6790a500de307ccd4980642f9352afd583
bennettn4 <nibennett@microsoft.com>
2024-12-26 15:20:38 -0500

    Update AppServiceInterpSpec.scala

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/AppServiceInterpSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/AppServiceInterpSpec.scala
@@ -3354,16 +3354,16 @@ class AppServiceInterpTest extends AnyFlatSpec with AppServiceInterpSpec with Le
     val map1 = LeoAppServiceInterp.getAppSamPolicyMap(userEmail, leoEmail, None)
     map1 should have size 1
     map1 should contain key "creator"
-    map1("creator") shouldBe SamPolicyData(List(userEmail), List(AppRole.Creator.asString))
+    map1("creator") shouldBe SamPolicyData(List(userEmail, leoEmail), List(AppRole.Creator.asString))
 
     val map2 = LeoAppServiceInterp.getAppSamPolicyMap(userEmail, leoEmail, Some(AppAccessScope.UserPrivate))
     map2 should have size 1
     map2 should contain key "creator"
-    map2("creator") shouldBe SamPolicyData(List(userEmail), List(AppRole.Creator.asString))
+    map2("creator") shouldBe SamPolicyData(List(userEmail, leoEmail), List(AppRole.Creator.asString))
 
     val map3 = LeoAppServiceInterp.getAppSamPolicyMap(userEmail, leoEmail, Some(AppAccessScope.WorkspaceShared))
     map3 should have size 1
     map3 should contain key "owner"
-    map3("owner") shouldBe SamPolicyData(List(userEmail), List(SharedAppRole.Owner.asString))
+    map3("owner") shouldBe SamPolicyData(List(userEmail, leoEmail), List(SharedAppRole.Owner.asString))
   }
 }
