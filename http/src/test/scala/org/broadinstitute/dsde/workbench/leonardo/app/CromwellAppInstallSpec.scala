ae716da3751e5ba084e4bcfd2e4c04f02f17674a
bennettn4 <nibennett@microsoft.com>
2024-12-26 15:40:12 -0500

    update helm override specs

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/app/CromwellAppInstallSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/app/CromwellAppInstallSpec.scala
@@ -51,9 +51,18 @@ class CromwellAppInstallSpec extends BaseAppInstallSpec {
       "config.subscriptionId=sub," +
       s"config.region=${azureRegion}," +
       "config.applicationInsightsConnectionString=applicationInsightsConnectionString," +
+      s"config.azureEnvironment=${ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment}" +
+      s"config.azureManagementTokenScope=${AzureEnvironmentConverter
+      .fromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)
+      .getResourceManagerEndpoint}.default" +
+      s"config.batchAccountSuffix=${AzureEnvironmentConverter
+      .batchAccountSuffixFromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)}" +
       "relay.path=https://relay.com/app," +
       "persistence.storageResourceGroup=mrg," +
       "persistence.storageAccount=storage," +
+      s"persistence.storageAccountSuffix=${AzureEnvironmentConverter
+        .fromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)
+        .getStorageEndpointSuffix}" +
       "persistence.blobContainer=sc-container," +
       "persistence.leoAppInstanceName=app1," +
       s"persistence.workspaceManager.url=${ConfigReader.appConfig.azure.wsm.uri.renderString}," +
@@ -108,9 +117,18 @@ class CromwellAppInstallSpec extends BaseAppInstallSpec {
       "config.subscriptionId=sub," +
       s"config.region=${azureRegion}," +
       "config.applicationInsightsConnectionString=applicationInsightsConnectionString," +
+      s"config.azureEnvironment=${ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment}" +
+      s"config.azureManagementTokenScope=${AzureEnvironmentConverter
+        .fromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)
+        .getResourceManagerEndpoint}.default" +
+      s"config.batchAccountSuffix=${AzureEnvironmentConverter
+        .batchAccountSuffixFromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)}" +
       "relay.path=https://relay.com/app," +
       "persistence.storageResourceGroup=mrg," +
       "persistence.storageAccount=storage," +
+      s"persistence.storageAccountSuffix=${AzureEnvironmentConverter
+        .fromString(ConfigReader.appConfig.azure.hostingModeConfig.azureEnvironment)
+        .getStorageEndpointSuffix}" +
       "persistence.blobContainer=sc-container," +
       "persistence.leoAppInstanceName=app1," +
       s"persistence.workspaceManager.url=${ConfigReader.appConfig.azure.wsm.uri.renderString}," +
