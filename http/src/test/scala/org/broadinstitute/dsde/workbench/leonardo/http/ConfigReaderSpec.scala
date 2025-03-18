9b9cc3f51c890c29deb3369986e17f41950ff574
Jonathon Saunders <jsaun@users.noreply.github.com>
2025-03-18 08:54:15 -0700

    Update ConfigReaderSpec.scala

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/ConfigReaderSpec.scala
@@ -74,7 +74,7 @@ class ConfigReaderSpec extends AnyFlatSpec with Matchers {
               "2.1",
               true,
               List(
-                "https://raw.githubusercontent.com/DataBiosphere/leonardo/f58c237b4dc235cd1c24c6dfc7500c07bdbd5bc3/http/src/main/resources/init-resources/azure_vm_init_script.sh"
+                "https://raw.githubusercontent.com/DataBiosphere/leonardo/4ae6ec54e73d3fb20e8c3a142488bd09db814160/http/src/main/resources/init-resources/azure_vm_init_script.sh"
               )
             ),
             // [IA-4997] to support CHIPS by setting partitioned cookies
