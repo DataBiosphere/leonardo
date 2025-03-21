d6d1b08093d1c36c7fd155f4b09b99bfce8d8740
LizBaldo <baldo@broadinstitute.org>
2025-03-21 19:33:01 -0400

    making sure that things are still working fine

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,8 +17,8 @@ object Dependencies {
   val munitCatsEffectV = "1.0.7"
   val pact4sV = "0.10.0"
 
-  private val workbenchLibsHash = "9d7d0680-SNAP"
-  val serviceTestV = s"5.1-$workbenchLibsHash"
+  private val workbenchLibsHash = "c153d812-SNAP"
+  val serviceTestV = s"6.1-$workbenchLibsHash"
   val workbenchModelV = s"0.21-$workbenchLibsHash"
   val workbenchGoogleV = s"0.35-$workbenchLibsHash"
   val workbenchGoogle2V = s"0.40-$workbenchLibsHash"
