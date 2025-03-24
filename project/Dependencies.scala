32527e4d26ea079fc48699a6aac21e97f0dda9fa
LizBaldo <baldo@broadinstitute.org>
2025-03-24 10:04:12 -0400

    use official workbenchlib hash

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val munitCatsEffectV = "1.0.7"
   val pact4sV = "0.10.0"
 
-  private val workbenchLibsHash = "c153d812-SNAP"
+  private val workbenchLibsHash = "ad2b686"
   val serviceTestV = s"6.1-$workbenchLibsHash"
   val workbenchModelV = s"0.21-$workbenchLibsHash"
   val workbenchGoogleV = s"0.35-$workbenchLibsHash"
