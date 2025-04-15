8e024bcc07f15f66f3c1153ba6551989c60741cb
Saman Ehsan <sehsan@broadinstitute.org>
2025-04-15 09:25:59 -0400

    Fix excluded runtime statuses
    Exclude deleted runtimes by default,
    and exclude nothing when listing out
    runtime records to delete.

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeServiceInterp.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/RuntimeServiceInterp.scala
@@ -407,7 +407,7 @@ class RuntimeServiceInterp[F[_]: Parallel](
     as: Ask[F, AppContext]
   ): F[Unit] =
     for {
-      runtimes <- listRuntimes(userInfo, Some(cloudContext), Map.empty, List(RuntimeStatus.Deleted))
+      runtimes <- listRuntimes(userInfo, Some(cloudContext), Map.empty, List.empty)
       _ <- runtimes.traverse(runtime => deleteRuntimeRecords(userInfo, cloudContext, runtime))
     } yield ()
 
--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/MockRuntimeServiceInterp.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/http/service/MockRuntimeServiceInterp.scala
@@ -28,7 +28,7 @@ class BaseMockRuntimeServiceInterp extends RuntimeService[IO] {
   override def listRuntimes(userInfo: UserInfo, cloudContext: Option[CloudContext], params: Map[String, String])(
     implicit as: Ask[IO, AppContext]
   ): IO[Vector[ListRuntimeResponse2]] =
-    listRuntimes(userInfo, cloudContext, params, List.empty)
+    listRuntimes(userInfo, cloudContext, params, List(RuntimeStatus.Deleted))
 
   override def listRuntimes(userInfo: UserInfo,
                             cloudContext: Option[CloudContext],
