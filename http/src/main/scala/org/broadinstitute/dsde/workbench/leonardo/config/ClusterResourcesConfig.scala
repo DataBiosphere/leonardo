7c9abd78376a659a01a9db03a945adfd3872005f
LizBaldo <baldo@broadinstitute.org>
2025-05-06 13:51:12 -0400

    fix formatting

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/ClusterResourcesConfig.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/ClusterResourcesConfig.scala
@@ -1,20 +1,21 @@
 package org.broadinstitute.dsde.workbench.leonardo
 package config
 
-case class ClusterResourcesConfig(initScript: RuntimeResource,
-                                  legacyAOUInitScript: RuntimeResource, // AN-503: Delete once AOU has switched to using Dataproc 2.2.X in prod
-                                  cloudInit: Option[RuntimeResource],
-                                  startupScript: RuntimeResource,
-                                  shutdownScript: RuntimeResource,
-                                  jupyterDockerCompose: RuntimeResource,
-                                  gpuDockerCompose: Option[RuntimeResource], // only applies to GCE runtimes
-                                  rstudioDockerCompose: RuntimeResource,
-                                  proxyDockerCompose: RuntimeResource,
-                                  welderDockerCompose: RuntimeResource,
-                                  proxySiteConf: RuntimeResource,
-                                  jupyterNotebookConfigUri: RuntimeResource,
-                                  jupyterNotebookFrontendConfigUri: RuntimeResource,
-                                  customEnvVarsConfigUri: RuntimeResource
+case class ClusterResourcesConfig(
+  initScript: RuntimeResource,
+  legacyAOUInitScript: RuntimeResource, // AN-503: Delete once AOU has switched to using Dataproc 2.2.X in prod
+  cloudInit: Option[RuntimeResource],
+  startupScript: RuntimeResource,
+  shutdownScript: RuntimeResource,
+  jupyterDockerCompose: RuntimeResource,
+  gpuDockerCompose: Option[RuntimeResource], // only applies to GCE runtimes
+  rstudioDockerCompose: RuntimeResource,
+  proxyDockerCompose: RuntimeResource,
+  welderDockerCompose: RuntimeResource,
+  proxySiteConf: RuntimeResource,
+  jupyterNotebookConfigUri: RuntimeResource,
+  jupyterNotebookFrontendConfigUri: RuntimeResource,
+  customEnvVarsConfigUri: RuntimeResource
 )
 
 object ClusterResourcesConfig {
