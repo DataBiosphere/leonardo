8918fcdad005d13f2c514dc2185ec989b4072d74
Uchechukwu Ngwudike <ungwudik@broadinstitute.org>
2025-05-29 07:40:41 -0500

    read from GAR

--- project/Settings.scala
+++ project/Settings.scala
@@ -10,8 +10,8 @@ import scalafix.sbt.ScalafixPlugin.autoImport.scalafixSemanticdb
 import scala.collection.JavaConverters._
 
 object Settings {
-
-  val artifactory = "https://broadinstitute.jfrog.io/broadinstitute/"
+  
+  val artifactory = "https://us-central1-maven.pkg.dev/dsp-artifact-registry/"
 
   val commonResolvers = List(
     "artifactory-releases" at artifactory + "libs-release",
