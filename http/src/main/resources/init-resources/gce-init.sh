78281b6873fc77d365cf446e2dfc2ef0563f4c59
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2025-06-05 17:34:45 -0400

    remove nbstripout install

--- http/src/main/resources/init-resources/gce-init.sh
+++ http/src/main/resources/init-resources/gce-init.sh
@@ -526,16 +526,11 @@ if [ ! -z "$JUPYTER_DOCKER_IMAGE" ] ; then
   # For older jupyter images, jupyter_delocalize.py is using 127.0.0.1 as welder's url, which won't work now that we're no longer using `network_mode: host` for GCE VMs
   docker exec $JUPYTER_SERVER_NAME /bin/bash -c "sed -i 's/127.0.0.1/welder/g' /etc/jupyter/custom/jupyter_delocalize.py"
 
-  log 'Wget the gitignore_global file'
+  log 'Wget the gitignore_global file, set gitignore in Git Config'
 
   # Copy gitignore into jupyter container (ask AOU?)
-  docker exec $JUPYTER_SERVER_NAME /bin/bash -c "wget -N https://raw.githubusercontent.com/DataBiosphere/terra-docker/045a139dbac19fbf2b8c4080b8bc7fff7fc8b177/terra-jupyter-aou/gitignore_global"
-
-  log 'Install nbstripout and set gitignore in Git Config'g
-
-  # Install nbstripout and set gitignore in Git Config (ask AOU?)
-  docker exec $JUPYTER_SERVER_NAME /bin/bash -c "nbstripout --install --global \
-        && git config --global core.excludesfile $JUPYTER_USER_HOME/gitignore_global"
+  docker exec $JUPYTER_SERVER_NAME /bin/bash -c "wget -N https://raw.githubusercontent.com/DataBiosphere/terra-docker/045a139dbac19fbf2b8c4080b8bc7fff7fc8b177/terra-jupyter-aou/gitignore_global \
+  && git config --global core.excludesfile $JUPYTER_USER_HOME/gitignore_global"
 
   # Starts the locking logic (used for AOU). google_sign_in.js  is likely not used anymore
   docker exec -u 0 $JUPYTER_SERVER_NAME /bin/bash -c "$JUPYTER_HOME/scripts/extension/install_jupyter_contrib_nbextensions.sh \
