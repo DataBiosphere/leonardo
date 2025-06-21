ae73b4cd2c1de029d20810eb4008d433de886fa1
Uchechukwu Ngwudike <ungwudik@broadinstitute.org>
2025-06-21 16:53:21 -0500

    update comment

--- codegen_java/project/Publishing.scala
+++ codegen_java/project/Publishing.scala
@@ -18,8 +18,9 @@ object Publishing {
   }
 
   val publishSettings: Seq[Setting[_]] =
-    // we only publish to libs-release-local because of a bug in sbt that makes snapshots take
-    // priority over the local package cache. see here: https://github.com/sbt/sbt/issues/2687#issuecomment-236586241
+    // we only publish to libs-release-local (now libs-release-standard in GAR) because of a bug in sbt that makes
+    // snapshots take priority over the local package cache.
+    // see here: https://github.com/sbt/sbt/issues/2687#issuecomment-236586241
     Seq(
       publishTo := Option(garResolver(false)),
       Compile / publishArtifact := true,
