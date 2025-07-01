583770029937ea56496d7f29a4be548aba2c91ea
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2025-07-01 15:58:48 -0400

    [AN-624] Strip out legacy AOU image (#4863)

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/DataprocConfig.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/config/DataprocConfig.scala
@@ -7,8 +7,6 @@ import org.broadinstitute.dsde.workbench.leonardo.CustomImage.DataprocCustomImag
 final case class DataprocConfig(
   defaultScopes: Set[String],
   customDataprocImage: DataprocCustomImage,
-  // AN-503: Delete once AOU has switched to using Dataproc 2.2.X in prod
-  legacyAouCustomDataprocImage: DataprocCustomImage,
   sparkMemoryConfigRatio: Option[Double],
   minimumRuntimeMemoryInGb: Option[Double],
   runtimeConfigDefaults: RuntimeConfig.DataprocConfig,
