b20bc8e5dda99c8102f0e368ab85c44721a564fb
lmcnatt <85642387+lucymcnatt@users.noreply.github.com>
2025-07-18 07:38:27 -0400

    use root user

--- http/src/main/resources/init-resources/gce-init.sh
+++ http/src/main/resources/init-resources/gce-init.sh
@@ -540,7 +540,7 @@ if [ ! -z "$JUPYTER_DOCKER_IMAGE" ] ; then
   docker exec $JUPYTER_SERVER_NAME /bin/bash -c "ls -l $JUPYTER_HOME/scripts/extension"
 
   # Starts the locking logic (used for AOU). google_sign_in.js  is likely not used anymore
-  docker exec $JUPYTER_SERVER_NAME /bin/bash -c "$JUPYTER_HOME/scripts/extension/install_jupyter_contrib_nbextensions.sh \
+  docker exec -u 0 $JUPYTER_SERVER_NAME /bin/bash -c "$JUPYTER_HOME/scripts/extension/install_jupyter_contrib_nbextensions.sh \
        && mkdir -p $JUPYTER_USER_HOME/.jupyter/custom/ \
        && cp $JUPYTER_HOME/custom/google_sign_in.js $JUPYTER_USER_HOME/.jupyter/custom/ \
        && ls -la $JUPYTER_HOME/custom/extension_entry_jupyter.js \
