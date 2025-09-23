fc877a39a1a30ea5cb34dd68855161bc30369c2e
Keith Suderman <suderman@jhu.edu>
2025-09-23 13:53:12 -0400

    Update GalaxyKubeman chart to version 2.10.1

--- http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
+++ http/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/KubernetesTestData.scala
@@ -48,7 +48,7 @@ object KubernetesTestData {
   val galaxyApp = AppType.Galaxy
 
   val galaxyChartName = ChartName("/leonardo/galaxykubeman")
-  val galaxyChartVersion = ChartVersion("2.10.0")
+  val galaxyChartVersion = ChartVersion("2.10.1")
   val galaxyChart = Chart(galaxyChartName, galaxyChartVersion)
 
   val galaxyReleasePrefix = "gxy-release"
