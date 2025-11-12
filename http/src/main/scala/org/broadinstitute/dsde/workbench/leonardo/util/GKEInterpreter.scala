0f28d57273932455e92cf9aea6ba15630aeef690
Liz Baldo <lizbaldo@Liz-Baldo-H5267Y2XDX.local>
2025-11-12 15:52:06 -0500

    setting is in MasterAuthorizedNetwork

--- http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GKEInterpreter.scala
+++ http/src/main/scala/org/broadinstitute/dsde/workbench/leonardo/util/GKEInterpreter.scala
@@ -199,15 +199,12 @@ class GKEInterpreter[F[_]](
         .setMasterAuthorizedNetworksConfig(
           new com.google.api.services.container.model.MasterAuthorizedNetworksConfig()
             .setEnabled(true)
+            .setGcpPublicCidrsAccessEnabled(true)
             .setCidrBlocks(
               config.clusterConfig.authorizedNetworks
                 .map(ip => new com.google.api.services.container.model.CidrBlock().setCidrBlock(ip.value))
                 .asJava
             )
-        )
-        .setPrivateClusterConfig(
-          new com.google.api.services.container.model.PrivateClusterConfig()
-            .setGcpPublicCidrsAccessEnabled(true)
         )
         .setIpAllocationPolicy(
           new com.google.api.services.container.model.IPAllocationPolicy()
