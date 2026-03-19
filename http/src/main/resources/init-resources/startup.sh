e78b31bed045eb6cca2725eb71e5000a023d551e
lmcnatt <lucymcnatt@gmail.com>
2026-03-19 16:39:41 -0400

    actually 3/18

--- http/src/main/resources/init-resources/startup.sh
+++ http/src/main/resources/init-resources/startup.sh
@@ -128,7 +128,7 @@ function validateCert() {
   ## If cert is old, then pull latest certs. Update date if we need to rotate cert again
   ## TODO: Update the date pattern below to match your NEW certificate's expiration date
   ## For example, if new certs expire "Mar 15 ... 2027", use *"notAfter=Mar 15"*
-  if [[ "$notAfter" != *"notAfter=Mar 19"* ]] ; then
+  if [[ "$notAfter" != *"notAfter=Mar 18"* ]] ; then
     ${GSUTIL_CMD} cp ${SERVER_CRT} ${certFileDirectory}
     ${GSUTIL_CMD} cp ${SERVER_KEY} ${certFileDirectory}
     ${GSUTIL_CMD} cp ${ROOT_CA} ${certFileDirectory}
