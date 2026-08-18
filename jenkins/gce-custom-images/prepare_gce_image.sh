87a0e8a7eab13b4a7a988a7a185f57a88e7d31c6
Adam Nichols <aednichols@gmail.com>
2026-08-18 15:58:28 -0400

    Update obsolete welder hash in cache

--- jenkins/gce-custom-images/prepare_gce_image.sh
+++ jenkins/gce-custom-images/prepare_gce_image.sh
@@ -20,7 +20,7 @@ terra_jupyter_python="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-python:1.1.6"
 terra_jupyter_r="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-r:2.2.7"
 terra_jupyter_bioconductor="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-bioconductor:2.2.7"
 terra_jupyter_gatk="us.gcr.io/broad-dsp-gcr-public/terra-jupyter-gatk:2.3.9"
-welder_server="us.gcr.io/broad-dsp-gcr-public/welder-server:8667bfe"
+welder_server="us.gcr.io/broad-dsp-gcr-public/welder-server:3944358"
 openidc_proxy="broadinstitute/openidc-proxy:2.3.1_2"
 anvil_rstudio_bioconductor="us.gcr.io/broad-dsp-gcr-public/anvil-rstudio-bioconductor:3.21.0"
 
